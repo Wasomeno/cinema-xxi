aff8fc378ae25f3b90426177d2f91f896b8d9182
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-26 23:01:15 +0700

    Add: env contract address variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+const env = {
+  ROLES_CONTRACT_ADDRESS: "0x4f9eF1f5217824122A5C1156DeF7C228F12cE2a8",
+  CINEMA_CONTRACT_ADDRESS: "0x11CD27C2e9bb73BBDa65520B452ba30e3912018d",
+  MOVIES_CONTRACT_ADDRESS: "0x1C0b2F345330afB974Ae568a0b1C7e74709ad38c",
+  TICKET_CONTRACT_ADDRESS: "0x20fE2eA02acE803b8CFf6329904B45AEE01C3569",
+};
+
+module.exports = () => {
+  nextConfig, env;
+
+  return {
+    env,
+  };
+};
