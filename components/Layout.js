95eac93de81c8416021cb8cc47609ebfb781c477
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-28 22:36:49 +0700

    Add: react query provider for admin and app

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,44 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { appContext } from "../context/AppContext";
 import AdminNav from "./AdminNav";
 import AppNav from "./AppNav";
+import NotConnected from "./NotConnected";
 import RegularNav from "./RegularNav";
 
 const Layout = ({ children }) => {
   const router = useRouter();
   const basePath = router.pathname.split("/")[1];
+  const queryClientApp = new QueryClient();
+  const queryClientAdmin = new QueryClient();
+  const isConnected = useContext(appContext).isConnected;
 
-  return basePath === "/" ? (
-    <main className="h-screen">
-      <RegularNav />
-      {children}
-    </main>
-  ) : basePath === "app" ? (
-    <main className="h-screen flex items-center justify-evenly relative">
-      {/* <AppNav /> */}
-      {children}
-    </main>
+  useEffect(() => {}, []);
+
+  return isConnected ? (
+    basePath === "/" ? (
+      <main className="h-screen">
+        <RegularNav />
+        {children}
+      </main>
+    ) : basePath === "app" ? (
+      <QueryClientProvider client={queryClientApp}>
+        <main className="h-screen flex items-center justify-evenly relative">
+          {/* <AppNav /> */}
+          {children}
+        </main>
+      </QueryClientProvider>
+    ) : (
+      <QueryClientProvider client={queryClientAdmin}>
+        <main className="h-screen flex items-center justify-evenly relative">
+          <AdminNav />
+          {children}
+        </main>
+      </QueryClientProvider>
+    )
   ) : (
-    <main className="h-screen flex items-center justify-evenly relative">
-      {children}
-    </main>
+    <NotConnected />
   );
 };
 
