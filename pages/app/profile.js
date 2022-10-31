ee87653f61c4cf052969c8d156fbcb1a1f1cc762
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-10-31 23:23:43 +0700

    Add: loading & toast components and profile page

---
+++ pages/app/profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const profile = () => {
+  return (
+    <div className="w-full h-full rounded-xl">
+      <div className="flex items-center justify-center">
+        <h1 className="font-poppins text-3xl p-2 m-4">Profile</h1>
+      </div>
+    </div>
+  );
+};
+
+export default profile;
