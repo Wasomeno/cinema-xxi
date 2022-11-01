220ae3bd611e43177902c6fd93a252b5e0464d42
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-01 18:33:46 +0700

    Add: styles and loading states (text & loading)

--- components/Loading.js
+++ components/Loading.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { useGetLoading } from "../store/stores";
+import { MoonLoader } from "react-spinners";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  const [loading, loadingText] = useGetLoading();
+  if (!loading) return;
+  return (
+    <>
+      <div className="absolute bg-slate-700 bg-opacity-80 w-screen h-screen" />
+      <div className="absolute w-60 h-64 rounded-lg bg-slate-100 shadow-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center">
+        <p className="m-2 my-4 font-poppins font-medium text-lg">
+          {loadingText}
+        </p>
+        <MoonLoader loading={loading} size={40} color={"black"} />
+      </div>
+    </>
+  );
 };
 
 export default Loading;
