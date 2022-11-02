e45b2bf731b655c8c3b9b556e3b5125cdfa93191
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-02 18:08:01 +0700

    Add: adminDetails & setAdminDetails

--- context/AppContext.js
+++ context/AppContext.js
@@ -2,12 +2,15 @@ import { createContext, useContext, useState } from "react";
 
 export const AppContextDOM = ({ children, Context }) => {
   const [account, setAccount] = useState([]);
+  const [adminDetails, setAdminDetails] = useState({ region: 0, cinema: 0 });
   return (
     <Context.Provider
       value={{
         account: account,
         setAccount: setAccount,
         isConnected: Boolean(account[0]),
+        adminDetails: adminDetails,
+        setAdminDetails: setAdminDetails,
       }}
     >
       {children}
