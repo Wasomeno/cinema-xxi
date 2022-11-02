6da3e4fdc82bb511a4edc10da3f787e4491c56cf
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-02 18:07:36 +0700

    Add: zustand stores for handling loading and toast

---
+++ store/stores.js
@@ -0,0 +1,50 @@
+import create from "zustand";
+
+const loadingStore = create((set) => ({
+  loading: false,
+  text: "",
+  setLoading: (condition) => set(() => ({ loading: condition })),
+  setText: (text) => set(() => ({ text: text })),
+}));
+
+const toastStore = create((set) => ({
+  show: false,
+  text: "",
+  condition: "",
+  error: (string) => {
+    set(() => ({ text: string, show: true, condition: "error" }));
+    setTimeout(() => set(() => ({ show: false })), 2500);
+  },
+  success: (string) => {
+    set(() => ({ text: string, show: true, condition: "success" }));
+    setTimeout(() => set(() => ({ show: false })), 2500);
+  },
+}));
+
+export const useGetLoading = () => {
+  const getLoading = loadingStore((state) => state.loading);
+  const getText = loadingStore((state) => state.text);
+
+  return [getLoading, getText];
+};
+
+export const useSetLoading = () => {
+  const setLoading = loadingStore((state) => state.setLoading);
+  const setText = loadingStore((state) => state.setText);
+
+  return [setLoading, setText];
+};
+
+export const useToast = () => {
+  const toggleSuccess = toastStore((state) => state.success);
+  const toggleError = toastStore((state) => state.error);
+  return [toggleSuccess, toggleError];
+};
+
+export const useToastDetails = () => {
+  const getShow = toastStore((state) => state.show);
+  const getText = toastStore((state) => state.text);
+  const getCondition = toastStore((state) => state.condition);
+
+  return [getShow, getText, getCondition];
+};
