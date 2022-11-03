1570dac876b16b4b2276f7f862dca8dcf7020058
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-03 23:27:29 +0700

    Change: fetch functions to be dynamic

--- pages/admin/add/showtimes.js
+++ pages/admin/add/showtimes.js
@@ -1,29 +1,61 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import useToggle from "../../../hooks/useToggle";
 import useUnary from "../../../hooks/useUnary";
+import { MoonLoader } from "react-spinners";
 import { cinemaContract } from "../../../hooks/useContract";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { ethers } from "ethers";
 import { useSetLoading, useToast } from "../../../store/stores";
 import {
+  fetchCinemaDetails,
   fetchShowTimes,
   fetchStudioShowTimes,
 } from "../../../fetchers/fetchers";
+import { appContext } from "../../../context/AppContext";
 
 const showtimes = () => {
+  const [selectedStudio, setSelectedStudio] = useState(1);
+  const [selectedShowTimes, setSelectedShowTimes] = useState([]);
   const [hour, incrementHour, decrementHour] = useUnary(1, 10, 1, 1);
   const [minutes, incrementMinutes, decrementMinutes] = useUnary(5, 55, 5, 5);
   const [setLoading, setLoadingText] = useSetLoading();
   const [toastSuccess, toastError] = useToast();
   const [showStudio, toggleShowStudio] = useToggle(false);
 
-  const showTimes = useQuery(["showTimes", 6996, 1], () =>
-    fetchShowTimes(6996, 1)
+  const adminDetails = useContext(appContext).adminDetails;
+
+  const cinemaStudiosAmount = useQuery(
+    ["studiosAmount", adminDetails.region, adminDetails.cinema],
+    () => fetchCinemaDetails(adminDetails.region, adminDetails.cinema)
   );
 
-  const studioShowTimes = useQuery(["studioShowTimes", 6996, 1, 1], () =>
-    fetchStudioShowTimes(6996, 1, 1)
+  const showTimes = useQuery(
+    ["showTimes", adminDetails.region, adminDetails.cinema],
+    () => fetchShowTimes(adminDetails.region, adminDetails.cinema)
   );
+
+  const studioShowTimes = useQuery(
+    [
+      "studioShowTimes",
+      adminDetails.region,
+      adminDetails.cinema,
+      selectedStudio,
+    ],
+    () =>
+      fetchStudioShowTimes(
+        adminDetails.region,
+        adminDetails.cinema,
+        selectedStudio
+      )
+  );
+
+  const getStudios = () => {
+    let studio = [];
+    for (let i = 0; i < cinemaStudiosAmount.data; ++i) {
+      studio.push(i + 1);
+    }
+    return studio;
+  };
 
   const addShowTimeMutation = useMutation(() => addShowTime(), {
     onMutate: () => {
@@ -42,7 +74,7 @@ const showtimes = () => {
 
   const addStudioShowTimesMutation = useMutation(() => addShowTimesToStudio(), {
     onMutate: () => {
-      setLoadingText("Adding new region");
+      setLoadingText("Adding show times to studio " + selectedStudio);
       setLoading(true);
     },
     onError: (result) => {
@@ -51,9 +83,7 @@ const showtimes = () => {
     },
     onSuccess: () => {
       setLoading(false);
-      toastSuccess(
-        "Region " + regionNameRef.current.value + " successfully added"
-      );
+      toastSuccess("Successfully added show times to studio " + selectedStudio);
     },
   });
 
@@ -61,7 +91,11 @@ const showtimes = () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = cinemaContract();
     const timeInSeconds = hour * 3600 + minutes * 60;
-    const addShowTime = await contract.addShowTime(12345, 1, timeInSeconds);
+    const addShowTime = await contract.addShowTime(
+      adminDetails.region,
+      adminDetails.cinema,
+      timeInSeconds
+    );
     const waitTransaction = await provider.waitForTransaction(addShowTime.hash);
     return waitTransaction;
   };
@@ -69,11 +103,11 @@ const showtimes = () => {
   const addShowTimesToStudio = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const contract = cinemaContract();
-    const timeInSeconds = hour * 3600 + minutes * 60;
     const addShowTimes = await contract.addStudioShowTimes(
-      12345,
-      1,
-      timeInSeconds
+      adminDetails.region,
+      adminDetails.cinema,
+      selectedStudio,
+      selectedShowTimes
     );
     const waitTransaction = await provider.waitForTransaction(
       addShowTimes.hash
@@ -81,14 +115,11 @@ const showtimes = () => {
     return waitTransaction;
   };
 
-  useEffect(() => {
-    console.log(showTimes.data);
-    console.log(studioShowTimes.data);
-  }, [studioShowTimes.isLoading]);
+  useEffect(() => {}, [studioShowTimes.isLoading]);
 
   return (
     <div className="w-full h-full">
-      {/* <div className="text-center">
+      <div className="text-center">
         <h1 className="font-poppins font-semibold text-2xl m-4 p-2">
           Manage Show Times
         </h1>
@@ -210,6 +241,27 @@ const showtimes = () => {
             <h1 className="font-poppins m-2 font-medium p-2 text-center">
               Current Show Times
             </h1>
+            <div className="flex items-center justify-center gap-2">
+              {showTimes.isLoading && (
+                <div className="flex flex-col justify-center items-center">
+                  <p className="font-poppins m-2 my-3">Fetching Show Times</p>
+                  <MoonLoader
+                    loading={studioShowTimes.isLoading}
+                    size={25}
+                    color={"black"}
+                  />
+                </div>
+              )}
+              {!showTimes.isLoading &&
+                showTimes.data.map((showtime, index) => (
+                  <div
+                    key={index}
+                    className="bg-slate-100 text-center shadow-md p-2 rounded-lg w-1/6"
+                  >
+                    {parseInt(showtime)}
+                  </div>
+                ))}
+            </div>
           </div>
         </div>
         <div className="h-full w-5/12 flex flex-col justify-around items-center">
@@ -222,38 +274,51 @@ const showtimes = () => {
               </div>
               <div className="flex flex-col items-center justify-center m-2 h-4/6 relative">
                 <h5 className="m-2 font-poppins font-sm">Select Studio</h5>
-                <div className="w-2/6 p-3 flex justify-between items-center bg-slate-100 shadow-md rounded-lg">
-                  <h5 className="font-poppins text-sm">Studio 1</h5>
-                  <button onClick={() => toggleShowStudio()}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="currentColor"
-                      className={
-                        !showStudio
-                          ? "transition duration-300 ease-in-out w-4 h-4"
-                          : "transition duration-300 ease-in-out w-4 h-4 rotate-90"
-                      }
-                      strokeWidth={2}
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div
-                  className={
-                    !showStudio
-                      ? "absolute overflow-hidden h-0 transition duration-300"
-                      : "absolute w-2/6 p-2 bg-slate-100 shadow-md rounded-b-lg transition duration-300 -bottom-4"
-                  }
-                >
-                  <button className="font-poppins p-2 w-full text-sm text-start">
-                    Studio 1
-                  </button>
+                <div className="flex justify-center relative w-full text-center">
+                  <div className="w-2/6 p-3 flex justify-between items-center bg-slate-100 shadow-md rounded-lg">
+                    <h5 className="font-poppins text-sm">
+                      {"Studio " + selectedStudio}
+                    </h5>
+                    <button onClick={() => toggleShowStudio()}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className={
+                          !showStudio
+                            ? "transition duration-300 ease-in-out w-4 h-4"
+                            : "transition duration-300 ease-in-out w-4 h-4 rotate-90"
+                        }
+                        strokeWidth={2}
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M16.28 11.47a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 011.06-1.06l7.5 7.5z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <div
+                    className={
+                      !showStudio
+                        ? "absolute overflow-hidden h-0 transition duration-300"
+                        : "absolute w-2/6 p-2 bg-slate-100 shadow-md rounded-b-lg transition duration-300 -bottom-28"
+                    }
+                  >
+                    {getStudios().map((studio, index) => (
+                      <button
+                        key={index}
+                        className="font-poppins p-2 w-full text-sm text-start"
+                        onClick={() => {
+                          setSelectedStudio(studio);
+                          toggleShowStudio();
+                        }}
+                      >
+                        Studio {studio}
+                      </button>
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
@@ -266,41 +331,75 @@ const showtimes = () => {
                   <h3 className="font-poppins text-sm m-2 text-center">
                     Available Show Time
                   </h3>
-                  <div className="w-full bg-slate-100 shadow-md rounded-lg h-4/6 flex flex-col items-center justify-center">
-                    {showTimes.length < 1 ? (
-                      <h5 className="text-center font-poppins text-xs">
-                        No Show Times
-                      </h5>
-                    ) : (
-                      showTimes.map((showTime, index) => (
-                        <button
-                          key={index}
-                          className="p-2 border mb-2 border-slate-300 rounded-xl w-3/6 h-20 text-center transition duration-200 ease-in-out hover:bg-black hover:text-white"
-                        >
-                          <p className="font-poppins text-sm">{"test"}</p>
-                        </button>
-                      ))
+                  <div className="w-full bg-slate-100 shadow-md rounded-lg h-4/6 flex flex-col items-center justify-center overflow-y-scroll">
+                    {showTimes.isLoading && (
+                      <>
+                        <p className="font-poppins m-2 my-3">
+                          Fetching Show Times
+                        </p>
+                        <MoonLoader
+                          loading={studioShowTimes.isLoading}
+                          size={25}
+                          color={"black"}
+                        />
+                      </>
                     )}
+                    {!showTimes.isLoading &&
+                      (showTimes.data.length < 1 ? (
+                        <h5 className="text-center font-poppins text-xs">
+                          No Show Times
+                        </h5>
+                      ) : (
+                        showTimes.data.map((showTime, index) => (
+                          <button
+                            key={index}
+                            onClick={() =>
+                              setSelectedShowTimes((current) => [
+                                ...current,
+                                showTime,
+                              ])
+                            }
+                            className="p-2 border mb-2 border-slate-300 rounded-xl w-3/6 h-1/6 text-center transition duration-200 ease-in-out hover:bg-black hover:text-white"
+                          >
+                            <p className="font-poppins text-sm">
+                              {parseInt(showTime)}
+                            </p>
+                          </button>
+                        ))
+                      ))}
                   </div>
                 </div>
                 <div className="w-3/6 h-full">
                   <h3 className="font-poppins text-sm m-2 text-center">
                     Current Show Times
                   </h3>
-                  <div className="w-full bg-slate-100 shadow-md rounded-lg h-4/6">
+                  <div className="w-full bg-slate-100 shadow-md rounded-lg h-4/6 overflow-y-scroll">
                     <div className="flex flex-col items-center justify-center py-3 h-full">
-                      {showTimes.length < 1 ? (
+                      {studioShowTimes.isLoading ? (
+                        <>
+                          <p className="font-poppins m-2 my-3">
+                            Fetching Show Times
+                          </p>
+                          <MoonLoader
+                            loading={studioShowTimes.isLoading}
+                            size={25}
+                            color={"black"}
+                          />
+                        </>
+                      ) : studioShowTimes.data.length < 1 ? (
                         <h5 className="text-center font-poppins text-xs">
                           No Show Times
                         </h5>
                       ) : (
-                        studioShowTimes.map((showTime, index) => (
-                          <div
+                        studioShowTimes.data.map((showTime, index) => (
+                          <button
                             key={index}
-                            className="p-2 border mb-2 border-slate-300 rounded-xl w-3/6 text-center"
+                            className="p-2 border mb-2 border-slate-300 rounded-xl w-3/6 h-1/6 text-center transition duration-200 ease-in-out hover:bg-black hover:text-white"
                           >
-                            <p className="font-poppins text-sm">{showTime}</p>
-                          </div>
+                            <p className="font-poppins text-sm">
+                              {parseInt(showTime)}
+                            </p>
+                          </button>
                         ))
                       )}
                     </div>
@@ -311,14 +410,14 @@ const showtimes = () => {
             <div className="text-center m-2 mx-auto">
               <button
                 className="bg-black p-2 rounded-lg text-white font-poppins text-sm w-2/6"
-                onClick={() => addShowTimeMutation.mutate()}
+                onClick={() => addStudioShowTimesMutation.mutate()}
               >
                 Submit
               </button>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
