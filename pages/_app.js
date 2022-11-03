d37de200ac4f2dcc1c82d562f3a6de6c91a1c2cc
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-03 23:23:03 +0700

    Add: move react query client to separate file

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,24 @@
 import Layout from "../components/Layout";
 import Loading from "../components/Loading";
 import Toast from "../components/Toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { appContext, AppContextDOM } from "../context/AppContext";
 import "../styles/globals.css";
+import { queryClientApp } from "../client/reactQueryClient";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AppContextDOM Context={appContext}>
-      <Layout>
-        <Loading />
-        <Component {...pageProps} />;
-        <Toast />
-      </Layout>
-    </AppContextDOM>
+    <QueryClientProvider client={queryClientApp}>
+      <AppContextDOM Context={appContext}>
+        <Layout>
+          <Loading />
+          <Component {...pageProps} />;
+          <Toast />
+        </Layout>
+      </AppContextDOM>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
