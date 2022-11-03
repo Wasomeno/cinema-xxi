018d02e905a0f938784af4f993514b2f42161500
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-11-03 23:24:58 +0700

    Change: changed order of conditional renders

--- components/Layout.js
+++ components/Layout.js
@@ -3,46 +3,36 @@ import { useRouter } from "next/router";
 import React, { useContext, useEffect } from "react";
 import { appContext } from "../context/AppContext";
 import AdminLayout from "./AdminLayout";
-import AdminNav from "./AdminNav";
 import AppNav from "./AppNav";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import NotConnected from "./NotConnected";
 import RegularNav from "./RegularNav";
 
 const Layout = ({ children }) => {
   const router = useRouter();
   const basePath = router.pathname.split("/")[1];
-  const queryClientApp = new QueryClient();
-  const queryClientAdmin = new QueryClient();
   const isConnected = useContext(appContext).isConnected;
-  const user = useContext(appContext).account[0];
-  const checkConnected = async () => {
-    const contract = rolesContract();
-    const result = await contract.adminToDetails(user);
-    return result;
-  };
 
-  useEffect(() => {}, []);
-
-  return isConnected ? (
-    basePath === "" ? (
-      <main className="h-screen">
-        <RegularNav />
+  return basePath === "app" ? (
+    isConnected ? (
+      <main className="h-screen flex items-center justify-evenly relative">
+        <AppNav />
         {children}
       </main>
-    ) : basePath === "app" ? (
-      <QueryClientProvider client={queryClientApp}>
-        <main className="h-screen flex items-center justify-evenly relative">
-          {/* <AppNav /> */}
-          {children}
-        </main>
-      </QueryClientProvider>
     ) : (
-      <QueryClientProvider client={queryClientAdmin}>
-        <AdminLayout>{children}</AdminLayout>
-      </QueryClientProvider>
+      <NotConnected />
+    )
+  ) : basePath === "admin" ? (
+    isConnected ? (
+      <AdminLayout>{children}</AdminLayout>
+    ) : (
+      <NotConnected />
     )
   ) : (
-    <NotConnected />
+    <main className="h-screen">
+      <RegularNav />
+      {children}
+    </main>
   );
 };
 
