7b2fa104273a193b8926fa1187092f1aeddef769
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-13 21:36:39 +0700

    Removed add region & add cinema menu

--- components/Admin/AdminNav/AdminNavMobile.js
+++ components/Admin/AdminNav/AdminNavMobile.js
@@ -58,49 +58,6 @@ const AdminNavMobile = () => {
           </button>
         </Link>
 
-        {/* <Link href={"/admin/add/region"}>
-          <button className={getClass("region")}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
-              />
-            </svg>
-          </button>
-        </Link> */}
-
-        <Link href={"/admin/add/cinema"}>
-          <button className={getClass("cinema")}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z"
-              />
-            </svg>
-          </button>
-        </Link>
-
         <Link href={"/admin/add/showtimes"}>
           <button className={getClass("showtimes")}>
             <svg
