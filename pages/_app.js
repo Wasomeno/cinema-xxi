01434af2e7788e484f3913fd48be1a82056f7889
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-17 18:15:13 +0700

    Added wagmi-client & used it in WagmiConfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,26 @@
 import Layout from "../components/Layout";
 import Loading from "../components/Loading";
 import Toast from "../components/Toast";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { appContext, AppContextDOM } from "../context/AppContext";
 import "../styles/globals.css";
 import { queryClientApp } from "../client/reactQueryClient";
+import { WagmiConfig } from "wagmi";
+import client from "../client/wagmiClient";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClientApp}>
-      <AppContextDOM Context={appContext}>
-        <Layout>
-          <Loading />
-          <Component {...pageProps} />;
-          <Toast />
-        </Layout>
-      </AppContextDOM>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <WagmiConfig client={client}>
+      <QueryClientProvider client={queryClientApp}>
+        <AppContextDOM Context={appContext}>
+          <Layout>
+            <Loading />
+            <Component {...pageProps} />;
+            <Toast />
+          </Layout>
+        </AppContextDOM>
+      </QueryClientProvider>
+    </WagmiConfig>
   );
 }
 
