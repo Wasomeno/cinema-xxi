e579a7561890fb775582c5538ca06d60daff549e
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-17 19:25:20 +0700

    Removed <a> tag

--- components/RegularNav.js
+++ components/RegularNav.js
@@ -4,15 +4,9 @@ import React from "react";
 const RegularNav = () => {
   return (
     <header className="flex justify-around items-center m-4 fixed w-screen">
-      <Link href={"/"}>
-        <a>Home</a>
-      </Link>
-      <Link href={"about"}>
-        <a>About</a>
-      </Link>
-      <Link href={"app"}>
-        <a>App</a>
-      </Link>
+      <Link href={"/"}>Home</Link>
+      <Link href={"about"}>About</Link>
+      <Link href={"app"}>App</Link>
     </header>
   );
 };
