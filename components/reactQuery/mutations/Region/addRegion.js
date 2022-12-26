1784ebc84ed2116dcc9fdd98063b745bd054a4ee
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-26 16:58:55 +0700

    Added addRegion mutation for adding new region

---
+++ components/reactQuery/mutations/Region/addRegion.js
@@ -0,0 +1,37 @@
+import { ethers } from "ethers";
+import { regionContract } from "../../../../hooks/useContract";
+import { useLoading, useToast } from "../../../../store/stores";
+import mutation from "../mutation";
+
+const addRegionSideEffects = (region) => {
+  const [setLoading, setLoadingText] = useLoading();
+  const [toastSuccess, toastError] = useToast();
+  return {
+    onMutate: () => {
+      setLoadingText("Adding new region");
+      setLoading(true);
+    },
+    onError: (result) => {
+      setLoading(false);
+      toastError(result.reason);
+    },
+    onSuccess: () => {
+      setLoading(false);
+      toastSuccess("Region " + region + " successfully added");
+    },
+  };
+};
+
+export const addRegion = ({ regionId, regionName }) => {
+  const contract = regionContract();
+
+  const { mutate } = mutation(async () => {
+    const provider = new ethers.providers.Web3Provider(window?.ethereum);
+    const regionNameBytes32 = ethers.utils.formatBytes32String(regionName);
+    const transaction = await contract.addRegion(regionId, regionNameBytes32);
+    const waitTransaction = await provider.waitForTransaction(transaction.hash);
+    return waitTransaction;
+  }, addRegionSideEffects(regionName));
+
+  return mutate;
+};
