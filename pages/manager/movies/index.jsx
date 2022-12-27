570814ff0e5b49469ae5e1bb806fa1dd03a9fd7f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-27 23:42:22 +0700

    Renamed ManagerHeader to ManagerDashboardHeader

--- pages/manager/movies/index.jsx
+++ pages/manager/movies/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AnimatedContainer from "../../../components/AnimatedContainer";
-import ManagerHeader from "../../../components/Manager/ManagerHeader";
+import ManagerDashboardHeader from "../../../components/Manager/ManagerDashboardHeader";
 
 const ManageMovies = () => {
   return (
     <AnimatedContainer className="w-full h-full p-4">
-      <ManagerHeader title="Manage Movies" />
+      <ManagerDashboardHeader title="Manage Movies" />
+      *//1. add tab menus for managing the cinemas (list, update, delete,
+      details, search); *//2. add tab bodies for all those menus
     </AnimatedContainer>
   );
 };
