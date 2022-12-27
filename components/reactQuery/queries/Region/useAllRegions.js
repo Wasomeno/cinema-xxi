35dfd11175b158c267239e6f1ee8c1b7a2121857
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-27 23:16:35 +0700

    Added read props

--- components/reactQuery/queries/Region/useAllRegions.js
+++ components/reactQuery/queries/Region/useAllRegions.js
@@ -3,10 +3,11 @@ import { regionContract } from "../../../../hooks/useContract";
 import { query } from "../../query";
 
 export const useAllRegions = () => {
-  const contract = regionContract();
+  const contract = regionContract({ read: true });
   const result = query({
     queryKey: ["allRegions"],
     queryFunction: async () => await contract.getRegions(),
+    enabledCondition: true,
   });
   return result;
 };
