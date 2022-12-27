ddf5621c414104941738341a531a1ba1860f3bd9
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-27 23:37:53 +0700

    Added RegionManagerMenu component

--- components/Manager/Region/RegionManagerMenu.jsx
+++ components/Manager/Region/RegionManagerMenu.jsx
@@ -1,19 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import AnimatedContainer from "../../AnimatedContainer";
+import Plus from "../../Icons/Plus";
 
-const RegionManagerMenu = () => {
+export const RegionManagerMenu = () => {
   return (
-    <div className="flex justify-end items-center h-16">
-      <div>
-        <Link
-          href={"/manager/region/add"}
-          className="p-2 px-3 bg-lime-500 text-xs font-medium font-poppins rounded-md"
-        >
-          Add New Region
-        </Link>
-      </div>
-    </div>
+    <AnimatedContainer className="flex flex-col justify-center items-center h-16 w-40 p-2 bg-slate-200 rounded-md z-20 shadow-md absolute right-16 top-10">
+      <Link
+        href={"/manager/region/add"}
+        className="text-sm flex justify-evenly items-center h-full w-full"
+      >
+        <Plus />
+        Add new region
+      </Link>
+    </AnimatedContainer>
   );
 };
-
-export default RegionManagerMenu;
