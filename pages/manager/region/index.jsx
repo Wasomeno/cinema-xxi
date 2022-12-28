e7aad961cc6688e046d5411bd6de7361a8c869ef
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-28 17:21:02 +0700

    Added option props & RegionList

--- pages/manager/region/index.jsx
+++ pages/manager/region/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import AnimatedContainer from "../../../components/AnimatedContainer";
-import ManagerHeader from "../../../components/Manager/ManagerHeader";
+import ManagerDashboardHeader from "../../../components/Manager/ManagerDashboardHeader";
+import RegionList from "../../../components/Manager/Region/RegionList";
+import { RegionManagerMenu } from "../../../components/Manager/Region/RegionManagerMenu";
 
 const ManageRegions = () => {
   return (
     <AnimatedContainer className="w-full h-full p-4">
-      <ManagerHeader title="Manage Regions" />
+      <ManagerDashboardHeader
+        title="Manage Regions"
+        withOption
+        OptionMenu={RegionManagerMenu}
+      />
+      <RegionList />
     </AnimatedContainer>
   );
 };
