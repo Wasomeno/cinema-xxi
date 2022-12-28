bbb5f2519e720180759327ae9218c68ed3091d55
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-28 18:09:13 +0700

    Added a simple showtimes list

--- pages/manager/region/[regionId]/[cinemaId]/showtimes/index.jsx
+++ pages/manager/region/[regionId]/[cinemaId]/showtimes/index.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import AnimatedContainer from "../../../../../../components/AnimatedContainer";
 import ManagerDashboardHeader from "../../../../../../components/Manager/ManagerDashboardHeader";
+import { useShowTimes } from "../../../../../../components/reactQuery/queries/Cinema/useShowTimes";
 
-export const getServerSideProps = (context) => {
+export const getServerSideProps = async (context) => {
   const { regionId, cinemaId } = context.query;
+  const showTimes = await useShowTimes({ region: regionId, cinema: cinemaId });
   return {
     props: {
+      showTimes: showTimes,
       region: regionId,
       cinema: cinemaId,
     },
   };
 };
 
-const ShowTimes = ({ region, cinema }) => {
+const ShowTimes = ({ showTimes }) => {
   return (
-    <AnimatedContainer>
-      <ManagerDashboardHeader title="Showtimes" />
+    <AnimatedContainer className="p-4">
+      <ManagerDashboardHeader title="Showtimes" withBackButton />
+      <div className="mt-5 mb-3 w-3/6">
+        <p className="font-medium font-poppins text-sm text-slate-500">
+          Showtimes list
+        </p>
+      </div>
+      <div className="w-full flex flex-col justify-start items-center gap-4">
+        {showTimes.length < 1 ? (
+          <p className="text-sm font-poppins font-medium">
+            No active showtimes
+          </p>
+        ) : (
+          showTimes.map((showtime) => <div>{showtime}</div>)
+        )}
+      </div>
     </AnimatedContainer>
   );
 };
