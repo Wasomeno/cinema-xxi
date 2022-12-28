4e59ef09015e3f891b667c9eb96493091210dc24
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-28 17:15:44 +0700

    Added withBackButton props & dynamic backbutton

--- components/Manager/ManagerDashboardHeader.jsx
+++ components/Manager/ManagerDashboardHeader.jsx
@@ -1,30 +1,47 @@
 import { AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
+import { useRouter } from "next/router";
 import AnimatedContainer from "../AnimatedContainer";
 import EllipsisVertical from "../Icons/EllipsisVertical";
+import ChevronLeft from "../Icons/ChevronLeft";
 import { Title } from "../shared/Texts";
 
-const ManagerDashboardHeader = ({ title, withOption, OptionMenu }) => {
+const ManagerDashboardHeader = ({
+  title,
+  withOption,
+  withBackButton,
+  OptionMenu,
+}) => {
   const [option, setOption] = useState();
+  const { asPath, push } = useRouter();
   return (
     <>
-      <div className="w-full flex justify-center items-center h-14">
-        <div className="w-1/6" />
+      <div className="w-full flex justify-evenly items-center h-14">
+        {withBackButton ? (
+          <div className="w-1/6 text-center">
+            <button
+              className="bg-slate-100 shadow-md rounded-full w-8 h-8 p-1 relative"
+              onClick={() => push(asPath.slice(0, asPath.lastIndexOf("/")))}
+            >
+              <ChevronLeft />
+            </button>
+          </div>
+        ) : (
+          <div className="w-1/6" />
+        )}
         <div className="w-3/6 text-center">
           <Title text={title} />
           <div className="h-1 w-4/6 rounded-full bg-blue-400 mx-auto mt-2" />
         </div>
         {withOption ? (
-          <>
-            <div className="w-1/6 text-center">
-              <button
-                className="bg-slate-100 shadow-md rounded-full w-8 h-8 relative z-30"
-                onClick={() => setOption((current) => !current)}
-              >
-                <EllipsisVertical />
-              </button>
-            </div>
-          </>
+          <div className="w-1/6 text-center">
+            <button
+              className="bg-slate-100 shadow-md rounded-full w-8 h-8 relative z-30"
+              onClick={() => setOption((current) => !current)}
+            >
+              <EllipsisVertical />
+            </button>
+          </div>
         ) : (
           <div className="w-1/6" />
         )}
