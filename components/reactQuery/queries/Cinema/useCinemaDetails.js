04a109422b8a24ae0104d520fece4548a6a3718a
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-28 17:26:26 +0700

    Moved parsing from cinema details page to useCinemaDetails

--- components/reactQuery/queries/Cinema/useCinemaDetails.js
+++ components/reactQuery/queries/Cinema/useCinemaDetails.js
@@ -1,7 +1,19 @@
+import { ethers } from "ethers";
 import { cinemaContract } from "../../../../hooks/useContract";
 import { query } from "../../query";
 
 export const useCinemaDetails = async ({ region, cinema }) => {
   const contract = cinemaContract({ read: true });
-  return await contract.getCinemaDetails(region, cinema);
+  const cinemaDetails = await contract.getCinemaDetails(region, cinema);
+  return JSON.parse(
+    JSON.stringify({
+      name: ethers.utils.parseBytes32String(cinemaDetails.name),
+      studiosAmount: cinemaDetails.studiosAmount,
+      moviesAmount: cinemaDetails.moviesAmount,
+      showTimesAmount: cinemaDetails.showTimesAmount,
+      showTimes: cinemaDetails.showTimes,
+      studioShowTimes: cinemaDetails.studioShowTimes,
+      studioCapacities: cinemaDetails.studiosCapacities,
+    })
+  );
 };
