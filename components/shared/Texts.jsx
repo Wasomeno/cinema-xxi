7d75781f7e5290adac22e4bd09299d184f0f61a0
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-29 18:10:22 +0700

    Added Paragraph and Subtitle components

--- components/shared/Texts.jsx
+++ components/shared/Texts.jsx
@@ -6,6 +6,35 @@ export const Title = ({ text, isLoading }) => {
   );
 };
 
+export const Paragraph = ({ text, size, style, margin }) => {
+  return (
+    <p
+      className={
+        "font-poppins " +
+        (size ? "text-" + size : "text-base") +
+        " " +
+        (style ? "font-" + style : "font-normal") +
+        " " +
+        (margin ? "m-" + margin : "m-0")
+      }
+    >
+      {text}
+    </p>
+  );
+};
+
+export const Subtitle = ({ text, size }) => {
+  return (
+    <h2
+      className={
+        "font-poppins font-medium " + (size ? "text-" + size : "text-base")
+      }
+    >
+      {text}
+    </h2>
+  );
+};
+
 const TitleLoading = () => {
   return <div className="w-3/6 h-8 bg-slate-500 rounded-md animate-pulse" />;
 };
