4d329181d72dcd09c2bb6f7197c1356b6c92151c
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-29 18:17:36 +0700

    Added a list to display all movies

--- pages/manager/movies/index.jsx
+++ pages/manager/movies/index.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import AnimatedContainer from "../../../components/AnimatedContainer";
 import ManagerDashboardHeader from "../../../components/Manager/ManagerDashboardHeader";
+import MovieListCard from "../../../components/Manager/Movies/MovieListCard";
+import MoviesManagerMenu from "../../../components/Manager/Movies/MoviesManagerMenu";
+import { useAllMovies } from "../../../components/reactQuery/queries/Movie/useAllMovies";
 
-const ManageMovies = () => {
+export const getServerSideProps = async () => {
+  const movies = await useAllMovies();
+  return { props: { movies: movies } };
+};
+
+const ManageMovies = ({ movies }) => {
   return (
     <AnimatedContainer className="w-full h-full p-4">
-      <ManagerDashboardHeader title="Manage Movies" />
-      *//1. add tab menus for managing the cinemas (list, update, delete,
-      details, search); *//2. add tab bodies for all those menus
+      <ManagerDashboardHeader
+        title="Manage Movies"
+        withOption
+        OptionMenu={MoviesManagerMenu}
+      />
+      <div className="">List of Movies</div>
+      <div className="flex flex-col gap-4 justify-start items-center">
+        {movies.length < 1 ? (
+          <p className="font-poppins text-sm">No movies</p>
+        ) : (
+          movies.map((movie) => <MovieListCard movie={movie} />)
+        )}
+      </div>
     </AnimatedContainer>
   );
 };
