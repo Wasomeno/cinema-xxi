9978416abc25017167a195969c83cf91ce3da1bf
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-29 18:09:44 +0700

    Added size & color props

--- components/Icons/ChevronLeft.jsx
+++ components/Icons/ChevronLeft.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
-const ChevronLeft = () => {
+const ChevronLeft = ({ size, color }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       strokeWidth={1.5}
-      stroke="currentColor"
-      className="w-6 h-6"
+      stroke={color ? color : "black"}
+      className={
+        "w-" + (size ? size : "6") + " h-" + (size ? size : "6") + " mx-auto"
+      }
     >
       <path
         strokeLinecap="round"
