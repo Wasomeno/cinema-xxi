e772b3bb81de59da5e7698d989a24c85375d5ec7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-29 18:15:45 +0700

    Added CinemaManagerMenu component

---
+++ components/Manager/Cinema/CinemaManagerMenu.jsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+import AnimatedContainer from "../../AnimatedContainer";
+import Plus from "../../Icons/Plus";
+
+export const CinemaManagerMenu = ({ region, cinemaId }) => {
+  return (
+    <AnimatedContainer className="flex flex-col justify-center items-center h-16 w-40 p-2 bg-slate-200 rounded-md z-20 shadow-md absolute right-16 top-10">
+      <Link
+        href={"/manager/region/" + region + "/" + cinemaId + "/showtimes/add"}
+        className="text-sm flex justify-evenly items-center h-full w-full"
+      >
+        <Plus />
+        Add Show Times
+      </Link>
+    </AnimatedContainer>
+  );
+};
