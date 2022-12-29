cfcefe619e1a1a47090ba6f5d7e83f7f12c43b01
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-29 18:09:52 +0700

    Added new icons

---
+++ components/Icons/ChevronRight.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const ChevronRight = ({ size, color }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke={color ? color : "black"}
+      className={
+        "w-" + (size ? size : "6") + " h-" + (size ? size : "6") + " mx-auto"
+      }
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+};
+
+export default ChevronRight;
