b1c2af30c8df8731bad8aab7cc3d322e15f474c7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-30 23:27:48 +0700

    Added AnimatePresence and replaced div with AnimatedContainer

--- components/Loading.js
+++ components/Loading.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { useLoadingDetails } from "../store/stores";
 import { MoonLoader } from "react-spinners";
+import { AnimatePresence } from "framer-motion";
+import AnimatedContainer from "./AnimatedContainer";
 
 const Loading = () => {
   const [loading, loadingText] = useLoadingDetails();
-  if (!loading) return;
+
   return (
-    <>
-      <div className="fixed bg-slate-700 bg-opacity-80 w-screen h-screen top-0 z-20" />
-      <div className="fixed w-64 h-72 rounded-lg bg-slate-100 shadow-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-5 z-30">
-        <p className="font-poppins font-medium text-lg text-center">
-          {loadingText}
-        </p>
-        <MoonLoader loading={loading} size={40} color={"black"} />
-        <p className="font-poppins text-sm text-center">
-          Confirm the transaction in your wallet
-        </p>
-      </div>
-    </>
+    <AnimatePresence>
+      {loading && (
+        <>
+          <AnimatedContainer className="fixed bg-slate-700 bg-opacity-80 w-screen h-screen top-0 z-20" />
+          <AnimatedContainer className="fixed w-64 h-72 rounded-lg bg-slate-100 shadow-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-5 z-30">
+            <p className="font-poppins font-medium text-lg text-center">
+              {loadingText}
+            </p>
+            <MoonLoader loading={loading} size={40} color={"black"} />
+            <p className="font-poppins text-sm text-center">
+              Confirm the transaction in your wallet
+            </p>
+          </AnimatedContainer>
+        </>
+      )}
+    </AnimatePresence>
   );
 };
 
--- components/Toast.js
+++ components/Toast.js
@@ -1,47 +1,35 @@
+import { AnimatePresence } from "framer-motion";
 import React from "react";
 import { useToastDetails } from "../store/stores";
+import AnimatedContainer from "./AnimatedContainer";
+import CheckMark from "./Icons/Checkmark";
+import Warning from "./Icons/Warning";
+import { Paragraph } from "./shared/Texts";
+
+const conditions = {
+  success: {
+    class: "from-green-200 via-green-300 to-green-100",
+    icon: <CheckMark />,
+  },
+  error: { class: "from-red-200 via-red-300 to-red-100", icon: <Warning /> },
+};
 
 const Toast = () => {
   const [show, text, condition] = useToastDetails();
-  if (!show) return;
-  return condition === "success" ? (
-    <div className="flex justify-center items-center absolute bg-gradient-to-r from-green-200 via-green-300 to-green-100 rounded-lg w-5/6 h-16 p-2 shadow-md bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          fillRule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-          clipRule="evenodd"
-        />
-      </svg>
-
-      <p className="font-poppins font-medium m-2 mx-4 text-sm text-center">
-        {text}
-      </p>
-    </div>
-  ) : (
-    <div className="flex justify-center items-center absolute bg-gradient-to-r from-red-200 via-red-300 to-red-100 rounded-lg w-5/6 h-16 p-2 shadow-md bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          fillRule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-          clipRule="evenodd"
-        />
-      </svg>
-
-      <p className="font-poppins font-medium m-2 mx-4 text-sm text-center">
-        {text}
-      </p>
-    </div>
+  return (
+    <AnimatePresence>
+      {show && (
+        <AnimatedContainer
+          className={
+            "flex justify-center items-center absolute bg-gradient-to-r rounded-lg w-5/6 h-14 p-2 shadow-md bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 z-30 " +
+            conditions[condition].class
+          }
+        >
+          {conditions[condition].icon}
+          <Paragraph text={text} size="sm" />
+        </AnimatedContainer>
+      )}
+    </AnimatePresence>
   );
 };
 
