0ecbaeaf4bf19c20856ee04d0576cfac6b4fe759
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-30 23:29:23 +0700

    Changed Link href to manager/movies/[movieId]

--- components/Manager/Movies/MovieListCard.jsx
+++ components/Manager/Movies/MovieListCard.jsx
@@ -1,14 +1,20 @@
+import { ethers } from "ethers";
+import Link from "next/link";
 import React from "react";
 import { Paragraph } from "../../shared/Texts";
 
 const MovieListCard = ({ movie }) => {
   return (
     <Link
-      href={"/manager/region/" + region + "/" + parseInt(cinema.hex)}
+      href={"/manager/movies/" + parseInt(movie.movieId.hex)}
       className="w-5/6 h-16 flex justify-evenly items-center rounded-md bg-slate-200 shadow-md p-2"
     >
       <div className="w-3/6 flex justify-center items-center gap-4">
-        <Paragraph text={movie} style="medium" />
+        <Paragraph
+          text={ethers.utils.parseBytes32String(movie.title)}
+          style="medium"
+          size="sm"
+        />
       </div>
       <div className="w-2/6">
         <p className="font-poppins text-xs text-center"></p>
