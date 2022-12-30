e4850ff37a2b2f249bbc8aeb1ec34bd27933fb5e
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-30 23:33:20 +0700

    Removed unused variable and fixed the mutation function

--- components/reactQuery/mutations/Movie/addMovie.js
+++ components/reactQuery/mutations/Movie/addMovie.js
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
 import React from "react";
 import { moviesContract } from "../../../../hooks/useContract";
+import useMetamask from "../../../../hooks/useMetamask";
 import { useLoading, useToast } from "../../../../store/stores";
 import mutation from "../mutation";
 
@@ -9,12 +10,12 @@ const addMovieSides = () => {
   const [toastSuccess, toastError] = useToast();
   return {
     onMutate: () => {
-      setLoadingText("Adding " + selectedMovies.length + " Movies");
+      setLoadingText("Adding New Movies");
       setLoading(true);
     },
     onSuccess: () => {
       setLoading(false);
-      toastSuccess("Successfully added " + selectedMovies.length + " movies");
+      toastSuccess("Successfully added new movies");
     },
     onError: (error) => {
       setLoading(false);
@@ -23,19 +24,19 @@ const addMovieSides = () => {
   };
 };
 
-const addMovie = ({ movieId, title, duration }) => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+export const addMovie = ({ movieId, title, duration }) => {
   const contract = moviesContract({ read: false });
-  const addMovies = mutation(async () => {
+  const { mutate } = mutation(async (event) => {
+    event.preventDefault();
+    const provider = useMetamask();
+    const titleBytes32 = ethers.utils.formatBytes32String(title);
     const transaction = await contract.addMovies(
       [movieId],
-      [title],
+      [titleBytes32],
       [duration]
     );
     return await provider.waitForTransaction(transaction.hash);
   }, addMovieSides());
 
-  return addMovies;
+  return mutate;
 };
-
-export default addMovie;
