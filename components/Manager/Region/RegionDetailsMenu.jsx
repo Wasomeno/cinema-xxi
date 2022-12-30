4d5e115a9427af10825550e1a0013499296e4806
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-30 23:32:00 +0700

    Added region props and inserted it in the link href

--- components/Manager/Region/RegionDetailsMenu.jsx
+++ components/Manager/Region/RegionDetailsMenu.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import AnimatedContainer from "../../AnimatedContainer";
 import Plus from "../../Icons/Plus";
 
-const RegionDetailsMenu = () => {
+export const RegionDetailsMenu = ({ region }) => {
   return (
     <AnimatedContainer className="flex flex-col justify-center items-center h-16 w-40 p-2 bg-slate-200 rounded-md z-20 shadow-md absolute right-16 top-10">
       <Link
-        href={"/manager/cinema/add"}
+        href={"/manager/region/" + region + "/add/cinema"}
         className="text-sm flex justify-evenly items-center h-full w-full"
       >
         <Plus />
@@ -16,5 +16,3 @@ const RegionDetailsMenu = () => {
     </AnimatedContainer>
   );
 };
-
-export default RegionDetailsMenu;
