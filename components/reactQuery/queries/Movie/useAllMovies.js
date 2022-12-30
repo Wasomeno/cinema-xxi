0dcb3d4b153d621be3202be79e1e6b76f50ef506
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-30 23:34:40 +0700

    Added contract.getMovieDetails so it can returns the movie titles not just the ids

--- components/reactQuery/queries/Movie/useAllMovies.js
+++ components/reactQuery/queries/Movie/useAllMovies.js
@@ -3,6 +3,12 @@ import { query } from "../../query";
 
 export const useAllMovies = async () => {
   const contract = moviesContract({ read: true });
-  const result = await contract.getMovies();
-  return result;
+  const movieIds = await contract.getMovies();
+  const movies = await Promise.all(
+    movieIds.map(async (movie) => {
+      const { title, duration } = await contract.getMovieDetails(movie);
+      return { movieId: movie, title: title, duration: duration };
+    })
+  );
+  return JSON.parse(JSON.stringify(movies));
 };
