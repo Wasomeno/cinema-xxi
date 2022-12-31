0cd0c5a24a6b613f713b18018716f551f2fbba1b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:16:10 +0700

    Added AdminManagerMenu component

---
+++ components/Manager/Admins/AdminManagerMenu.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import AnimatedContainer from "../../AnimatedContainer";
+import Plus from "../../Icons/Plus";
+import { Paragraph } from "../../shared/Texts";
+
+const AdminManagerMenu = ({ regionId, cinemaId }) => {
+  return (
+    <AnimatedContainer className="flex flex-col gap-2 justify-center items-center w-40 p-2 bg-slate-200 rounded-md z-20 shadow-md absolute right-16 top-10">
+      <Link
+        href={"/manager/region/" + regionId + "/" + cinemaId + "/admins/add"}
+        className="text-sm flex justify-evenly items-center h-full w-full p-1"
+      >
+        <div className="w-1/6">
+          <Plus size="4" color="grey" />
+        </div>
+        <div className="w-4/6">
+          <Paragraph text="Add Admin" size="xs" />
+        </div>
+      </Link>
+    </AnimatedContainer>
+  );
+};
+
+export default AdminManagerMenu;
