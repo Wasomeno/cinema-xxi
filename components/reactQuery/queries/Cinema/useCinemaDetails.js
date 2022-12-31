d7af6e988a571cdc3fbc740b2bb45715ab008b7f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:18:30 +0700

    Added additional results data( transactionsAmount & admins)

--- components/reactQuery/queries/Cinema/useCinemaDetails.js
+++ components/reactQuery/queries/Cinema/useCinemaDetails.js
@@ -1,19 +1,26 @@
 import { ethers } from "ethers";
-import { cinemaContract } from "../../../../hooks/useContract";
-import { query } from "../../query";
+import { cinemaContract, rolesContract } from "../../../../hooks/useContract";
+import { useCinemaTransactions } from "./useCinemaTransactions";
 
 export const useCinemaDetails = async ({ region, cinema }) => {
-  const contract = cinemaContract({ read: true });
-  const cinemaDetails = await contract.getCinemaDetails(region, cinema);
+  const contractCinema = cinemaContract({ read: true });
+  const contractRoles = rolesContract({ read: true });
+  const cinemaDetails = await contractCinema.getCinemaDetails(region, cinema);
+  const cinemaAdmins = await contractRoles.getCinemaAdmins(region, cinema);
+  const cinemaTransactions = await useCinemaTransactions({
+    region: region,
+    cinema: cinema,
+  });
+
   return JSON.parse(
     JSON.stringify({
       name: ethers.utils.parseBytes32String(cinemaDetails.name),
       studiosAmount: cinemaDetails.studiosAmount,
       moviesAmount: cinemaDetails.moviesAmount,
       showTimesAmount: cinemaDetails.showTimesAmount,
-      showTimes: cinemaDetails.showTimes,
-      studioShowTimes: cinemaDetails.studioShowTimes,
       studioCapacities: cinemaDetails.studiosCapacities,
+      transactionsAmount: cinemaTransactions.transactionsAmount,
+      admins: cinemaAdmins,
     })
   );
 };
