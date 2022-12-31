8459661c623531bbd2a2298a958241eb118c67dc
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:12:48 +0700

    Added a loader when isConnecting true

--- components/NotConnected.js
+++ components/NotConnected.js
@@ -1,8 +1,12 @@
-import React from "react";
-import { useConnect } from "wagmi";
+import React, { useEffect } from "react";
+import MoonLoader from "react-spinners/MoonLoader";
+import { useUserDetails } from "../hooks/useUserDetails";
+import { useAccount, useConnect } from "wagmi";
 
 const NotConnected = () => {
   const { connect, connectors } = useConnect();
+  const { isConnecting } = useUserDetails();
+
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
       <div className="text-center">
@@ -11,14 +15,18 @@ const NotConnected = () => {
         </h1>
         <p className="font-sans text-base">You need to connect your wallet</p>
       </div>
-      <div className="w-3/6 m-3 text-center">
+      <div className="w-2/6 m-3 text-center">
         {connectors.map((connector, index) => (
           <button
             key={index}
-            className="bg-slate-100 p-2 font-poppins font-medium text-sm w-4/6 rounded-xl shadow-md transition duration-300 ease-in-out hover:bg-black hover:text-white"
-            onClick={connect({ connector: connector })}
+            className="flex justify-center items-center bg-slate-100 p-2 font-poppins font-medium text-sm w-full rounded-xl shadow-md transition duration-300 ease-in-out hover:bg-black hover:text-white"
+            onClick={() => connect({ connector: connector })}
           >
-            Connect
+            {isConnecting ? (
+              <MoonLoader size="15" color="white" loading={isConnecting} />
+            ) : (
+              "Connect"
+            )}
           </button>
         ))}
       </div>
