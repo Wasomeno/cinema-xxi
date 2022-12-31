f64e6e9e83737a5006e56816a67091ac62095c00
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:15:41 +0700

    Added AdminListCard component to display cinema admin's address

---
+++ components/Manager/Admins/AdminListCard.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Paragraph } from "../../shared/Texts";
+
+const AdminListCard = ({ address }) => {
+  return (
+    <div className="w-4/6 flex justify-center items-center h-12 bg-slate-400 p-2 rounded-md">
+      <div className="text-center">
+        <Paragraph
+          text={address.slice(0, 12) + "...."}
+          size="sm"
+          style="medium"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AdminListCard;
