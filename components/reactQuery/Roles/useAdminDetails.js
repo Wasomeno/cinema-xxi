3631a65fd9e546b91a63667f51a96ece2ac8bafe
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:19:38 +0700

    Moved into Roles directory in reactQuery/queries directory

--- components/reactQuery/Roles/useAdminDetails.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { rolesContract } from "../../../hooks/useContract";
-import { query } from "../query";
-
-export const useAdminDetails = ({ user }) => {
-  const contract = rolesContract({ read: true });
-  const result = query(["adminDetails", user], contract.adminToDetails(user));
-  return result;
-};
