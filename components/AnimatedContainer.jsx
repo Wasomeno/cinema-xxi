56592d6e0bde1858c5df9e87de05c2c1a7d482d5
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2022-12-31 23:15:52 +0700

    Added onClick props

--- components/AnimatedContainer.jsx
+++ components/AnimatedContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-const AnimatedContainer = ({ className, children }) => {
+const AnimatedContainer = ({ className, onClick, children }) => {
   return (
     <motion.div
+      onClick={onClick}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.25, ease: "easeInOut" }}
