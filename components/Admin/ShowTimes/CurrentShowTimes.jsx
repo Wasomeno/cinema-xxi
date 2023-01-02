e782ae982f6b960db84ad89c0f398de058054e91
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-02 23:05:33 +0700

    Replaced adminDetails props with region & cinema, added react query for useShowTimes

--- components/Admin/ShowTimes/CurrentShowTimes.jsx
+++ components/Admin/ShowTimes/CurrentShowTimes.jsx
@@ -1,30 +1,36 @@
+import { query } from "@/components/reactQuery/query";
+import { Paragraph, Subtitle } from "@/components/shared/Texts";
 import React from "react";
 import { MoonLoader } from "react-spinners";
 import { useShowTimes } from "../../reactQuery/queries/Cinema/useShowTimes";
 
-const CurrentShowTimes = ({ adminDetails }) => {
-  const showTimes = useShowTimes({
-    cinema: adminDetails.cinema,
-    region: adminDetails.region,
+const CurrentShowTimes = ({ region, cinema }) => {
+  const showTimes = query({
+    queryKey: ["cinemaShowTimes", cinema],
+    queryFunction: async () =>
+      useShowTimes({
+        cinema: cinema,
+        region: region,
+      }),
   });
 
   return (
-    <div className="bg-slate-200 shadow-lg rounded-lg w-5/6 h-40">
-      <h1 className="font-poppins m-2 font-medium p-2 text-center">
-        Current Show Times
-      </h1>
-      <div className="flex items-center justify-center gap-2">
-        {showTimes.isLoading && (
-          <div className="flex flex-col justify-center items-center">
-            <p className="font-poppins m-2 my-3">Fetching Show Times</p>
-            <MoonLoader
-              loading={showTimes.isLoading}
-              size={25}
-              color={"black"}
-            />
-          </div>
-        )}
-        {!showTimes.isLoading &&
+    <div className="flex flex-col items-center justify-start gap-2 p-2">
+      {showTimes.isLoading && (
+        <div className="flex flex-col justify-center items-center">
+          <Paragraph text="Fetching Showtimes" size="sm" margin="4" />
+          <MoonLoader loading={showTimes.isLoading} size={25} color={"black"} />
+        </div>
+      )}
+      {!showTimes.isLoading &&
+        (showTimes.data.length < 1 ? (
+          <Paragraph
+            text="No Active Showtimes"
+            margin="4"
+            style="medium"
+            size="sm"
+          />
+        ) : (
           showTimes.data.map((showtime, index) => (
             <div
               key={index}
@@ -32,8 +38,8 @@ const CurrentShowTimes = ({ adminDetails }) => {
             >
               {parseInt(showtime)}
             </div>
-          ))}
-      </div>
+          ))
+        ))}
     </div>
   );
 };
