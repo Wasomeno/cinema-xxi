9f9de16d0a840883e47647f3644b7f29247e8649
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-02 22:57:58 +0700

    Removed unused hooks and changed adminDetails with cinemaAdminStatus

--- components/Admin/AdminLayout.js
+++ components/Admin/AdminLayout.js
@@ -3,57 +3,39 @@ import { appContext } from "../../context/AppContext";
 import { MoonLoader } from "react-spinners";
 import AdminNav from "./AdminNav";
 import { useRouter } from "next/router";
-import { useAdminDetails } from "../reactQuery/Roles/useAdminDetails";
+import { useAdminDetails } from "../reactQuery/queries/Roles/useAdminDetails";
+import { useUserDetails } from "hooks/useUserDetails";
+import { useCinemaAdminStatus } from "../reactQuery/queries/Roles/useCinemaAdminStatus";
+import { Paragraph } from "../shared/Texts";
+import NotValidAdmin from "./NotValidAdmin";
 
 const AdminLayout = ({ children }) => {
-  const [width, setWidth] = useState(0);
-  const router = useRouter();
-  const user = useContext(appContext).account[0];
-  const screenSize = useRef();
-  const setAdminDetails = useContext(appContext).setAdminDetails;
-  const adminDetails = useAdminDetails({ user: user });
+  const { user } = useUserDetails();
+  const cinemaAdminStatus = useCinemaAdminStatus(1, 1, user);
 
-  useEffect(() => {
-    if (!adminDetails.isLoading) {
-      setAdminDetails({
-        region: adminDetails.data.region,
-        cinema: adminDetails.data.cinema,
-      });
-      setWidth(screenSize.current.clientWidth);
-      screenSize.current.addEventListener("resize", () =>
-        setWidth(screenSize.current.clientWidth)
-      );
-    }
-  }, [adminDetails.isLoading, width]);
-
-  return adminDetails.isLoading ? (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <p className="font-poppins text-xl font-medium m-2 my-4">
-        Fetching Details
-      </p>
-      <MoonLoader loading={adminDetails.isLoading} size={50} color={"black"} />
-    </div>
-  ) : adminDetails.data.region !== 0 ? (
-    <main
-      ref={screenSize}
-      className="h-screen flex-col items-center justify-evenly relative"
-    >
-      {children}
-      <div className="h-20 w-full">
-        <AdminNav width={width} />
-      </div>
-    </main>
-  ) : (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <p className="font-poppins text-xl font-medium m-2 my-4">
-        You are not an Admin
-      </p>
-      <button
-        className="bg-slate-100 p-2 font-poppins font-medium text-lg w-1/6 rounded-xl shadow-md transition duration-300 ease-in-out hover:bg-black hover:text-white"
-        onClick={() => router.push("/app")}
-      >
-        Back to App
-      </button>
+  return (
+    <div className="flex flex-col justify-center items-center">
+      {cinemaAdminStatus.isLoading ? (
+        <div className="w-screen h-screen flex flex-col justify-center items-center gap-4">
+          <Paragraph text="Fetching Details" size="sm" style="medium" />
+          <MoonLoader
+            loading={cinemaAdminStatus.isLoading}
+            size="30px"
+            color="black"
+          />
+        </div>
+      ) : (
+        <main className="w-full h-full relative">
+          {cinemaAdminStatus.data ? (
+            <>
+              {children}
+              <AdminNav width={500} />
+            </>
+          ) : (
+            <NotValidAdmin />
+          )}
+        </main>
+      )}
     </div>
   );
 };
