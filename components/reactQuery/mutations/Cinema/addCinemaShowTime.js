d8e47711fba35b151d5bf4d89e116bc6dfb9fd2a
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-02 23:07:13 +0700

    Replaced addCinemaShowTimeSides with createSideEffects

--- components/reactQuery/mutations/Cinema/addCinemaShowTime.js
+++ components/reactQuery/mutations/Cinema/addCinemaShowTime.js
@@ -1,44 +1,20 @@
 import { ethers } from "ethers";
 import { cinemaContract } from "../../../../hooks/useContract";
-import { useLoading, useToast } from "../../../../store/stores";
-import { invalidateQueries } from "../../query";
+import { createSideEffects } from "../createSideEffects";
 import mutation from "../mutation";
 
-const addCinemaShowTimeSides = () => {
-  const [setLoading, setLoadingText] = useLoading();
-  const [toastSuccess, toastError] = useToast();
-  const sideEffects = {
-    onMutate: () => {
-      setLoadingText("Adding new show time");
-      setLoading(true);
-    },
-    onError: (error) => {
-      setLoading(false);
-      toastError(error.reason);
-    },
-    onSuccess: () => {
-      setLoading(false);
-      invalidateQueries(["cinemaShowTimes"]);
-      toastSuccess("Show time successfully added");
-    },
-  };
-
-  return sideEffects;
-};
-
-export const addCinemaShowTime = ({ adminDetails, hour, minutes }) => {
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
-  const contract = cinemaContract();
+export const addCinemaShowTime = ({ region, cinema, hour, minutes }) => {
+  const contract = cinemaContract({ read: false });
   const timeInSeconds = hour * 3600 + minutes * 60;
-  const { mutate } = mutation(async () => {
-    const addShowTime = await contract.addShowTime(
-      adminDetails.region,
-      adminDetails.cinema,
-      timeInSeconds
-    );
+  const { mutate } = mutation(async (event) => {
+    event.preventDefault();
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const addShowTime = await contract.addShowTimes(region, cinema, [
+      timeInSeconds,
+    ]);
     const waitTransaction = await provider.waitForTransaction(addShowTime.hash);
     return waitTransaction;
-  }, addCinemaShowTimeSides());
+  }, createSideEffects({ context: "add", object: "showtime" }));
 
   return mutate;
 };
