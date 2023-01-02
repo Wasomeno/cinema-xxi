007881fc84fa1096a6205a0c4a6a78cdc57d4f63
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-02 23:08:10 +0700

    Added useSuperAdminStatus for getting the status of super admin (Manager)

---
+++ components/reactQuery/queries/Roles/useSuperAdminStatus.js
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { rolesContract } from "../../../../hooks/useContract";
+
+export const useSuperAdminStatus = (address) => {
+  const contract = rolesContract({ read: false });
+  const result = useQuery(["superAdminStatus", address], async () => {
+    return await contract.superAdminStatus(address);
+  });
+  return result;
+};
