6670552460b1edc79332466068e6021408014932
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-08 19:49:52 +0700

    Changed p html tag with Paragraph component

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -3,6 +3,7 @@ import { useLoadingDetails } from "../store/stores";
 import { MoonLoader } from "react-spinners";
 import { AnimatePresence } from "framer-motion";
 import AnimatedContainer from "./AnimatedContainer";
+import { Paragraph } from "./shared/Texts";
 
 const Loading = () => {
   const [loading, loadingText] = useLoadingDetails();
@@ -13,13 +14,14 @@ const Loading = () => {
         <>
           <AnimatedContainer className="fixed bg-slate-700 bg-opacity-80 w-screen h-screen top-0 z-20" />
           <AnimatedContainer className="fixed w-64 h-72 rounded-lg bg-slate-100 shadow-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center justify-center gap-5 z-30">
-            <p className="font-poppins font-medium text-lg text-center">
-              {loadingText}
-            </p>
-            <MoonLoader loading={loading} size={40} color={"black"} />
-            <p className="font-poppins text-sm text-center">
-              Confirm the transaction in your wallet
-            </p>
+            <Paragraph text={loadingText} size="sm" style="medium" />
+            <MoonLoader loading={loading} size="30px" color="black" />
+            <div className="text-center p-2">
+              <Paragraph
+                text="Confirm the transaction in your wallet"
+                size="sm"
+              />
+            </div>
           </AnimatedContainer>
         </>
       )}
