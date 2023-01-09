304539d8ea67c01242546ead22a0750de6d67d69
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-09 21:56:00 +0700

    Implemented query function instead of async await

--- components/reactQuery/queries/Cinema/useCinemaDetails.js
+++ components/reactQuery/queries/Cinema/useCinemaDetails.js
@@ -1,26 +1,35 @@
-import { ethers } from "ethers";
+import { parseBytes32String } from "ethers/lib/utils.js";
 import { cinemaContract, rolesContract } from "../../../../hooks/useContract";
+import { query } from "../../query";
 import { useCinemaTransactions } from "./useCinemaTransactions";
 
-export const useCinemaDetails = async ({ region, cinema }) => {
+export const useCinemaDetails = ({ region, cinema }) => {
   const contractCinema = cinemaContract({ read: true });
   const contractRoles = rolesContract({ read: true });
-  const cinemaDetails = await contractCinema.getCinemaDetails(region, cinema);
-  const cinemaAdmins = await contractRoles.getCinemaAdmins(region, cinema);
-  const cinemaTransactions = await useCinemaTransactions({
-    region: region,
-    cinema: cinema,
-  });
+  const details = query({
+    queryKey: ["cinemaDetails", region, cinema],
+    queryFunction: async () => {
+      const cinemaDetails = await contractCinema.getCinemaDetails(
+        region,
+        cinema
+      );
+      const cinemaAdmins = await contractRoles.getCinemaAdmins(region, cinema);
+      const cinemaTransactions = await useCinemaTransactions({
+        region: region,
+        cinema: cinema,
+      });
 
-  return JSON.parse(
-    JSON.stringify({
-      name: ethers.utils.parseBytes32String(cinemaDetails.name),
-      studiosAmount: cinemaDetails.studiosAmount,
-      moviesAmount: cinemaDetails.moviesAmount,
-      showTimesAmount: cinemaDetails.showTimesAmount,
-      studioCapacities: cinemaDetails.studiosCapacities,
-      transactionsAmount: cinemaTransactions.transactionsAmount,
-      admins: cinemaAdmins,
-    })
-  );
+      return {
+        name: parseBytes32String(cinemaDetails.name),
+        studioAmount: parseInt(cinemaDetails.studiosAmount),
+        moviesAmount: parseInt(cinemaDetails.moviesAmount),
+        showTimesAmount: parseInt(cinemaDetails.showTimesAmount),
+        studioCapacities: parseInt(cinemaDetails.studiosCapacities),
+        transactionsAmount: parseInt(cinemaTransactions.transactionsAmount),
+        // capacityTotal: capacityTotal,
+        admins: cinemaAdmins,
+      };
+    },
+  });
+  return details;
 };
