210fd1424a27684f6fb2a708b5607b4f78d4bc2b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-09 22:02:50 +0700

    Removed deselectMovie function & replaced button with MovieListCard

--- modules/adminPages/moviesPage/SelectedMovies.jsx
+++ modules/adminPages/moviesPage/SelectedMovies.jsx
@@ -1,15 +1,9 @@
 import { Paragraph } from "@/components/shared/Texts";
 import { parseBytes32String } from "ethers/lib/utils";
+import MovieListCard from "@/components/MovieListCard";
 import React from "react";
 
-const SelectedMovies = ({ selectedMovies, setSelectedMovies }) => {
-  const deselectMovie = (selected) => {
-    setSelectedMovies((currentSelected) =>
-      currentSelected.filter((movie, index) => {
-        return movie.movieId !== selected;
-      })
-    );
-  };
+const SelectedMovies = ({ selectedMovies, deselectMovie }) => {
   return (
     <div className="p-2">
       <div className="mt-2">
@@ -22,13 +16,12 @@ const SelectedMovies = ({ selectedMovies, setSelectedMovies }) => {
       ) : (
         <div className="flex flex-col gap-3 items-center h-4/6 overflow-y-scroll m-2 p-2">
           {selectedMovies.map((movie, index) => (
-            <button
+            <MovieListCard
               key={index}
+              movieTitle={movie}
+              clickable
               onClick={() => deselectMovie(movie.movieId)}
-              className="font-poppins font-normal text-center text-sm p-2 shadow-md bg-slate-200 rounded-lg w-5/6 transition duration-300 ease-in-out hover:bg-red-200"
-            >
-              {parseBytes32String(movie.title)}
-            </button>
+            />
           ))}
         </div>
       )}
