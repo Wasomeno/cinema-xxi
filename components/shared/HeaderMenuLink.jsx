d03e4f9a8cb832c94424c32c5599a42d685e6714
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-09 21:57:21 +0700

    Added text-center class

--- components/shared/HeaderMenuLink.jsx
+++ components/shared/HeaderMenuLink.jsx
@@ -5,19 +5,19 @@ import Minus from "../Icons/Minus";
 import Plus from "../Icons/Plus";
 
 const contextIcons = {
-  add: <Plus size="4" />,
-  delete: <Minus size="4" />,
-  list: <Cinema size="4" />,
+  add: <Plus size="3" />,
+  delete: <Minus size="3" />,
+  list: <Cinema size="3" />,
 };
 
 const HeaderMenuLink = ({ href, context, text }) => {
   return (
     <Link
       href={href}
-      className="text-sm flex justify-evenly items-center h-full w-full"
+      className="text-sm flex justify-evenly items-center h-full w-full p-1"
     >
       <div className="w-1/6">{contextIcons[context]}</div>
-      <div className="w-4/6">
+      <div className="w-4/6 text-center">
         <p className="font-poppins text-xs">{text}</p>
       </div>
     </Link>
