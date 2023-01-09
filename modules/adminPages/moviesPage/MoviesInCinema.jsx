c20e64fe485cd5ea608df56174bde4844f3f417d
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-09 22:01:09 +0700

    Replaced Paragraph with Subtitle component and implemented DataContainer component

--- modules/adminPages/moviesPage/MoviesInCinema.jsx
+++ modules/adminPages/moviesPage/MoviesInCinema.jsx
@@ -1,7 +1,9 @@
-import { Paragraph } from "@/components/shared/Texts";
+import { Paragraph, Subtitle } from "@/components/shared/Texts";
 import { parseBytes32String } from "ethers/lib/utils.js";
 import { MoonLoader } from "react-spinners";
 import { useCinemaMovies } from "@/components/reactQuery/queries/Movie/useCinemaMovies";
+import DataContainer from "@/components/DataContainer";
+import MovieListCard from "@/components/MovieListCard";
 
 const MoviesInCinema = ({ region, cinema }) => {
   const fetchedCinemaMovies = useCinemaMovies({
@@ -11,33 +13,27 @@ const MoviesInCinema = ({ region, cinema }) => {
 
   return (
     <div className="w-full p-2">
-      <div className="">
-        <Paragraph text="List of Movies" size="sm" style="medium" />
+      <div className="my-2">
+        <Subtitle text="List of Movies" size="sm" />
       </div>
-      <div className="flex flex-col gap-3 items-center h-4/6 overflow-y-scroll p-2">
-        {fetchedCinemaMovies.isLoading ? (
-          <>
-            <Paragraph text="Fetching Movies" size="xs" style="medium" />
-            <MoonLoader
-              loading={fetchedCinemaMovies.isLoading}
-              size={25}
-              color={"black"}
-            />
-          </>
-        ) : fetchedCinemaMovies.data.length < 1 ? (
+      <DataContainer
+        className="flex flex-col gap-3 items-center h-4/6 overflow-y-scroll p-2"
+        object="Movies"
+        loading={fetchedCinemaMovies.isLoading}
+      >
+        {fetchedCinemaMovies.data?.length < 1 ? (
           <Paragraph text="No Active Movies" size="sm" />
         ) : (
-          fetchedCinemaMovies.data.map((movie, index) => (
-            <button
+          fetchedCinemaMovies.data?.map((movie, index) => (
+            <MovieListCard
               key={index}
+              movieTitle={movie.movieTitle}
+              clickable
               onClick={() => selectMovie(index)}
-              className="font-poppins font-normal text-center text-sm p-2 shadow-md bg-slate-200 rounded-lg w-5/6 transition duration-300 ease-in-out hover:bg-white"
-            >
-              {parseBytes32String(movie)}
-            </button>
+            />
           ))
         )}
-      </div>
+      </DataContainer>
     </div>
   );
 };
