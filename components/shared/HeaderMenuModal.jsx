a5c47a64e6d7012218923c12e93d057be429045d
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-09 21:57:59 +0700

    Replaced z-20 with z-10

--- components/shared/HeaderMenuModal.jsx
+++ components/shared/HeaderMenuModal.jsx
@@ -3,7 +3,7 @@ import AnimatedContainer from "../AnimatedContainer";
 
 const HeaderMenuModal = ({ children }) => {
   return (
-    <AnimatedContainer className="flex flex-col justify-center items-center h-16 w-40 p-2 bg-slate-200 rounded-md z-20 shadow-md absolute right-16 top-10">
+    <AnimatedContainer className="flex flex-col justify-center items-center gap-3 w-40 p-3 bg-slate-200 rounded-md z-10 shadow-md absolute right-16 top-10">
       {children}
     </AnimatedContainer>
   );
