405aa32cf87f4a7e33aed483bfe4e6e4951eb845
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 20:52:13 +0700

    Replaced svg with Icon component

--- components/AppNav.jsx
+++ components/AppNav.jsx
@@ -1,47 +1,21 @@
 import React from "react";
 import { useRouter } from "next/router";
+import { House } from "./Icons/House";
+import CheckMark from "./Icons/Checkmark";
+import Ticket from "./Icons/Ticket";
 
 const AppNav = () => {
   const router = useRouter();
   return (
-    <div className="fixed w-full bg-slate-300 flex items-center justify-evenly bottom-0">
-      <div
-        className="flex flex-col items-center w-3/6 p-2"
-        onClick={() => router.push("/app")}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-5 h-5"
-        >
-          <path d="M19.006 3.705a.75.75 0 00-.512-1.41L6 6.838V3a.75.75 0 00-.75-.75h-1.5A.75.75 0 003 3v4.93l-1.006.365a.75.75 0 00.512 1.41l16.5-6z" />
-          <path
-            fillRule="evenodd"
-            d="M3.019 11.115L18 5.667V9.09l4.006 1.456a.75.75 0 11-.512 1.41l-.494-.18v8.475h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3v-9.129l.019-.006zM18 20.25v-9.565l1.5.545v9.02H18zm-9-6a.75.75 0 00-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75H9z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <p className="font-poppins text-xs mt-1">Home</p>
+    <div className="fixed w-full bg-slate-200 h-14 flex items-center justify-evenly bottom-0 z-10">
+      <div className="w-3/6 p-3" onClick={() => router.push("/app")}>
+        <House size="5" />
       </div>
       <div
         className="flex flex-col items-center w-3/6 p-2"
         onClick={() => router.push("/app/profile")}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          className="w-5 h-5"
-        >
-          <path d="M19.006 3.705a.75.75 0 00-.512-1.41L6 6.838V3a.75.75 0 00-.75-.75h-1.5A.75.75 0 003 3v4.93l-1.006.365a.75.75 0 00.512 1.41l16.5-6z" />
-          <path
-            fillRule="evenodd"
-            d="M3.019 11.115L18 5.667V9.09l4.006 1.456a.75.75 0 11-.512 1.41l-.494-.18v8.475h.75a.75.75 0 010 1.5H2.25a.75.75 0 010-1.5H3v-9.129l.019-.006zM18 20.25v-9.565l1.5.545v9.02H18zm-9-6a.75.75 0 00-.75.75v4.5c0 .414.336.75.75.75h3a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75H9z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <p className="font-poppins text-xs mt-1">Home</p>
+        <Ticket size="5" color="black" />
       </div>
     </div>
   );
