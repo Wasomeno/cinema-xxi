daa5341620b545cf1021aa1c005bd4f6e285de81
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 20:56:39 +0700

    Renamed to useCinemaShowtimes

--- components/reactQuery/queries/Cinema/useShowTimes.js
+++
@@ -1,8 +0,0 @@
-import { cinemaContract } from "../../../../hooks/useContract";
-import { query } from "../../query";
-
-export const useShowTimes = async ({ region, cinema }) => {
-  const contract = cinemaContract({ read: true });
-  const result = await contract.getCinemaShowTimes(region, cinema);
-  return result;
-};
