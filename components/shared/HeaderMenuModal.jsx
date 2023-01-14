89d3cb728fac9cd18d2e23dd58286c5c384e0cc5
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 20:59:18 +0700

    Changed z-index to z-15

--- components/shared/HeaderMenuModal.jsx
+++ components/shared/HeaderMenuModal.jsx
@@ -3,7 +3,7 @@ import AnimatedContainer from "../AnimatedContainer";
 
 const HeaderMenuModal = ({ children }) => {
   return (
-    <AnimatedContainer className="flex flex-col justify-center items-center gap-3 w-40 p-3 bg-slate-200 rounded-md z-10 shadow-md absolute right-16 top-10">
+    <AnimatedContainer className="flex flex-col justify-center items-center gap-3 w-40 p-3 bg-slate-200 rounded-md z-15 shadow-md absolute right-16 top-10">
       {children}
     </AnimatedContainer>
   );
