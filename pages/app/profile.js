165617fc9983d0e3d1799276e40c0be4718779d1
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 21:06:16 +0700

    Added custom tab button for displaying active tickets & ticket history

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,12 +1,36 @@
+import AnimatedContainer from "@/components/AnimatedContainer";
+import { Paragraph, Title } from "@/components/shared/Texts";
+import useToggle from "hooks/useToggle";
 import React from "react";
 
 const profile = () => {
+  const [activeTab, toggleActiveTab] = useToggle(false);
   return (
-    <div className="w-full h-full rounded-xl">
-      <div className="flex items-center justify-center">
-        <h1 className="font-poppins text-3xl p-2 m-4">Profile</h1>
+    <AnimatedContainer className="p-4 w-full">
+      <div className="text-center mb-2">
+        <Title text="Ticket Details" />
       </div>
-    </div>
+      <div className="flex justify-center items-center my-2 rounded-lg shadow-md bg-slate-100">
+        <div
+          onClick={() => activeTab && toggleActiveTab()}
+          className={
+            "w-3/6 p-2 text-center rounded-l-lg transition duration-200 ease-in-out " +
+            (!activeTab && "bg-slate-400")
+          }
+        >
+          <Paragraph text="Active Tickets" size="xs" style="medium" />
+        </div>
+        <div
+          onClick={() => !activeTab && toggleActiveTab()}
+          className={
+            "w-3/6 p-2 text-center rounded-r-lg transition duration-200 ease-in-out " +
+            (activeTab && "bg-slate-400")
+          }
+        >
+          <Paragraph text="Ticket History" size="xs" style="medium" />
+        </div>
+      </div>
+    </AnimatedContainer>
   );
 };
 
