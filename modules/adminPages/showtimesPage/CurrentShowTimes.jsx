a24a59581dffd91f3eb297d39c72552664ae92cb
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 21:02:23 +0700

    Implemented DataContainer component

--- modules/adminPages/showtimesPage/CurrentShowTimes.jsx
+++ modules/adminPages/showtimesPage/CurrentShowTimes.jsx
@@ -1,46 +1,38 @@
-import { query } from "@/components/reactQuery/query";
 import { Paragraph, Subtitle } from "@/components/shared/Texts";
 import React from "react";
-import { MoonLoader } from "react-spinners";
-import { useShowTimes } from "@/components/reactQuery/queries/Cinema/useShowTimes";
+import { useCinemaShowTimes } from "@/components/reactQuery/queries/Cinema/useCinemaShowTimes";
+import DataContainer from "@/components/DataContainer";
 
 const CurrentShowTimes = ({ region, cinema }) => {
-  const showTimes = query({
-    queryKey: ["cinemaShowTimes", cinema],
-    queryFunction: async () =>
-      useShowTimes({
-        cinema: cinema,
-        region: region,
-      }),
+  const cinemaShowtimes = useCinemaShowTimes({
+    region: region,
+    cinema: cinema,
   });
 
   return (
-    <div className="flex flex-col items-center justify-start gap-2 p-2">
-      {showTimes.isLoading && (
-        <div className="flex flex-col justify-center items-center">
-          <Paragraph text="Fetching Showtimes" size="sm" margin="4" />
-          <MoonLoader loading={showTimes.isLoading} size={25} color={"black"} />
-        </div>
+    <DataContainer
+      className="flex flex-col items-center justify-start gap-4 p-2"
+      object="showtimes"
+      loading={cinemaShowtimes.isLoading}
+    >
+      {cinemaShowtimes.data?.length < 1 ? (
+        <Paragraph
+          text="No Active Showtimes"
+          margin="4"
+          style="medium"
+          size="sm"
+        />
+      ) : (
+        cinemaShowtimes.data?.map((showtime, index) => (
+          <div
+            key={index}
+            className="bg-slate-100 text-center shadow-md p-2 rounded-lg w-2/6"
+          >
+            {parseInt(showtime)}
+          </div>
+        ))
       )}
-      {!showTimes.isLoading &&
-        (showTimes.data.length < 1 ? (
-          <Paragraph
-            text="No Active Showtimes"
-            margin="4"
-            style="medium"
-            size="sm"
-          />
-        ) : (
-          showTimes.data.map((showtime, index) => (
-            <div
-              key={index}
-              className="bg-slate-100 text-center shadow-md p-2 rounded-lg w-2/6"
-            >
-              {parseInt(showtime)}
-            </div>
-          ))
-        ))}
-    </div>
+    </DataContainer>
   );
 };
 
