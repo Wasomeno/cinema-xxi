f75b556676a94ccc05fbd0160777e10ce95f0208
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 21:07:46 +0700

    Changed the returns value to region id,name, and cinemaAmount instead

--- components/reactQuery/queries/Region/useAllRegions.js
+++ components/reactQuery/queries/Region/useAllRegions.js
@@ -1,3 +1,4 @@
+import { parseBytes32String } from "ethers/lib/utils.js";
 import React from "react";
 import { regionContract } from "../../../../hooks/useContract";
 import { query } from "../../query";
@@ -6,7 +7,22 @@ export const useAllRegions = () => {
   const contract = regionContract({ read: true });
   const result = query({
     queryKey: ["allRegions"],
-    queryFunction: async () => await contract.getRegions(),
+    queryFunction: async () => {
+      const regionIds = await contract.getRegions();
+      const regions = Promise.all(
+        regionIds.map(async (regionId) => {
+          const { _name, _cinemasAmount } = await contract.getRegionsDetails(
+            regionId
+          );
+          return {
+            id: parseInt(regionId),
+            name: parseBytes32String(_name),
+            cinemaAmount: parseInt(_cinemasAmount),
+          };
+        })
+      );
+      return regions;
+    },
   });
   return result;
 };
