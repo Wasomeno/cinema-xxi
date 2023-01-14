1691f385d6519b97f9f2175733774a6a6a829cbe
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-01-14 20:58:52 +0700

    Parsed movieTitle

--- components/reactQuery/queries/Movie/useCinemaMovies.js
+++ components/reactQuery/queries/Movie/useCinemaMovies.js
@@ -1,3 +1,4 @@
+import { parseBytes32String } from "ethers/lib/utils.js";
 import React from "react";
 import { cinemaContract, moviesContract } from "../../../../hooks/useContract";
 import { query } from "../../query";
@@ -5,15 +6,17 @@ import { query } from "../../query";
 export const useCinemaMovies = ({ region, cinema }) => {
   const contractCinema = cinemaContract({ read: true });
   const contractMovies = moviesContract({ read: true });
-
   const result = query({
     queryKey: ["cinemaMovies", region, cinema],
     queryFunction: async () => {
       const movieIds = await contractCinema.getCinemaMovies(region, cinema);
       const moviesDetails = Promise.all(
         movieIds.map(async (movieId) => {
-          const details = await contractMovies.getMovieDetails(movieId);
-          return { movieId: movieId, movieTitle: details.title };
+          const { title } = await contractMovies.getMovieDetails(movieId);
+          return {
+            movieId: movieId,
+            movieTitle: parseBytes32String(title),
+          };
         })
       );
       return moviesDetails;
