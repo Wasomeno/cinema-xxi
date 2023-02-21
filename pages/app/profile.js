87f94c45240ea59c9e578cba188c0ceab8eed100
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-02-22 00:33:50 +0700

    Emptied profile page

--- pages/app/profile.js
+++ pages/app/profile.js
@@ -1,26 +1,7 @@
-import { AnimatePresence } from "framer-motion";
-import useToggle from "hooks/useToggle";
-import ActiveTicketList from "modules/userPages/profilePage/ActiveTicketList";
-import TicketHistory from "modules/userPages/profilePage/TicketHistory";
-import TicketPageTab from "modules/userPages/profilePage/TicketPageTab";
-import React from "react";
-
 import AnimatedContainer from "@/components/AnimatedContainer";
-import { Paragraph, Title } from "@/components/shared/Texts";
 
 const Profile = () => {
-  const [activeTab, toggleActiveTab] = useToggle(false);
-  return (
-    <AnimatedContainer className="w-full p-4">
-      <div className="mb-2 text-center">
-        <Title text="Ticket Details" />
-      </div>
-      <TicketPageTab activeTab={activeTab} toggleActiveTab={toggleActiveTab} />
-      <AnimatePresence>
-        {!activeTab ? <ActiveTicketList /> : <TicketHistory />}
-      </AnimatePresence>
-    </AnimatedContainer>
-  );
+  return <AnimatedContainer className="w-full p-4"></AnimatedContainer>;
 };
 
 export default Profile;
