73ae3732b93637f2a4646720c08d618487b027f7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-02-21 16:28:33 +0700

    Added simple 404 & 500 page

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 const FourZeroFour = () => {
-  return <div>index</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <div className="p-2">
+        <h1 className="font-inter text-4xl font-semibold tracking-wider lg:text-5xl">
+          404
+        </h1>
+      </div>
+      <div className="p-2">
+        <p className="font-poppins text-lg tracking-wide">Page Not Found</p>
+      </div>
+      <div>
+        <Link
+          href="/app"
+          className="font-poppins rounded-lg bg-slate-800 p-2 px-5 text-white"
+        >
+          Go to App
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FourZeroFour;
--- pages/500/index.jsx
+++ pages/500/index.jsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 const FiveHundred = () => {
-  return <div>index</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <div className="p-2">
+        <h1 className="font-inter text-4xl font-semibold tracking-wider lg:text-5xl">
+          500
+        </h1>
+      </div>
+      <div className="p-2">
+        <p className="font-poppins text-lg tracking-wide">Server Error</p>
+      </div>
+      <div>
+        <Link
+          href="/app"
+          className="font-poppins rounded-lg bg-slate-800 p-2 px-5 text-white"
+        >
+          Go to App
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default FiveHundred;
