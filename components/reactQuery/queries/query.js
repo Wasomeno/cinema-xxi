bf33596e64e2d683867ee909805cdee50ae1e692
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-03-23 03:45:36 +0700

    Added enabled condition

--- components/reactQuery/queries/query.js
+++ components/reactQuery/queries/query.js
@@ -1,9 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
-export const query = ({ queryKey, url }) => {
+export const query = ({ queryKey, url, enabledCondition }) => {
+  console.log(enabledCondition);
   // eslint-disable-next-line react-hooks/rules-of-hooks
-  const result = useQuery(queryKey, () =>
-    fetch(url, { method: "GET" }).then((result) => result.json())
+  const result = useQuery(
+    queryKey,
+    () => fetch(url, { method: "GET" }).then((result) => result.json()),
+    { enabled: enabledCondition }
   );
   return result;
 };
