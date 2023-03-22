c0acd3b7da3ec8b7a132b468b28c5bc43e126da7
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-03-23 04:32:58 +0700

    Added try & catch in GET method handler

--- pages/api/users/[userAddress]/index.js
+++ pages/api/users/[userAddress]/index.js
@@ -2,20 +2,26 @@ import { prisma } from "lib/prisma";
 
 export default async function userDetailsHandler(req, res) {
   if (req.method === "GET") {
-    const { userAddress } = req.query;
-    const userDetails = await prisma.user.findUnique({
-      where: { address: userAddress.toString() },
-      include: {
-        transactions: { include: { cinema: true, movie: true, region: true } },
-      },
-    });
-    const convertedDetails = {
-      ...userDetails,
-      transactions: userDetails.transactions.map((transaction) => ({
-        ...transaction,
-        showtime: transaction.showtime.toString(),
-      })),
-    };
-    res.status(200).json(convertedDetails);
+    try {
+      const { userAddress } = req.query;
+      const userDetails = await prisma.user.findUnique({
+        where: { address: userAddress.toString() },
+        include: {
+          transactions: {
+            include: { cinema: true, movie: true, region: true },
+          },
+        },
+      });
+      const convertedDetails = {
+        ...userDetails,
+        transactions: userDetails.transactions.map((transaction) => ({
+          ...transaction,
+          showtime: transaction.showtime.toString(),
+        })),
+      };
+      res.status(200).json(convertedDetails);
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
   }
 }
