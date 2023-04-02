1be2fbfc2f3f4a470ad850a8e443ea835b47272f
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-04-03 03:43:40 +0700

    Added ISR for manager region details page & manager cinema details page

--- pages/manager/region/[regionId]/[cinemaId]/index.jsx
+++ pages/manager/region/[regionId]/[cinemaId]/index.jsx
@@ -1,6 +1,48 @@
+import { prisma } from "lib/prisma";
 import { ManagerCinemaDetailsPage } from "modules/managerPages/cinemaPage/ManagerCinemaDetailsPage";
 
-const CinemaDetails = () => {
+export async function getStaticPaths() {
+  const regions = await prisma.region.findMany({ include: { cinema: true } });
+  const regionAndCinemaIds = regions.map((region) => {
+    return region.cinema.map((cinema) => ({
+      params: {
+        regionId: region.id.toString(),
+        cinemaId: cinema.id.toString(),
+      },
+    }));
+  });
+
+  return { paths: regionAndCinemaIds.flat(), fallback: "blocking" };
+}
+
+export async function getStaticProps({ params }) {
+  const cinemaDetails = await prisma.cinema.findUnique({
+    where: { id: parseInt(params.cinemaId) },
+    include: {
+      movie: true,
+      region: true,
+      showtimes: true,
+      studio: true,
+      transactions: true,
+    },
+  });
+  return {
+    props: {
+      cinemaDetails: {
+        ...cinemaDetails,
+        transactions: cinemaDetails.transactions.map((transaction) => ({
+          ...transaction,
+          showtime: transaction.showtime.toString(),
+          createdAt: transaction.createdAt.getSeconds(),
+        })),
+      },
+    },
+    revalidate: 30,
+  };
+}
+
+const CinemaDetails = ({ cinemaDetails }) => {
+  console.log(cinemaDetails);
   return <ManagerCinemaDetailsPage />;
 };
 
--- pages/manager/region/[regionId]/index.jsx
+++ pages/manager/region/[regionId]/index.jsx
@@ -1,6 +1,29 @@
+import { prisma } from "lib/prisma";
 import { ManagerRegionDetailsPage } from "modules/managerPages/regionsPage/ManagerRegionDetailsPage";
 
-const RegionDetails = () => {
+export async function getStaticPaths() {
+  const regions = await prisma.region.findMany();
+  const regionIdPaths = regions.map((region) => ({
+    params: {
+      regionId: region.id.toString(),
+    },
+  }));
+
+  return { paths: regionIdPaths, fallback: "blocking" };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const regionDetails = await prisma.region.findUnique({
+    where: { id: parseInt(params.regionId) },
+    include: { cinema: true },
+  });
+
+  return { props: { regionDetails: regionDetails }, revalidate: 30 };
+}
+
+const RegionDetails = ({ regionDetails }) => {
+  console.log(regionDetails);
   return <ManagerRegionDetailsPage />;
 };
 
