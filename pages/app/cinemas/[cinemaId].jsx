b853c745a97286bd2c83914aea601ac4abeeca15
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-04-08 03:11:45 +0700

    Implemented ISR for CinemMovies Page

--- pages/app/cinemas/[cinemaId].jsx
+++ pages/app/cinemas/[cinemaId].jsx
@@ -1,7 +1,31 @@
+import { prisma } from "lib/prisma";
 import { CinemaMoviesPage } from "modules/appPages/cinemaPages/CinemaMoviesPage";
 
-const CinemaMovies = () => {
-  return <CinemaMoviesPage />;
+export async function getStaticPaths() {
+  const cinemas = await prisma.cinema.findMany();
+  const cinemaPaths = cinemas.map((cinema) => ({
+    params: { cinemaId: cinema.id.toString() },
+  }));
+
+  return { paths: cinemaPaths, fallback: "blocking" };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const cinemaDetails = await prisma.cinema.findUnique({
+    where: { id: parseInt(params.cinemaId) },
+    include: {
+      movie: {
+        where: { cinema: { every: { id: parseInt(params.cinemaId) } } },
+      },
+    },
+  });
+
+  return { props: { cinemaDetails }, revalidate: 30 };
+}
+
+const CinemaMovies = ({ cinemaDetails }) => {
+  return <CinemaMoviesPage cinemaDetails={cinemaDetails} />;
 };
 
 export default CinemaMovies;
