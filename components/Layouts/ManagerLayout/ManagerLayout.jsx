a11a30b41511ee35a85644bcfd288a0a90c3164b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-04-08 02:59:48 +0700

    Removed functions & hooks that related to manager authentication using metamask

--- components/Layouts/ManagerLayout/ManagerLayout.jsx
+++ components/Layouts/ManagerLayout/ManagerLayout.jsx
@@ -1,12 +1,7 @@
-import { useUserConnectionDetails } from "hooks/useUserConnectionDetails";
 import { useViewport } from "hooks/useViewport";
 import { ManagerLoginPage } from "modules/managerPages/ManagerLoginPage";
 import dynamic from "next/dynamic";
-import { MoonLoader } from "react-spinners";
-
-import { useManagerStatus } from "../../reactQuery/queries/Manager/useManagerStatus";
-import { Paragraph } from "../../shared/Texts";
-import NotValidManager from "./NotValidManager";
+import { useSession } from "next-auth/react";
 
 const ManagerNavigation = dynamic(() =>
   import("../../Navigations/Manager/ManagerNavigation").then(
@@ -21,27 +16,10 @@ const ManagerNavigationMobile = dynamic(() =>
 );
 
 export const ManagerLayout = ({ children }) => {
-  const { user, isConnected } = useUserConnectionDetails();
-  const managerStatus = useManagerStatus({ address: user });
+  const { data: sessionData } = useSession();
   const viewport = useViewport();
 
-  if (!isConnected) return <ManagerLoginPage />;
-
-  if (managerStatus.isLoading && isConnected)
-    return (
-      <div className="flex h-screen w-screen flex-col items-center justify-center gap-4 bg-slate-50 bg-opacity-95 dark:bg-slate-800">
-        <Paragraph size="sm" style="medium">
-          Fetching Manager Status
-        </Paragraph>
-        <MoonLoader
-          loading={managerStatus.isLoading}
-          size="30px"
-          color="black"
-        />
-      </div>
-    );
-
-  if (!managerStatus.data && isConnected) return <NotValidManager />;
+  if (!sessionData) return <ManagerLoginPage />;
 
   return (
     <>
