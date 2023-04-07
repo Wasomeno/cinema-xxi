9663a7acca380494b4410aac11b5d2cca0faf99b
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-04-08 02:59:19 +0700

    Removed function & hooks that related to admin authentication with metamask

--- components/Layouts/AdminLayout/AdminLayout.jsx
+++ components/Layouts/AdminLayout/AdminLayout.jsx
@@ -1,15 +1,7 @@
-import { AdminDetailsContextProvider } from "context/AdminDetails/AdminDetailsContextProvider";
-import { useUserConnectionDetails } from "hooks/useUserConnectionDetails";
 import { useViewport } from "hooks/useViewport";
 import { AdminLoginPage } from "modules/adminPages/AdminLoginPage";
 import dynamic from "next/dynamic";
-
-import { useCinemaAdminDetails } from "../../reactQuery/queries/Admin/useCinemaAdminDetails";
-import { useCinemaAdminStatus } from "../../reactQuery/queries/Admin/useCinemaAdminStatus";
-
-const NotValidAdmin = dynamic(() => import("./NotValidAdmin"));
-
-const AdminStatusLoading = dynamic(() => import("./AdminStatusLoading"));
+import { useSession } from "next-auth/react";
 
 const AdminNavigation = dynamic(() =>
   import("../../Navigations/Admin/AdminNavigation").then(
@@ -24,22 +16,16 @@ const AdminNavigationMobile = dynamic(() =>
 );
 
 export const AdminLayout = ({ children }) => {
-  const { user, isConnected } = useUserConnectionDetails();
-  const cinemaAdminStatus = useCinemaAdminStatus({ address: user });
-  const adminDetails = useCinemaAdminDetails({ address: user });
+  const { data: sessionData } = useSession();
   const viewport = useViewport();
-
-  if (!isConnected) return <AdminLoginPage />;
-  if (cinemaAdminStatus.isLoading) return <AdminStatusLoading />;
-  if (!cinemaAdminStatus.data) return <NotValidAdmin />;
-
+  if (!sessionData) return <AdminLoginPage />;
   return (
-    <AdminDetailsContextProvider adminDetails={adminDetails.data}>
+    <>
       {viewport.width > 1024 && <AdminNavigation />}
       <main className="relative w-full overflow-y-scroll">{children}</main>
       {viewport.width < 1024 && viewport.height > 400 && (
         <AdminNavigationMobile />
       )}
-    </AdminDetailsContextProvider>
+    </>
   );
 };
