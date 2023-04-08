069580883eefc8e87b890df5ab4231d97c7e334e
Wasomeno <60634068+Wasomeno@users.noreply.github.com>
2023-04-08 16:37:30 +0700

    typo in NEXTAUTH_SECRET

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const env = {
   ROLES_CONTRACT_ADDRESS: "0x6986f8ac26ed37c46dd8c166450d9a0cc464ade7",
   TICKET_CONTRACT_ADDRESS: "0x16cee9a9dd6e65ce9ec600f1b2bd0a7bf99dc6ff",
-  NEXT_AUTH_SECRET: "5c3d24485bb838879e06f5bebca5652f",
+  NEXTAUTH_SECRET: "5c3d24485bb838879e06f5bebca5652f",
   // API_MOVIES: /
 };
 
