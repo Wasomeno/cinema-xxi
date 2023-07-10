7e6f78e3b38fd541a9e58482d5b8be1acf2177e7
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-10 19:32:18 +0700

    feat: add PUT route for updating selected movies

--- pages/api/movies/[movieId].js
+++ pages/api/movies/[movieId].js
@@ -4,8 +4,21 @@ export default async function movieDetailsHandler(req, res) {
   const { movieId } = req.query;
   if (req.method === "GET") {
     const movieDetails = await prisma.movie.findUnique({
-      where: { id: parseInt(movieId) },
+      where: { id: movieId },
     });
     res.status(200).json(movieDetails);
+  } else if (req.method === "PUT") {
+    const { title, duration, synopsis, casts } = req.body;
+    try {
+      await prisma.movie.update({
+        where: { id: parseInt(movieId) },
+        data: { title, synopsis, casts, duration: parseInt(duration) },
+      });
+      res
+        .status(200)
+        .json({ code: "200", message: "Successfully Updated Movie" });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
   }
 }
