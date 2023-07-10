15ba7b770dae98025ac0d2a049b1bddfc121ea0b
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-10 19:45:38 +0700

    refactor: remove toggleShowMenu props and the section for it

--- components/Headers/AdminSubHeader.jsx
+++ components/Headers/AdminSubHeader.jsx
@@ -1,6 +1,4 @@
-import EllipsisVertical from "../Icons/EllipsisVertical";
-
-export const AdminSubHeader = ({ toggleShowMenu, children }) => {
+export const AdminSubHeader = ({ children }) => {
   return (
     <>
       <div className="my-2 flex items-center justify-between">
@@ -9,17 +7,6 @@ export const AdminSubHeader = ({ toggleShowMenu, children }) => {
             {children}
           </p>
         </div>
-        <span className="relative">
-          <button
-            className="flex h-8 w-8 items-center justify-center rounded-full bg-slate-100 shadow-md dark:bg-slate-700"
-            onClick={toggleShowMenu}
-          >
-            <EllipsisVertical
-              size="5"
-              color="stroke-slate-800 dark:stroke-slate-50"
-            />
-          </button>
-        </span>
       </div>
     </>
   );
