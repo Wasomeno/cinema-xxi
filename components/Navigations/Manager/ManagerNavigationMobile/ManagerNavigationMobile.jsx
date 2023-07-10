4403263a65380c86e554dfd3b5b8c91d390d8afc
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-10 20:08:01 +0700

    refactor: remove ManagerMenuModal and replace icon props with Icon props that accept react-icon component

--- components/Navigations/Manager/ManagerNavigationMobile/ManagerNavigationMobile.jsx
+++ components/Navigations/Manager/ManagerNavigationMobile/ManagerNavigationMobile.jsx
@@ -1,14 +1,12 @@
-import { AnimatePresence } from "framer-motion";
-import useToggle from "hooks/useToggle";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-
-import ManagerMenuModal from "@/components/ManagerMenuModal";
+import { HiGlobe } from "react-icons/hi";
+import { HiHome } from "react-icons/hi2";
+import { MdLocalMovies } from "react-icons/md";
 
 import { ManagerNavigationMobileLink } from "./ManagerNavigationMobileLink";
 
 export const ManagerNavigationMobile = () => {
-  const [showManagerMenuModal, toggleShowManagerMenuModal] = useToggle(false);
   const [activeRoute, setActiveRoute] = useState("manager");
   const { pathname: path } = useRouter();
 
@@ -18,38 +16,25 @@ export const ManagerNavigationMobile = () => {
   }, [path]);
 
   return (
-    <>
-      <div className="sticky bottom-0 z-10 flex w-full items-center justify-center bg-slate-200 shadow-md shadow-slate-700 backdrop-blur-md dark:bg-slate-700">
-        <ManagerNavigationMobileLink
-          activeRoute={activeRoute}
-          route="manager"
-          text="Dashboard"
-          icon="house"
-        />
-        <ManagerNavigationMobileLink
-          activeRoute={activeRoute}
-          route="movies"
-          text="Movies"
-          icon="cinema"
-        />
-        <ManagerNavigationMobileLink
-          activeRoute={activeRoute}
-          route="region"
-          text="Regions"
-          icon="globe"
-        />
-        <button
-          onClick={toggleShowManagerMenuModal}
-          className="flex w-20 flex-col items-center justify-center gap-2"
-        >
-          <span className="h-8 w-8 rounded-full bg-blue-400" />
-        </button>
-      </div>
-      <AnimatePresence>
-        {showManagerMenuModal && (
-          <ManagerMenuModal toggleShowUserModal={toggleShowManagerMenuModal} />
-        )}
-      </AnimatePresence>
-    </>
+    <div className="sticky bottom-0 z-10 flex w-full items-center justify-center border-t bg-slate-50 shadow-md shadow-slate-700 backdrop-blur-md dark:bg-slate-700">
+      <ManagerNavigationMobileLink
+        activeRoute={activeRoute}
+        route="manager"
+        text="Dashboard"
+        Icon={HiHome}
+      />
+      <ManagerNavigationMobileLink
+        activeRoute={activeRoute}
+        route="movies"
+        text="Movies"
+        Icon={MdLocalMovies}
+      />
+      <ManagerNavigationMobileLink
+        activeRoute={activeRoute}
+        route="regions"
+        text="Regions"
+        Icon={HiGlobe}
+      />
+    </div>
   );
 };
