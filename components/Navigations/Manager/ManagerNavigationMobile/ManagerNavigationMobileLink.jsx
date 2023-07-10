addcea84450c2e9d53af1571f92de5b7e696c0cc
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-10 20:06:35 +0700

    refactor: replace Memoize with Icon props and remove useIcon

--- components/Navigations/Manager/ManagerNavigationMobile/ManagerNavigationMobileLink.jsx
+++ components/Navigations/Manager/ManagerNavigationMobile/ManagerNavigationMobileLink.jsx
@@ -1,39 +1,33 @@
-import { useIcon } from "hooks/useIcon";
 import Link from "next/link";
-import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const ManagerNavigationMobileLink = ({
   activeRoute,
   route,
   text,
-  icon,
+  Icon,
 }) => {
-  const Icon = useIcon(icon);
-  const MemoizedIcon = useMemo(() => Icon, []);
   return (
     <Link
       href={"/manager/" + (route === "manager" ? "" : route)}
-      className="flex h-16 w-4/12 flex-col items-center justify-center gap-2 p-2 transition duration-300 ease-in-out"
+      className="flex h-16 w-4/12 flex-col items-center justify-center gap-1.5 p-2 transition duration-300 ease-in-out"
       prefetch={false}
     >
       <span>
-        <MemoizedIcon
-          size="5"
-          color={
-            activeRoute === route
-              ? "stroke-blue-500 dark:stroke-blue-300"
-              : "stroke-slate-800 dark:stroke-slate-50"
-          }
+        <Icon
+          size="18"
+          className={twMerge(
+            "text-slate-400",
+            activeRoute === route && "text-blue-500"
+          )}
         />
       </span>
       <span className="text-center">
         <p
-          className={
-            (activeRoute === route
-              ? "text-blue-500 dark:text-blue-300 "
-              : "text-slate-800 dark:text-slate-50") +
-            " font-poppins text-xs tracking-wider"
-          }
+          className={twMerge(
+            "font-poppins text-xs tracking-wider text-slate-400 dark:text-slate-50",
+            activeRoute === route && "text-blue-500"
+          )}
         >
           {text}
         </p>
