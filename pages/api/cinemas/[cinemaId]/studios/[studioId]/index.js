0b06e80884c14e80df9031c2895de4e5ca79a05a
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-11 13:55:22 +0700

    feat: add cinema studio details route

---
+++ pages/api/cinemas/[cinemaId]/studios/[studioId]/index.js
@@ -0,0 +1,12 @@
+import { prisma } from "lib/prisma";
+
+export default async function studioDetailsHandler(req, res) {
+  const { studio } = req.query;
+  if (req.method === "GET") {
+    const studioDetails = await prisma.studio.findUnique({
+      where: { id: parseInt(studio) },
+      include: { showtime: true },
+    });
+    res.status(200).json(studioDetails);
+  }
+}
