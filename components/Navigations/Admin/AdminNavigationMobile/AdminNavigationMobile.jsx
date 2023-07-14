6a4e0408e9afe1866afa0aafeeaf5cb9c23898c2
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-14 22:56:05 +0700

    refactor: remove useToggle, useState & useEffect hooks, remove AdminMenuModal component

--- components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobile.jsx
+++ components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobile.jsx
@@ -1,66 +1,50 @@
-import { AnimatePresence } from "framer-motion";
-import useToggle from "hooks/useToggle";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-
-import AdminMenuModal from "@/components/AdminMenuModal";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { HiHome } from "react-icons/hi2";
+import { IoTimeOutline } from "react-icons/io5";
+import { MdMeetingRoom, MdOutlineMovie } from "react-icons/md";
 
 import { AdminNavigationMobileLink } from "./AdminNavigationMobileLink";
 
 export const AdminNavigationMobile = () => {
-  const [showUserModal, toggleShowUserModal] = useToggle(false);
-  const [activeRoute, setActiveRoute] = useState("admin");
-  const { pathname: path } = useRouter();
-
-  useEffect(() => {
-    const basePath = path.split("/")[2];
-    setActiveRoute(basePath === undefined ? "admin" : basePath);
-  }, [path]);
-
+  const { pathname } = useRouter();
   return (
-    <>
-      <div className="sticky bottom-0 z-10 flex w-full items-center justify-start overflow-x-scroll bg-slate-100 shadow-md dark:bg-slate-700">
-        <AdminNavigationMobileLink
-          href="admin"
-          icon="house"
-          activeRoute={activeRoute}
-        >
-          Dashboard
-        </AdminNavigationMobileLink>
-        <AdminNavigationMobileLink
-          href="movies"
-          icon="cinema"
-          activeRoute={activeRoute}
-        >
-          Movies
-        </AdminNavigationMobileLink>
-        <AdminNavigationMobileLink
-          href="showtimes"
-          icon="time"
-          activeRoute={activeRoute}
-        >
-          Showtimes
-        </AdminNavigationMobileLink>
-        <AdminNavigationMobileLink
-          href="studios"
-          icon="rectangleStack"
-          activeRoute={activeRoute}
-          s
-        >
-          Studios
-        </AdminNavigationMobileLink>
-        <button
-          onClick={toggleShowUserModal}
-          className="flex w-60 flex-col items-center justify-center gap-2"
-        >
-          <span className="h-8 w-8 rounded-full bg-blue-400" />
-        </button>
-      </div>
-      <AnimatePresence>
-        {showUserModal && (
-          <AdminMenuModal toggleShowUserModal={toggleShowUserModal} />
-        )}
-      </AnimatePresence>
-    </>
+    <div className="sticky bottom-0 z-10 flex w-screen items-center justify-start overflow-x-scroll border bg-slate-50 shadow-md dark:bg-slate-700 sm:justify-evenly sm:p-1.5 lg:hidden">
+      <AdminNavigationMobileLink
+        href="admin"
+        Icon={HiHome}
+        activeRoute={pathname}
+      >
+        Dashboard
+      </AdminNavigationMobileLink>
+      <AdminNavigationMobileLink
+        href="movies"
+        Icon={MdOutlineMovie}
+        activeRoute={pathname}
+      >
+        Movies
+      </AdminNavigationMobileLink>
+      <AdminNavigationMobileLink
+        href="showtimes"
+        Icon={IoTimeOutline}
+        activeRoute={pathname}
+      >
+        Showtimes
+      </AdminNavigationMobileLink>
+      <AdminNavigationMobileLink
+        href="studios"
+        Icon={MdMeetingRoom}
+        activeRoute={pathname}
+      >
+        Studios
+      </AdminNavigationMobileLink>
+      <AdminNavigationMobileLink
+        href="admins"
+        Icon={BsPersonLinesFill}
+        activeRoute={pathname}
+      >
+        Admins
+      </AdminNavigationMobileLink>
+    </div>
   );
 };
