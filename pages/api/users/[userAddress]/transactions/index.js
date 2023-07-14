d380e3fe7ca48cd15d0d4e15afe0f810666fcb22
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-14 22:37:03 +0700

    feat: add api route for user transactions

--- pages/api/users/[userAddress]/transactions/index.js
+++ pages/api/users/[userAddress]/transactions/index.js
@@ -1,1 +1,19 @@
-export async function transactionsHandler() {}
+import { prisma } from "lib/prisma";
+
+export default async function userTransactionsHandler(req, res) {
+  const { userAddress } = req.query;
+  console.log(req, res);
+  if (req.method === "GET") {
+    const transactions = await prisma.transaction.findMany({
+      where: { user: userAddress },
+      include: { movie: true, cinema: true, region: true },
+    });
+
+    res.status(200).json(
+      transactions.map((transaction) => ({
+        ...transaction,
+        showtime: parseInt(transaction.showtime),
+      }))
+    );
+  }
+}
