1beab53cdf378c5b47be071524817292f1b6131c
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-14 22:57:12 +0700

    refactor: remove useIcon & useMemo hooks, replace MemoizedIcon with Icon passed from props

--- components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobileLink.jsx
+++ components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobileLink.jsx
@@ -1,43 +1,33 @@
-import { useIcon } from "hooks/useIcon";
 import Link from "next/link";
-import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
 
 export const AdminNavigationMobileLink = ({
   href,
   children,
-  icon,
+  Icon,
   activeRoute,
 }) => {
-  const Icon = useIcon(icon);
-  const MemoizedIcon = useMemo(() => Icon, []);
   return (
     <Link
       href={"/admin/" + (href === "admin" ? "" : href)}
-      className="flex h-16 w-96 flex-col items-center justify-center gap-2 transition duration-300 ease-in-out"
+      className="flex h-16 flex-col items-center justify-center gap-1.5 transition duration-300 ease-in-out"
       prefetch={false}
     >
       <span>
-        <MemoizedIcon
-          size="5"
-          color={
-            activeRoute === href
-              ? "stroke-blue-500 dark:stroke-blue-300"
-              : "stroke-slate-800 dark:stroke-slate-50"
-          }
+        <Icon
+          className={twMerge(
+            "h-4 w-4 text-slate-400 dark:text-slate-50 sm:h-6 sm:w-6",
+            activeRoute.includes(href) && "text-blue-500 dark:text-blue-300"
+          )}
         />
       </span>
-      <span className="text-center">
-        <p
-          className={
-            (activeRoute === href
-              ? "text-blue-500 dark:text-blue-300"
-              : "text-slate-800 dark:text-slate-50") +
-            " " +
-            "font-poppins text-xs font-normal tracking-wide dark:font-light "
-          }
-        >
-          {children}
-        </p>
+      <span
+        className={twMerge(
+          "w-24 text-center font-poppins text-xs font-normal tracking-wide text-slate-400 dark:font-light dark:text-slate-50 sm:text-sm",
+          activeRoute.includes(href) && "text-blue-500 dark:text-blue-300"
+        )}
+      >
+        {children}
       </span>
     </Link>
   );
