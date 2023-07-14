84b16fcb996ca1af89a414f15e83d090958189bf
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-14 22:51:57 +0700

    refactor: remove useSession & useState hooks, replace framer motion div with regular div

--- components/Navigations/Admin/AdminNavigation/AdminNavigation.jsx
+++ components/Navigations/Admin/AdminNavigation/AdminNavigation.jsx
@@ -1,98 +1,32 @@
-import { motion } from "framer-motion";
-import { signOut, useSession } from "next-auth/react";
-import { useState } from "react";
-
-import ChevronRight from "@/components/Icons/ChevronRight";
-import Power from "@/components/Icons/Power";
+import { BsPersonLinesFill } from "react-icons/bs";
+import { HiHome } from "react-icons/hi2";
+import { IoTimeOutline } from "react-icons/io5";
+import { MdMeetingRoom, MdOutlineMovie } from "react-icons/md";
 
 import { AdminNavigationLink } from "./AdminNavigationLink";
 
 export const AdminNavigation = () => {
-  const { data: sessionData } = useSession();
-  const [showNav, setShowNav] = useState(false);
-
   return (
-    <motion.div
-      initial={{ width: "70px" }}
-      animate={{ width: showNav ? "280px" : "70px" }}
-      transition={{ ease: "easeInOut", duration: "0.3" }}
-      className={
-        "fixed top-0 left-0 z-10 hidden h-full bg-slate-100 shadow-md dark:bg-gray-700 md:block"
-      }
-    >
-      <button
-        className="top-24 -right-4 z-10 h-7 w-7 items-center justify-center rounded-lg bg-blue-300 text-center shadow-md dark:bg-slate-500 md:absolute md:flex"
-        onClick={() => setShowNav(!showNav)}
-      >
-        <span className={(showNav ? "rotate-180 " : "") + "p-2"}>
-          <ChevronRight size="5" color="stroke-slate-800" />
-        </span>
-      </button>
-      <div className="flex h-36 flex-col items-center justify-center gap-2 border-b border-b-gray-400 bg-slate-100 transition duration-300 dark:bg-gray-700">
-        <div className="h-12 w-12 rounded-full bg-slate-500 dark:bg-slate-300" />
-        <motion.div
-          className="text-center"
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: showNav ? 1 : 0,
-          }}
-          transition={{ duration: 0.2, ease: "easeInOut" }}
-        >
-          <p className="w-60 font-poppins text-sm font-medium">
-            {sessionData.user.name}
-          </p>
-          <p className="my-2 w-60 font-poppins text-xs font-medium text-slate-300">
-            {showNav && sessionData.user.cinemaName}
-          </p>
-        </motion.div>
-      </div>
-      <div className="flex h-5/6 flex-col justify-between">
-        <div className="flex h-4/6 flex-col items-center gap-4 overflow-hidden">
-          <AdminNavigationLink
-            setShowNav={setShowNav}
-            href="/admin"
-            icon="house"
-            showNav={showNav}
-          >
+    <div className="sticky top-0 z-10 hidden w-[240px] rounded-lg border bg-slate-50 dark:border-slate-500 dark:bg-gray-700 lg:block">
+      <div className="flex h-5/6 flex-col justify-start">
+        <div className="flex flex-col justify-evenly gap-2 overflow-hidden p-3">
+          <AdminNavigationLink href="/admin" Icon={HiHome}>
             Dashboard
           </AdminNavigationLink>
-          <AdminNavigationLink
-            setShowNav={setShowNav}
-            href="/admin/movies"
-            icon="cinema"
-            showNav={showNav}
-          >
-            Manage Movies
+          <AdminNavigationLink href="/admin/movies" Icon={MdOutlineMovie}>
+            Movies
           </AdminNavigationLink>
-          <AdminNavigationLink
-            setShowNav={setShowNav}
-            href="/admin/showtimes"
-            icon="time"
-            showNav={showNav}
-          >
-            Manage Showtimes
+          <AdminNavigationLink href="/admin/showtimes" Icon={IoTimeOutline}>
+            Showtimes
           </AdminNavigationLink>
-          <AdminNavigationLink
-            setShowNav={setShowNav}
-            href="/admin/studios"
-            icon="rectangleStack"
-            showNav={showNav}
-          >
-            Manage Studios
+          <AdminNavigationLink href="/admin/studios" Icon={MdMeetingRoom}>
+            Studios
+          </AdminNavigationLink>
+          <AdminNavigationLink href="/admin/admins" Icon={BsPersonLinesFill}>
+            Admins
           </AdminNavigationLink>
         </div>
-        <button
-          onClick={() => signOut({ redirect: false })}
-          className="flex h-36 items-center justify-center p-2"
-        >
-          <span className="flex h-10 w-10 items-center justify-center rounded-md bg-slate-700 dark:bg-slate-500">
-            <Power size="4" color="stroke-slate-50" />
-          </span>
-          {showNav && (
-            <span className="w-3/6 font-poppins text-sm">Logout</span>
-          )}
-        </button>
       </div>
-    </motion.div>
+    </div>
   );
 };
