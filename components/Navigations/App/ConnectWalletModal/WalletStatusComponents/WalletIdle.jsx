0d98bb0f8692064c312e12fa00b398d8b9538355
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-14 22:46:49 +0700

    refactor: remove text-white class, change base background from bg-slate-700 to bg-slate-200 & add bg-slate-700 as dark theme base background

--- components/Navigations/App/ConnectWalletModal/WalletStatusComponents/WalletIdle.jsx
+++ components/Navigations/App/ConnectWalletModal/WalletStatusComponents/WalletIdle.jsx
@@ -2,14 +2,14 @@ export const WalletIdle = ({ connectors, connect }) => {
   return (
     <>
       <div className="my-2">
-        <p className="font-poppins text-sm text-white">Connect a Wallet</p>
+        <p className="font-poppins text-sm">Connect a Wallet</p>
       </div>
       <div className="my-2 flex flex-col gap-4">
         {connectors.map((connector, index) => (
           <button
             key={index}
             onClick={() => connect({ connector: connector, chainId: 11155111 })}
-            className="font-poppins w-full rounded-lg bg-slate-700 p-3 text-sm tracking-wide text-white"
+            className="w-full rounded-lg bg-slate-200 p-3 font-poppins text-sm tracking-wide dark:bg-slate-700"
           >
             Metamask Wallet
           </button>
