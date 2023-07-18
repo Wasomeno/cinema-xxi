4bb5a54fcd79d5acca33e24b171fd545a21e8fef
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-18 23:15:15 +0700

    refactor: replace showtimeDetails & closeModal props with fetched data & router.back, add update studio mutation

--- modules/adminPages/studiosPage/CinemaStudios/components/EditStudioModal.jsx
+++ modules/adminPages/studiosPage/CinemaStudios/components/EditStudioModal.jsx
@@ -1,15 +1,51 @@
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 import FormModalContainer from "@/components/FormModalContainer";
+import mutation from "@/components/reactQuery/mutations/mutation";
+import { useSideEffects } from "@/components/reactQuery/mutations/useSideEffects";
+import { query } from "@/components/reactQuery/queries/query";
+import { cinemaStudioQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaStudioQueryKeys";
 
-export const EditStudioModal = ({ studioDetails, closeModal }) => {
-  const [studioNumber, setStudioNumber] = useState(studioDetails.studio);
-  const [capacity, setCapacity] = useState(studioDetails.capacity);
+export const EditStudioModal = () => {
+  const [studioNumber, setStudioNumber] = useState();
+  const [capacity, setCapacity] = useState();
+
+  const router = useRouter();
+  const { data: session } = useSession();
+  const studioDetails = query({
+    queryKey: ["studio", router.query.id],
+    url: `/api/cinemas/${session.user.cinemaId}/studios/${router.query.id}`,
+  });
+
+  const sideEffects = useSideEffects({
+    queryKeys: cinemaStudioQueryKeys.allStudio,
+    text: `Updating studio ${studioNumber}`,
+  });
+
+  const updateStudio = mutation({
+    method: "PATCH",
+    body: {
+      studio: studioNumber,
+      capacity,
+    },
+    url: `/api/cinemas/${session.user.cinemaId}/studios/${router.query.id}`,
+    sideEffects,
+  });
+
+  useEffect(() => {
+    if (!studioDetails.isLoading) {
+      setStudioNumber(studioDetails.data?.studio);
+      setCapacity(studioDetails.data?.capacity);
+    }
+  }, [studioDetails.isLoading]);
+
   return (
     <FormModalContainer
       title="Edit Studio"
-      onSubmit={() => {}}
-      closeModal={closeModal}
+      onSubmit={() => updateStudio.mutate()}
+      closeModal={() => router.back()}
     >
       <FormModalContainer.Input
         type="text"
