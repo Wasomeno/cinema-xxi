2ac171a9c7ea160aac359dfa626833c80c8a3ce2
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-18 15:10:47 +0700

    refactor: replace custom icons with react-icons, replace conditions with inline conditions

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -1,22 +1,10 @@
 import { AnimatePresence } from "framer-motion";
+import { HiCheckCircle } from "react-icons/hi";
+import { RiErrorWarningFill } from "react-icons/ri";
+import { twMerge } from "tailwind-merge";
 
 import { useToastDetails } from "../stores/toastStore";
 import AnimatedContainer from "./AnimatedContainer";
-import CheckMark from "./Icons/Checkmark";
-import Warning from "./Icons/Warning";
-
-const conditions = {
-  success: {
-    class:
-      "from-green-800 via-green-700 to-green-600 dark:from-green-400 dark:via-green-300 dark:to-green-200",
-    icon: <CheckMark size="5 lg:w-7 lg:h-7" color="fill-slate-200" />,
-  },
-  error: {
-    class:
-      "from-red-800 via-red-700 to-red-600 dark:from-red-400 dark:via-red-300 dark:to-red-200",
-    icon: <Warning size="5 lg:w-7 lg:h-7" color="fill-slate-200" />,
-  },
-};
 
 const Toast = () => {
   const [show, text, condition] = useToastDetails();
@@ -24,14 +12,28 @@ const Toast = () => {
     <AnimatePresence>
       {show && (
         <AnimatedContainer
-          className={
-            "fixed bottom-0 left-1/2 z-30 flex h-14 w-5/6 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg bg-gradient-to-r p-2 shadow-md lg:w-2/6 " +
-            conditions[condition]?.class
-          }
+          className={twMerge(
+            "fixed bottom-0 left-1/2 z-30 flex h-14 w-5/6 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg p-2 shadow-md lg:w-2/6",
+            condition === "success"
+              ? "bg-green-100 dark:bg-green-700"
+              : "bg-red-100 dark:bg-red-700"
+          )}
         >
-          <div className="w-1/6">{conditions[condition]?.icon}</div>
+          <div className="w-1/6">
+            {condition === "success" ? (
+              <HiCheckCircle
+                size="18"
+                className="text-green-700 dark:text-green-500"
+              />
+            ) : (
+              <RiErrorWarningFill
+                size="18"
+                className="text-red-700 dark:text-red-500"
+              />
+            )}
+          </div>
           <div className="w-4/6 text-center">
-            <p className="font-poppins text-xs text-slate-50 dark:font-medium dark:text-slate-900 lg:text-sm">
+            <p className="font-poppins text-xs text-slate-800 dark:font-medium dark:text-slate-100 lg:text-sm">
               {text}
             </p>
           </div>
