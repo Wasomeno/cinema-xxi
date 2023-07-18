7555aa7116c3319d2c5d87cdadac1d447b29ef95
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-18 14:53:14 +0700

    refactor: add skeletons, add ModalContainer, add checkmark icon when region selected

--- modules/appPages/homePage/components/RegionListModal.jsx
+++ modules/appPages/homePage/components/RegionListModal.jsx
@@ -1,39 +1,55 @@
+import { useSkeleton } from "hooks/useSkeleton";
+import { HiCheck, HiXMark } from "react-icons/hi2";
+
 import AnimatedContainer from "@/components/AnimatedContainer";
-import DataContainer from "@/components/DataContainer";
+import { ModalContainer } from "@/components/ModalContainer";
 import { useAllRegions } from "@/components/reactQuery/queries/Region/useAllRegions";
 
-const RegionListModal = ({ setSelectedRegion, toggleShowRegionList }) => {
+const RegionListModal = ({
+  selectedRegion,
+  setSelectedRegion,
+  toggleShowRegionList,
+}) => {
   const allRegion = useAllRegions();
+
+  const skeletons = useSkeleton(
+    <div className="h-10 w-full animate-pulse rounded-lg bg-slate-300" />,
+    5
+  );
   return (
-    <>
-      <AnimatedContainer
-        className="z-15 fixed left-0 bottom-0 h-screen w-screen bg-black bg-opacity-70"
-        onClick={toggleShowRegionList}
-      />
-      <AnimatedContainer className="fixed bottom-0 z-20 h-96 w-full rounded-t-lg bg-slate-100 p-4 dark:bg-slate-800 lg:top-1/2 lg:left-1/2 lg:h-4/6 lg:w-4/6 lg:-translate-x-1/2 lg:-translate-y-1/2 lg:rounded-lg">
-        <div className="my-4 text-center">
-          <p className="font-poppins text-sm">Region List</p>
+    <ModalContainer closeModal={toggleShowRegionList}>
+      <AnimatedContainer className="fixed bottom-0 z-20 h-96 w-full rounded-t-lg bg-slate-100 px-6 py-4 dark:bg-slate-800 lg:left-1/2 lg:top-1/2 lg:h-4/6 lg:w-2/6 lg:-translate-x-1/2 lg:-translate-y-1/2 lg:rounded-lg">
+        <div className="mb-4 mt-2.5 flex items-center justify-between">
+          <h3 className="font-poppins text-sm lg:text-base">Region List</h3>
+          <button onClick={toggleShowRegionList}>
+            <HiXMark size="22" />
+          </button>
         </div>
-        <DataContainer
-          loading={allRegion.isLoading}
-          object="region"
-          className="flex flex-col items-center justify-start gap-3"
-        >
-          {allRegion.data?.map((region) => (
-            <button
-              key={region.id}
-              onClick={() => {
-                toggleShowRegionList();
-                setSelectedRegion(region);
-              }}
-              className="w-5/6 rounded-md bg-gray-200 p-3 font-poppins text-xs shadow-sm backdrop-blur-sm dark:bg-gray-700 lg:w-4/6"
-            >
-              {region.name}
-            </button>
-          ))}
-        </DataContainer>
+        <div className="flex flex-col items-center gap-2 overflow-y-scroll">
+          {allRegion.isLoading && skeletons.map((skeleton) => skeleton)}
+          {!allRegion.isLoading && allRegion.status !== "error"
+            ? allRegion.data?.map((region) => (
+                <div
+                  key={region.id}
+                  onClick={() => {
+                    setSelectedRegion(region);
+                    toggleShowRegionList();
+                  }}
+                  className="relative flex w-full cursor-pointer justify-center rounded-lg bg-slate-200 p-2 text-sm tracking-wide"
+                >
+                  {selectedRegion.id === region.id && (
+                    <span className="absolute left-4 top-1/2 -translate-y-1/2">
+                      <HiCheck />
+                    </span>
+                  )}
+
+                  <span className="text-xs lg:text-sm">{region.name}</span>
+                </div>
+              ))
+            : null}
+        </div>
       </AnimatedContainer>
-    </>
+    </ModalContainer>
   );
 };
 
