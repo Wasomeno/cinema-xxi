84a465c582c46a5943d6d0a078edfcaf5435881a
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-18 15:08:11 +0700

    refactor: add Head tag, replace main tag with div

--- components/Layouts/AppLayout.jsx
+++ components/Layouts/AppLayout.jsx
@@ -1,17 +1,21 @@
 import { useViewport } from "hooks/useViewport";
-import React from "react";
+import Head from "next/head";
 
 import AppNavigation from "../Navigations/App/AppNavigation";
 import AppNavigationMobile from "../Navigations/App/AppNavigationMobile";
 
-const AppLayout = ({ children }) => {
+const AppLayout = ({ children, pageTitle }) => {
   const viewport = useViewport();
   return (
-    <>
+    <div className="flex min-h-screen flex-1 flex-col">
+      <Head>
+        <title>{`XXI Cinema App  ${pageTitle ? `| ${pageTitle}` : ""}`}</title>
+        <meta property="description" content="App page" />
+      </Head>
       <AppNavigation />
-      <main className="relative">{children}</main>
+      <div className="relative">{children}</div>
       {viewport.width < 768 && <AppNavigationMobile />}
-    </>
+    </div>
   );
 };
 
