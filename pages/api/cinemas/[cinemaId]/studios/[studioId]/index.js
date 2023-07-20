4e58be8543f10300c1dc5b5b3d1553e536de2956
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-20 23:28:59 +0700

    feat: add PATCH method handler

--- pages/api/cinemas/[cinemaId]/studios/[studioId]/index.js
+++ pages/api/cinemas/[cinemaId]/studios/[studioId]/index.js
@@ -1,12 +1,24 @@
 import { prisma } from "lib/prisma";
 
 export default async function studioDetailsHandler(req, res) {
-  const { studio } = req.query;
+  const { studioId } = req.query;
   if (req.method === "GET") {
     const studioDetails = await prisma.studio.findUnique({
-      where: { id: parseInt(studio) },
-      include: { showtime: true },
+      where: { id: parseInt(studioId) },
+      include: { showtime_to_movie: true },
     });
     res.status(200).json(studioDetails);
+  }
+  if (req.method === "PATCH") {
+    const { capacity, studio } = req.body;
+    try {
+      await prisma.studio.update({
+        where: { id: parseInt(studioId) },
+        data: { capacity: parseInt(capacity), studio: parseInt(studio) },
+      });
+      res.status(200).json({ message: "Succesfully updated studio" });
+    } catch (error) {
+      res.status(500).json(error);
+    }
   }
 }
