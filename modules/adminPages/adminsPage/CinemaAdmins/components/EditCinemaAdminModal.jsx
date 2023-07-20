48d7999bcac5fcf4873970ccea25dc87720735e8
Wasomeno <kevinanandapradipta@gmail.com>
2023-07-20 23:37:17 +0700

    refactor: remove closeModal & adminDetails props, remove adminDetails in multiple useStates, add query function for fetching the admin details & add useEffect for setting the states after the admin details have been fetched

--- modules/adminPages/adminsPage/CinemaAdmins/components/EditCinemaAdminModal.jsx
+++ modules/adminPages/adminsPage/CinemaAdmins/components/EditCinemaAdminModal.jsx
@@ -1,17 +1,28 @@
+import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import FormModalContainer from "@/components/FormModalContainer";
 import mutation from "@/components/reactQuery/mutations/mutation";
 import { useSideEffects } from "@/components/reactQuery/mutations/useSideEffects";
+import { query } from "@/components/reactQuery/queries/query";
 import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys";
 
-export const EditCinemaAdminModal = ({ closeModal, adminDetails }) => {
-  const [username, setUsername] = useState(adminDetails.username);
-  const [password, setPassword] = useState(adminDetails.password);
-  const [name, setName] = useState(adminDetails.name);
+export const EditCinemaAdminModal = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const session = useSession();
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  const adminDetails = query({
+    queryKey: ["admin", router.query.id],
+    url: `/api/cinemas/${session.user.cinemaId}/admins/${router.query.id}`,
+    enabledCondition: session.user.cinemaId !== undefined,
+  });
+
   const sideEffects = useSideEffects({
     text: "Updating admin",
     queryKeys: cinemaQueryKeys.cinemaAdmins(session.data?.user.cinemaId),
@@ -28,11 +39,20 @@ export const EditCinemaAdminModal = ({ closeModal, adminDetails }) => {
     sideEffects,
   });
 
+  useEffect(() => {
+    if (!adminDetails.isLoading) {
+      setName(adminDetails.data?.name);
+      setUsername(adminDetails.data?.username);
+      setPassword(adminDetails.data?.password);
+    }
+  }, [adminDetails.isLoading]);
+
   return (
     <FormModalContainer
       onSubmit={updateCinemaAdmin.mutate}
       title="Edit Admin"
-      closeModal={closeModal}
+      closeModal={() => router.push(`/admin/admins`)}
+      className="lg:w-2/6 lg:h-4/6"
     >
       <FormModalContainer.Input
         type="text"
