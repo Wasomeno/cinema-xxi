459c45e0d3a1e6336cc77e1f63d3dadb6abff3ec
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-02 23:36:54 +0700

    refactor: remove min-h-screen

--- modules/adminPages/studiosPage/CinemaStudioDetails/CinemaStudioDetails.jsx
+++ modules/adminPages/studiosPage/CinemaStudioDetails/CinemaStudioDetails.jsx
@@ -37,7 +37,7 @@ export const CinemaStudioDetails = ({ studioDetails }) => {
   );
 
   return (
-    <div className="flex min-h-screen flex-1 flex-col rounded-lg border bg-white p-4 dark:border-slate-500 dark:bg-slate-700">
+    <div className="flex flex-1 flex-col rounded-lg border bg-white p-4 dark:border-slate-500 dark:bg-slate-700">
       <AdminHeader>Studio {studioDetails.studio} Showtimes</AdminHeader>
       <StudioShowtimesTable dispatch={dispatch} />
       <AnimatePresence>
