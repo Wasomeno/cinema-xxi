4705e50ecdea4561e7e5b3646895c0a4865bc2e2
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-02 23:24:47 +0700

    feat: add image urls

--- next.config.js
+++ next.config.js
@@ -7,10 +7,32 @@ const env = {
   ROLES_CONTRACT_ADDRESS: "0x6986f8ac26ed37c46dd8c166450d9a0cc464ade7",
   TICKET_CONTRACT_ADDRESS: "0x16cee9a9dd6e65ce9ec600f1b2bd0a7bf99dc6ff",
   NEXTAUTH_SECRET: "5c3d24485bb838879e06f5bebca5652f",
-  // API_MOVIES: /
+  IMDB_API_KEY: "k_96rtl2kc",
 };
 
 module.exports = withBundleAnalyzer({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "imdb-api.com",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "media.21cineplex.com",
+        port: "",
+        pathname: "/webcontent/**",
+      },
+    ],
+  },
   reactStrictMode: true,
   swcMinify: true,
   env: env,
