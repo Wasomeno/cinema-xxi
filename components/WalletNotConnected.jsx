b601dd36cec6ecbf26af167f95bc9775c4bfb92e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-02 23:26:39 +0700

    refactor: replace h-[800px] with flex-1 & add responsive class for the text

--- components/WalletNotConnected.jsx
+++ components/WalletNotConnected.jsx
@@ -2,8 +2,8 @@ import { VscDebugDisconnect } from "react-icons/vsc";
 
 export const WalletNotConnected = () => {
   return (
-    <div className="flex h-[800px] flex-col items-center justify-center  gap-2 bg-transparent">
-      <span className="font-poppins text-slate-400">
+    <div className="flex flex-1 flex-col items-center justify-center gap-2 bg-white">
+      <span className="font-poppins lg:text-base text-sm text-slate-400">
         Connect your wallet first
       </span>
       <VscDebugDisconnect size="25" className="text-slate-400" />
