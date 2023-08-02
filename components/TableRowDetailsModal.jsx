ab73cf30b8215fbf08bcf51cae4a9eeb2857dc25
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-02 23:25:28 +0700

    refactor: change bg-slate-100 to bg-white

--- components/TableRowDetailsModal.jsx
+++ components/TableRowDetailsModal.jsx
@@ -6,7 +6,7 @@ import { ModalContainer } from "./ModalContainer";
 export const TableRowDetailsModal = ({ title, children, closeModal }) => {
   return (
     <ModalContainer toggleModal={closeModal}>
-      <AnimatedContainer className="fixed inset-x-1/2 inset-y-1/2 z-30 h-5/6 w-3/6 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-slate-200 px-6 py-4 shadow-md dark:bg-slate-700">
+      <AnimatedContainer className="fixed inset-x-1/2 inset-y-1/2 z-30 h-5/6 w-3/6 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white px-6 py-4 shadow-md dark:bg-slate-700">
         <div className="my-4 flex items-center justify-between">
           <h3 className="font-poppins text-lg tracking-wider">{title}</h3>
           <button onClick={closeModal}>
