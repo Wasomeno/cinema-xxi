ac9bcbe76f6a926a729b2c1dba469d38970e5c37
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:37:18 +0700

    refactor: change Dropdown content width & add text-sm for large viewport and text-xs for viewport smaller than large

--- components/TableRowMenu.jsx
+++ components/TableRowMenu.jsx
@@ -1,6 +1,6 @@
-import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import Link from "next/link";
-import { BsThreeDots } from "react-icons/bs";
+import Link from "next/link"
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu"
+import { BsThreeDots } from "react-icons/bs"
 
 const TableRowMenu = ({ children }) => {
   return (
@@ -11,8 +11,8 @@ const TableRowMenu = ({ children }) => {
         </div>
       </DropdownMenu.Trigger>
       <DropdownMenu.Portal>
-        <DropdownMenu.Content className="w-48 rounded-md bg-slate-100 px-1.5 py-3 font-poppins text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] dark:bg-slate-700 ">
-          <div className="mx-2 font-medium text-gray-600 dark:text-gray-300">
+        <DropdownMenu.Content className="w-40 rounded-md bg-slate-100 px-1.5 py-3 font-poppins text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] dark:bg-slate-700 lg:w-48 ">
+          <div className="mx-2 text-xs font-medium text-gray-600 dark:text-gray-300 lg:text-sm">
             Menu
           </div>
           <DropdownMenu.Separator className="my-2 h-px bg-slate-300 dark:bg-slate-600" />
@@ -20,21 +20,21 @@ const TableRowMenu = ({ children }) => {
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
-  );
-};
+  )
+}
 
 const Button = ({ children, onClick }) => {
   return (
     <DropdownMenu.Item>
       <button
         onClick={onClick}
-        className="w-full rounded-md px-2 py-1.5 text-start transition duration-200 hover:bg-blue-100 hover:dark:bg-slate-600"
+        className="w-full rounded-md px-2 py-1.5 text-start text-xs transition duration-200 hover:bg-blue-100 hover:dark:bg-slate-600 lg:text-sm"
       >
         {children}
       </button>
     </DropdownMenu.Item>
-  );
-};
+  )
+}
 const ButtonLink = ({ children, href }) => {
   return (
     <DropdownMenu.Item>
@@ -45,10 +45,10 @@ const ButtonLink = ({ children, href }) => {
         {children}
       </Link>
     </DropdownMenu.Item>
-  );
-};
+  )
+}
 
-TableRowMenu.Button = Button;
-TableRowMenu.Link = ButtonLink;
+TableRowMenu.Button = Button
+TableRowMenu.Link = ButtonLink
 
-export default TableRowMenu;
+export default TableRowMenu
