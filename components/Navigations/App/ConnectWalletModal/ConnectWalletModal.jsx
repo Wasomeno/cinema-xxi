c72064eb00eee37b284422bf8c1863b9336a76ad
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-08 01:52:07 +0700

    refactor: replace ModalContainer with CenteredModalContainer

--- components/Navigations/App/ConnectWalletModal/ConnectWalletModal.jsx
+++ components/Navigations/App/ConnectWalletModal/ConnectWalletModal.jsx
@@ -1,37 +1,37 @@
-import React from "react";
-import { useConnect } from "wagmi";
+import React from "react"
+import { useConnect } from "wagmi"
 
-import AnimatedContainer from "@/components/AnimatedContainer";
-import { ModalContainer } from "@/components/ModalContainer";
+import { CenteredModalContainer } from "@/components/ModalContainer"
 
 import {
   WalletError,
   WalletIdle,
   WalletLoading,
   WalletSuccess,
-} from "./WalletStatusComponents";
+} from "./WalletStatusComponents"
 
 export const ConnectWalletModal = ({ toggleWalletModal }) => {
-  const { connect, connectors, status, error } = useConnect();
+  const { connect, connectors, status, error } = useConnect()
   const walletComponents = {
     loading: WalletLoading,
     error: WalletError,
     success: WalletSuccess,
     idle: WalletIdle,
-  };
-  const WalletStatusComponent = walletComponents[status];
+  }
+  const WalletStatusComponent = walletComponents[status]
   return (
-    <ModalContainer closeModal={toggleWalletModal}>
-      <AnimatedContainer className="fixed bottom-0 z-40 flex h-72 w-full flex-col items-center gap-2 rounded-t-lg bg-slate-50 p-4 dark:bg-slate-800 lg:left-1/2 lg:top-1/2 lg:h-80 lg:w-80 lg:-translate-x-1/2 lg:-translate-y-1/2 lg:rounded-lg">
-        <div className="h-5/6 w-full">
-          <WalletStatusComponent
-            connectors={connectors}
-            connect={connect}
-            error={error}
-            toggleWalletModal={toggleWalletModal}
-          />
-        </div>
-      </AnimatedContainer>
-    </ModalContainer>
-  );
-};
+    <CenteredModalContainer
+      closeModal={toggleWalletModal}
+      className="flex h-72 w-full flex-col items-center gap-2 p-4 lg:h-80 lg:w-80"
+    >
+      <div className="h-5/6 w-full">
+        <WalletStatusComponent
+          connectors={connectors}
+          connect={connect}
+          error={error}
+          toggleWalletModal={toggleWalletModal}
+        />
+      </div>
+    </CenteredModalContainer>
+  )
+}
