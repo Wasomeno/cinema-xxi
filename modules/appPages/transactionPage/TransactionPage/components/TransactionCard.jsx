a16b9f5cb761d6ff334e14e4398f0b9c2bb94b33
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:03:38 +0700

    refactor: add border

--- modules/appPages/transactionPage/TransactionPage/components/TransactionCard.jsx
+++ modules/appPages/transactionPage/TransactionPage/components/TransactionCard.jsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
+import React from "react"
+import Image from "next/image"
+import Link from "next/link"
 
 export const TransactionCard = ({
   movie,
@@ -10,11 +10,11 @@ export const TransactionCard = ({
   ticketAmount,
   href,
 }) => {
-  const dateTime = new Date(showtime * 1000);
+  const dateTime = new Date(showtime * 1000)
   return (
     <Link
       href={href}
-      className="h-40 w-full rounded-lg bg-slate-50 p-2 shadow-sm lg:h-52 lg:w-[30rem]"
+      className="h-40 w-full rounded-lg border bg-slate-50 p-2 shadow-sm dark:border-slate-700 dark:bg-slate-900 lg:h-52 lg:w-[30rem]"
     >
       <div className="flex h-full items-center justify-between">
         <div className="relative h-full w-4/12">
@@ -39,25 +39,25 @@ export const TransactionCard = ({
         </div>
       </div>
     </Link>
-  );
-};
+  )
+}
 
 export const TransactionCardSkeleton = () => {
   return (
-    <div className="h-40 w-full animate-pulse rounded-lg bg-slate-200 p-4 shadow-sm lg:h-52 lg:w-[30rem]">
+    <div className="h-40 w-full animate-pulse rounded-lg bg-slate-300 p-4 shadow-sm dark:bg-slate-700 lg:h-52 lg:w-[30rem]">
       <div className="flex h-full items-center justify-between">
         <div className="relative h-full w-4/12">
-          <div className="h-full w-full animate-pulse rounded-lg bg-slate-400" />
+          <div className="h-full w-full animate-pulse rounded-lg bg-slate-400 dark:bg-slate-800" />
         </div>
         <div className="flex h-full w-7/12 items-center justify-center">
           <div className="flex w-5/6 flex-col gap-2">
-            <div className="h-[14px] w-full animate-pulse rounded-lg bg-slate-400" />
-            <div className="h-[12px] w-5/6 animate-pulse rounded-lg bg-slate-400" />
-            <div className="h-[12px] w-5/6 animate-pulse rounded-lg bg-slate-400" />
-            <div className="h-[14px] w-3/6 animate-pulse rounded-lg bg-slate-400" />
+            <div className="h-[14px] w-full animate-pulse rounded-lg bg-slate-400 dark:bg-slate-800" />
+            <div className="h-[12px] w-5/6 animate-pulse rounded-lg bg-slate-400 dark:bg-slate-800" />
+            <div className="h-[12px] w-5/6 animate-pulse rounded-lg bg-slate-400 dark:bg-slate-800" />
+            <div className="h-[14px] w-3/6 animate-pulse rounded-lg bg-slate-400 dark:bg-slate-800" />
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
