ff00f7720c2ea5b612f8c0ee7dc0372663a53c8f
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:19:51 +0700

    refactor: change the ui to only show the image and the title of the movie

--- modules/adminPages/moviesPage/CinemaMovies/components/CinemaMovieDetailsModal.jsx
+++ modules/adminPages/moviesPage/CinemaMovies/components/CinemaMovieDetailsModal.jsx
@@ -1,34 +1,29 @@
-import { query } from "@/components/reactQuery/queries/query";
-import { TableRowDetailsModal } from "@/components/TableRowDetailsModal";
+import Image from "next/image"
+
+import { query } from "@/components/reactQuery/queries/query"
+import { TableRowDetailsModal } from "@/components/TableRowDetailsModal"
 
 export const CinemaMovieDetailsModal = ({ closeModal, movieId }) => {
   const movieDetails = query({
     queryKey: ["movieDetails", movieId],
     url: `/api/movies/${movieId}`,
-  });
+  })
 
   return (
     <TableRowDetailsModal title="Movie Details" closeModal={closeModal}>
-      <div className="flex flex-col gap-1">
-        <span className="text-sm">Title</span>
-        <h5>{movieDetails.data?.title}</h5>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-sm">Casts</span>
-        <h5>{movieDetails.data?.casts}</h5>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-sm">Casts</span>
-        <h5>{movieDetails.data?.duration}</h5>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-sm">Synopsis</span>
-        <p className="tracking-wide">{movieDetails.data?.synopsis}</p>
-      </div>
-      <div className="flex flex-col gap-1">
-        <span className="text-sm">Watched Amount</span>
-        <p className="tracking-wide">{movieDetails.data?.watchedAmount}</p>
+      <div className="space-y-4">
+        <div className="flex flex-col items-center gap-2">
+          <h3>{movieDetails.data?.title}</h3>
+
+          <Image
+            src={movieDetails.data?.image_url}
+            alt="movie-image"
+            className="rounded-lg shadow-sm"
+            width={250}
+            height={300}
+          />
+        </div>
       </div>
     </TableRowDetailsModal>
-  );
-};
+  )
+}
