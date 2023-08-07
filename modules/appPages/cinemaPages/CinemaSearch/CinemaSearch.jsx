072e7dd5793c2869b98145f7318fbc95d1a6f6ae
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 19:56:06 +0700

    refactor: change title font style & add dark theme styles for cinema list

--- modules/appPages/cinemaPages/CinemaSearch/CinemaSearch.jsx
+++ modules/appPages/cinemaPages/CinemaSearch/CinemaSearch.jsx
@@ -13,10 +13,12 @@ export const CinemaSearch = () => {
 
   if (!isConnected) return <WalletNotConnected />;
   return (
-    <AnimatedContainer className="flex flex-1 flex-col items-start gap-4 bg-white p-5 dark:bg-slate-800 lg:p-10">
+    <AnimatedContainer className="flex flex-1 flex-col items-start gap-4 p-5 lg:p-10">
       <div className="w-full">
         <div className="mb-4 space-y-1.5 text-start">
-          <h1 className="font-poppins text-base lg:text-3xl">Search Cinemas</h1>
+          <h1 className="font-openSans font-medium text-base lg:text-3xl">
+            Search Cinemas
+          </h1>
           <p className="text-xs lg:text-base">
             Search for cinema of your choosing
           </p>
@@ -27,7 +29,7 @@ export const CinemaSearch = () => {
             onChange={(event) => {
               setTimeout(() => setSearch(event.target.value), 1000);
             }}
-            className="w-full rounded-lg border bg-slate-100 p-2 text-xs focus:outline-none dark:bg-slate-600 lg:text-base"
+            className="w-full rounded-lg border dark:border-slate-700 dark:bg-slate-900 bg-white p-2 text-xs focus:outline-none lg:text-base"
             placeholder="Search cinema..."
           />
           <CinemaList search={search} />
