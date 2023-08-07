038c9f019cf0621b516e0f72260ded1fc577ce94
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 15:52:21 +0700

    refactor: fix typo in cinema inside include in GET method

--- pages/api/regions/[regionId]/index.js
+++ pages/api/regions/[regionId]/index.js
@@ -1,26 +1,24 @@
-import { prisma } from "lib/prisma";
+import { prisma } from "lib/prisma"
 
 export default async function regionDetailsHandler(req, res) {
-  const { regionId } = req.query;
+  const { regionId } = req.query
   if (req.method === "GET") {
     const regionDetails = await prisma.region.findUnique({
       where: { id: parseInt(regionId) },
-      include: { cinema: true },
-    });
-    res.status(200).json(regionDetails);
+      include: { cinemas: true },
+    })
+    res.status(200).json(regionDetails)
   }
   if (req.method === "PUT") {
-    const { name } = req.body;
+    const { name } = req.body
     try {
       await prisma.region.update({
         where: { id: parseInt(regionId) },
         data: { name },
-      });
-      res
-        .status(200)
-        .json({ code: 200, message: "Succesfully updated region" });
+      })
+      res.status(200).json({ code: 200, message: "Succesfully updated region" })
     } catch (error) {
-      res.status(500).json({ code: 500, message: error });
+      res.status(500).json({ code: 500, message: error })
     }
   }
 }
