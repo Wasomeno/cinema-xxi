d8223f65e02d4d54ace1560a8d5a3bea82568645
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 15:45:31 +0700

    feat: add theme switcher component, used for changing the theme

---
+++ components/ThemeSwitcher.jsx
@@ -0,0 +1,22 @@
+import { useTheme } from "next-themes"
+import { BsSun } from "react-icons/bs"
+import { FaRegMoon } from "react-icons/fa"
+
+export const ThemeSwitcher = () => {
+  const { setTheme, resolvedTheme } = useTheme()
+
+  return (
+    <button
+      onClick={() => {
+        if (resolvedTheme === "dark") {
+          setTheme("light")
+        } else {
+          setTheme("dark")
+        }
+      }}
+    >
+      {resolvedTheme === "light" && <BsSun size={20} />}
+      {resolvedTheme === "dark" && <FaRegMoon size={20} />}
+    </button>
+  )
+}
