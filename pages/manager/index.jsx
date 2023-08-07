7972e2ab4d9a27a80891371f399e42025ad1f82e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:03:11 +0700

    refactor: remove unnecessary clasess in parent div

--- pages/manager/index.jsx
+++ pages/manager/index.jsx
@@ -1,13 +1,13 @@
-import { Pie } from "react-chartjs-2";
+import { Pie } from "react-chartjs-2"
 
-import AnimatedContainer from "@/components/AnimatedContainer";
-import { ChartComponent } from "@/components/ChartComponent";
-import ManagerHeader from "@/components/Headers/ManagerHeader";
-import { ManagerLayout } from "@/components/Layouts/ManagerLayout";
+import AnimatedContainer from "@/components/AnimatedContainer"
+import { ChartComponent } from "@/components/ChartComponent"
+import ManagerHeader from "@/components/Headers/ManagerHeader"
+import { ManagerLayout } from "@/components/Layouts/ManagerLayout"
 
 const Dashboard = () => {
   return (
-    <div className="flex flex-1 flex-col gap-2 overflow-y-scroll rounded-lg border bg-white p-4 shadow-[0_3px_10px_rgb(0,0,0,0.2)] dark:border-slate-500 dark:bg-slate-700">
+    <div className="flex flex-1 flex-col gap-2 overflow-y-scroll p-4">
       <ManagerHeader>Dashboard</ManagerHeader>
       <AnimatedContainer className="flex h-full justify-center">
         <div className="w-4/6 lg:w-2/6">
@@ -26,13 +26,13 @@ const Dashboard = () => {
         </div>
       </AnimatedContainer>
     </div>
-  );
-};
+  )
+}
 
 export default function DashboardPage() {
   return (
     <ManagerLayout pageTitle="Dashboard">
       <Dashboard />
     </ManagerLayout>
-  );
+  )
 }
