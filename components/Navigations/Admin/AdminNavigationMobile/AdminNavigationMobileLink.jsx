012103b72e578cf21726c47d0b01eb6958b197b0
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:15:51 +0700

    refactor: replace activeRoute props with pathname from useRouter,

--- components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobileLink.jsx
+++ components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobileLink.jsx
@@ -1,15 +1,12 @@
-import Link from "next/link";
-import { twMerge } from "tailwind-merge";
+import Link from "next/link"
+import { useRouter } from "next/router"
+import { twMerge } from "tailwind-merge"
 
-export const AdminNavigationMobileLink = ({
-  href,
-  children,
-  Icon,
-  activeRoute,
-}) => {
+export const AdminNavigationMobileLink = ({ href, children, Icon }) => {
+  const { pathname } = useRouter()
   return (
     <Link
-      href={"/admin/" + (href === "admin" ? "" : href)}
+      href={href}
       className="flex h-16 flex-col items-center justify-center gap-1.5 transition duration-300 ease-in-out"
       prefetch={false}
     >
@@ -17,18 +14,18 @@ export const AdminNavigationMobileLink = ({
         <Icon
           className={twMerge(
             "h-4 w-4 text-slate-400 dark:text-slate-50 sm:h-6 sm:w-6",
-            activeRoute.includes(href) && "text-blue-500 dark:text-blue-300"
+            pathname === href && "text-blue-500 dark:text-blue-300"
           )}
         />
       </span>
       <span
         className={twMerge(
           "w-24 text-center font-poppins text-xs font-normal tracking-wide text-slate-400 dark:font-light dark:text-slate-50 sm:text-sm",
-          activeRoute.includes(href) && "text-blue-500 dark:text-blue-300"
+          pathname === href && "text-blue-500 dark:text-blue-300"
         )}
       >
         {children}
       </span>
     </Link>
-  );
-};
+  )
+}
