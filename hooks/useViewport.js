1046ac3c38a00f854ae5bcb95274419a2fc800a2
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 19:42:23 +0700

    refactor: skip the curly braces and return currentViewport with the spread operator

--- hooks/useViewport.js
+++ hooks/useViewport.js
@@ -1,19 +1,23 @@
 import { useLayoutEffect, useState } from "react";
 
 export const useViewport = () => {
-  const [viewport, setViewport] = useState({ width: 0, height: 0 });
+  const [viewport, setViewport] = useState({
+    width: 0,
+    height: 0,
+  });
+
   useLayoutEffect(() => {
-    setViewport((currentViewport) => {
-      currentViewport.width = window.innerWidth;
-      currentViewport.height = window.innerHeight;
-      return { ...currentViewport };
-    });
+    setViewport((currentViewport) => ({
+      ...currentViewport,
+      width: window.innerWidth,
+      height: window.innerHeight,
+    }));
     window.addEventListener("resize", () =>
-      setViewport((currentViewport) => {
-        currentViewport.width = window.innerWidth;
-        currentViewport.height = window.innerHeight;
-        return { ...currentViewport };
-      })
+      setViewport((currentViewport) => ({
+        ...currentViewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      }))
     );
   }, []);
 
