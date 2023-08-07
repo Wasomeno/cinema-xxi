6e303c92f6ef97ae1754dc13285950d6a7ebce36
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 18:59:57 +0700

    refactor: remove useState, useRouter, useEffect & remove showNav props in ManagerNavigationLink

--- components/Navigations/Manager/ManagerNavigation/ManagerNavigation.jsx
+++ components/Navigations/Manager/ManagerNavigation/ManagerNavigation.jsx
@@ -1,46 +1,25 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { HiGlobe, HiHome } from "react-icons/hi";
-import { MdLocalMovies } from "react-icons/md";
+import { useRouter } from "next/router"
+import { HiGlobe, HiHome } from "react-icons/hi"
+import { MdLocalMovies } from "react-icons/md"
 
-import { ManagerNavigationLink } from "./ManagerNavigationLink";
+import { ManagerNavigationLink } from "./ManagerNavigationLink"
 
 export const ManagerNavigation = () => {
-  const [showNav, setShowNav] = useState(false);
-
-  const { pathname } = useRouter();
-
-  useEffect(() => {
-    setShowNav(false);
-  }, [pathname]);
-
   return (
-    <div className="sticky z-10 rounded-lg border bg-white dark:border-slate-500 dark:bg-gray-700">
+    <div className="sticky z-10 rounded-lg border bg-white dark:border-slate-700 dark:bg-slate-900">
       <div className="flex w-[240px] flex-col justify-start">
         <div className="flex h-3/6 flex-col justify-start gap-3 overflow-hidden p-3">
-          <ManagerNavigationLink
-            href="/manager"
-            Icon={HiHome}
-            showNav={showNav}
-          >
+          <ManagerNavigationLink href="/manager" Icon={HiHome}>
             Dashboard
           </ManagerNavigationLink>
-          <ManagerNavigationLink
-            href="/manager/movies"
-            Icon={MdLocalMovies}
-            showNav={showNav}
-          >
+          <ManagerNavigationLink href="/manager/movies" Icon={MdLocalMovies}>
             Movies
           </ManagerNavigationLink>
-          <ManagerNavigationLink
-            href="/manager/regions"
-            Icon={HiGlobe}
-            showNav={showNav}
-          >
+          <ManagerNavigationLink href="/manager/regions" Icon={HiGlobe}>
             Regions
           </ManagerNavigationLink>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
