11fff691490a174cc6046079ae101d6c37d3b88a
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 17:28:29 +0700

    refactor: change the ui

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
 import Head from "next/head";
 import Link from "next/link";
+import { BsArrowRight } from "react-icons/bs";
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
+    <div className="flex p-4 h-screen gap-5 flex-col items-center justify-center">
       <Head>
         <title>Cinema App</title>
       </Head>
-      <div className="m-4 p-2">
-        <h1 className="font-inter text-lg">Cinema Ticket Booking App</h1>
-      </div>
-      <div className="w-5/6 text-center">
-        <p className="font-poppins text-sm">
+      <div className="text-center space-y-2">
+        <h1 className="font-semibold font-poppins text-lg lg:text-4xl">
+          Cinema Ticket Booking App
+        </h1>
+        <p className="font-openSans lg:text-base text-xs">
           This is my attempt on making a Cinema Ticket Booking App
         </p>
       </div>
       <Link
         href="app"
-        className="m-5 w-40 rounded-full border-2 border-solid border-black bg-slate-900 p-2 text-center text-white transition duration-300 ease-in-out hover:scale-x-105 hover:bg-white hover:text-black"
+        className="p-2 w-24 border dark:border-white border-slate-900 lg:w-32 flex justify-center items-center gap-2 rounded-full transition hover:scale-[105%] duration-200 hover:text-white hover:bg-slate-900 dark:hover:text-slate-900 dark:hover:bg-white text-slate-800 dark:text-white font-semibold"
       >
-        Go To App
+        <span className="lg:text-base text-sm">App</span>
+        <BsArrowRight size="18" />
       </Link>
     </div>
   );
