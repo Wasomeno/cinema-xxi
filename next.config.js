dc1a5ecbfd4835de098472b5d21eaf4bbb7eacad
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 19:49:00 +0700

    chore: remove imdb-api image url

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
-});
+})
 
 const env = {
   ROLES_CONTRACT_ADDRESS: "0x6986f8ac26ed37c46dd8c166450d9a0cc464ade7",
   TICKET_CONTRACT_ADDRESS: "0x16cee9a9dd6e65ce9ec600f1b2bd0a7bf99dc6ff",
   NEXTAUTH_SECRET: "5c3d24485bb838879e06f5bebca5652f",
   IMDB_API_KEY: "k_96rtl2kc",
-};
+}
 
 module.exports = withBundleAnalyzer({
   images: {
@@ -16,12 +16,6 @@ module.exports = withBundleAnalyzer({
       {
         protocol: "https",
         hostname: "m.media-amazon.com",
-        port: "",
-        pathname: "/images/**",
-      },
-      {
-        protocol: "https",
-        hostname: "imdb-api.com",
         port: "",
         pathname: "/images/**",
       },
@@ -36,4 +30,4 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   swcMinify: true,
   env: env,
-});
+})
