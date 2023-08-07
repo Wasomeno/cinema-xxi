eeada67776111262f71145d95e705b5ce01662c7
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 17:58:28 +0700

    refactor: replace repeated AdminNavigaitonMobileLink with adminRoutes map

--- components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobile.jsx
+++ components/Navigations/Admin/AdminNavigationMobile/AdminNavigationMobile.jsx
@@ -1,50 +1,18 @@
-import { useRouter } from "next/router";
-import { BsPersonLinesFill } from "react-icons/bs";
-import { HiHome } from "react-icons/hi2";
-import { IoTimeOutline } from "react-icons/io5";
-import { MdMeetingRoom, MdOutlineMovie } from "react-icons/md";
-
-import { AdminNavigationMobileLink } from "./AdminNavigationMobileLink";
+import { adminRoutes } from "../AdminNavigation/AdminNavigation"
+import { AdminNavigationMobileLink } from "./AdminNavigationMobileLink"
 
 export const AdminNavigationMobile = () => {
-  const { pathname } = useRouter();
   return (
-    <div className="sticky bottom-0 z-10 flex w-screen items-center justify-start overflow-x-scroll border bg-white shadow-md dark:bg-slate-700 sm:justify-evenly sm:p-1.5 lg:hidden">
-      <AdminNavigationMobileLink
-        href="admin"
-        Icon={HiHome}
-        activeRoute={pathname}
-      >
-        Dashboard
-      </AdminNavigationMobileLink>
-      <AdminNavigationMobileLink
-        href="movies"
-        Icon={MdOutlineMovie}
-        activeRoute={pathname}
-      >
-        Movies
-      </AdminNavigationMobileLink>
-      <AdminNavigationMobileLink
-        href="showtimes"
-        Icon={IoTimeOutline}
-        activeRoute={pathname}
-      >
-        Showtimes
-      </AdminNavigationMobileLink>
-      <AdminNavigationMobileLink
-        href="studios"
-        Icon={MdMeetingRoom}
-        activeRoute={pathname}
-      >
-        Studios
-      </AdminNavigationMobileLink>
-      <AdminNavigationMobileLink
-        href="admins"
-        Icon={BsPersonLinesFill}
-        activeRoute={pathname}
-      >
-        Admins
-      </AdminNavigationMobileLink>
+    <div className="sticky bottom-0 z-10 flex w-screen items-center justify-start overflow-x-scroll border-t bg-white shadow-md dark:border-slate-700 dark:bg-slate-900  sm:justify-evenly sm:p-1.5 lg:hidden">
+      {adminRoutes.map((route) => (
+        <AdminNavigationMobileLink
+          key={route.title}
+          href={route.href}
+          Icon={route.icon}
+        >
+          {route.title}
+        </AdminNavigationMobileLink>
+      ))}
     </div>
-  );
-};
+  )
+}
