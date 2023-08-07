b8e3e350f5236ec656250baef1d275f60c920921
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 15:53:08 +0700

    refactor: change selectedMovies to title and image_url in POST method

--- pages/api/movies/index.js
+++ pages/api/movies/index.js
@@ -1,20 +1,18 @@
-import { prisma } from "lib/prisma";
+import { prisma } from "lib/prisma"
 
 export default async function moviesHandler(req, res) {
   if (req.method === "GET") {
-    const movies = await prisma.movie.findMany();
-    res.status(200).json(movies);
+    const movies = await prisma.movie.findMany()
+    res.status(200).json(movies)
   } else if (req.method === "POST") {
-    const { selectedMovies } = req.body;
+    const { title, image_url } = req.body
     try {
       await prisma.movie.createMany({
-        data: [...selectedMovies],
-      });
-      res
-        .status(200)
-        .json({ code: "200", message: "Successfully Added Movie" });
+        data: { title, image_url },
+      })
+      res.status(200).json({ code: "200", message: "Successfully Added Movie" })
     } catch (error) {
-      await res.status(500).send(error.message);
+      await res.status(500).send(error.message)
     }
   }
 }
