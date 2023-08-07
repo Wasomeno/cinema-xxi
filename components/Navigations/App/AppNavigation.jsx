75509f581fc3b693fbbda33f2644a3abbc098fcd
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-07 19:45:02 +0700

    refactor: replace repetitive Link with array of needed props

--- components/Navigations/App/AppNavigation.jsx
+++ components/Navigations/App/AppNavigation.jsx
@@ -1,67 +1,70 @@
-import { AnimatePresence } from "framer-motion";
-import useToggle from "hooks/useToggle";
-import { useUserConnectionDetails } from "hooks/useUserConnectionDetails";
-import dynamic from "next/dynamic";
-import Link from "next/link";
+import Link from "next/link"
+import { AnimatePresence } from "framer-motion"
+import useToggle from "hooks/useToggle"
+import { useUserConnectionDetails } from "hooks/useUserConnectionDetails"
 
-import { ConnectWalletModal } from "./ConnectWalletModal";
+import UserMenuModal from "@/components/UserMenuModal"
 
-const UserMenuModal = dynamic(() => import("../../UserMenuModal"));
+import { ConnectWalletModal } from "./ConnectWalletModal"
+
+export const appPaths = [
+  {
+    text: "Home",
+    href: "/app",
+  },
+  {
+    text: "Cinemas",
+    href: "/app/cinemas",
+  },
+  {
+    text: "Transactions",
+    href: "/app/transactions",
+  },
+]
 
 const AppNavigation = () => {
-  const { isConnected, user } = useUserConnectionDetails();
-  const [showWalletModal, toggleShowWalletModal] = useToggle(false);
-  const [showUserModal, toggleShowUserModal] = useToggle(false);
+  const { isConnected, user } = useUserConnectionDetails()
+  const [showWalletModal, toggleShowWalletModal] = useToggle(false)
+  const [showUserModal, toggleShowUserModal] = useToggle(false)
 
   return (
-    <>
-      <div className="sticky left-0 top-0 z-10 flex w-full items-center justify-between border-b bg-slate-50 p-2 dark:bg-slate-900 lg:h-16">
-        <div className="w-2/12 p-2 text-center">
-          <span className="font-inter border-2 border-slate-900 bg-slate-50 px-2 text-xs tracking-widest text-slate-900 sm:text-base md:text-base">
-            XXI
-          </span>
-        </div>
-        <div className="hidden w-4/12 justify-evenly gap-5 md:flex">
+    <div className="sticky top-0 z-10 flex w-full items-center justify-between border-b px-4 py-2 dark:border-b-slate-800 lg:h-16">
+      <div className="w-auto p-2 text-center lg:w-1/12">
+        <span className="font-inter border-2 border-slate-900 bg-slate-50 px-2 text-xs tracking-widest text-slate-900 sm:text-base md:text-base">
+          XXI
+        </span>
+      </div>
+      <div className="hidden w-2/6 justify-evenly gap-5 md:flex">
+        {appPaths.map((path) => (
           <Link
-            href="/app"
-            className="w-1/6 p-2 text-center font-poppins text-xs tracking-wider md:text-sm"
+            key={path.text}
+            href={path.href}
+            className="p-2 text-center font-poppins text-xs tracking-wider md:text-sm"
           >
-            Home
+            {path.text}
           </Link>
-          <Link
-            href="/app/cinemas"
-            className="w-1/6 p-2 text-center font-poppins text-xs tracking-wider md:text-sm"
+        ))}
+      </div>
+      <div className="w-auto text-center">
+        {isConnected ? (
+          <button
+            onClick={() => toggleShowUserModal()}
+            className="flex items-center justify-center gap-2 rounded-full border border-transparent p-1 transition duration-300 ease-in-out"
           >
-            Cinemas
-          </Link>
-          <Link
-            href="/app/transactions"
-            className="w-1/6 p-2 text-center font-poppins text-xs tracking-wider md:text-sm"
+            <div className="h-8 w-8 rounded-full border-2 border-slate-300 bg-blue-400 md:h-8 md:w-8" />
+            <span className="hidden font-poppins text-xs sm:hidden sm:text-sm md:block">
+              {user.slice(0, 6)}...
+              {user.slice(-6, -1) + user.slice(-1)}
+            </span>
+          </button>
+        ) : (
+          <button
+            onClick={() => toggleShowWalletModal()}
+            className="w-20 rounded-md bg-slate-900 p-1.5 font-openSans text-xs font-medium text-white dark:bg-white dark:text-slate-950 lg:text-xs"
           >
-            Transactions
-          </Link>
-        </div>
-        <div className="w-auto text-center lg:w-2/12">
-          {isConnected ? (
-            <button
-              onClick={() => toggleShowUserModal()}
-              className="flex w-full items-center justify-center gap-2 rounded-full border border-transparent p-1 transition duration-300 ease-in-out"
-            >
-              <span className="h-8 w-8 rounded-full border-2 border-slate-300 bg-blue-400 md:h-8 md:w-8" />
-              <p className="hidden font-poppins text-xs sm:hidden sm:text-sm md:block">
-                {user.slice(0, 6)}...
-                {user.slice(-6, -1) + user.slice(-1)}
-              </p>
-            </button>
-          ) : (
-            <button
-              onClick={() => toggleShowWalletModal()}
-              className="rounded-lg bg-slate-900 p-2 px-4 font-poppins text-xs lg:text-sm tracking-wider text-white dark:bg-slate-700"
-            >
-              Connect
-            </button>
-          )}
-        </div>
+            Connect
+          </button>
+        )}
       </div>
       <AnimatePresence>
         {showWalletModal && (
@@ -71,8 +74,8 @@ const AppNavigation = () => {
           <UserMenuModal toggleShowUserModal={toggleShowUserModal} />
         )}
       </AnimatePresence>
-    </>
-  );
-};
+    </div>
+  )
+}
 
-export default AppNavigation;
+export default AppNavigation
--- components/Navigations/App/AppNavigationMobile.jsx
+++ components/Navigations/App/AppNavigationMobile.jsx
@@ -1,101 +1,64 @@
-import clsx from "clsx";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { HiOutlineHome } from "react-icons/hi";
-import { HiOutlineRectangleStack, HiOutlineTicket } from "react-icons/hi2";
-import { twMerge } from "tailwind-merge";
+import Link from "next/link"
+import { useRouter } from "next/router"
+import clsx from "clsx"
+import {
+  HiOutlineHome,
+  HiOutlineRectangleStack,
+  HiOutlineTicket,
+} from "react-icons/hi2"
+import { twMerge } from "tailwind-merge"
+
+const appPaths = [
+  {
+    text: "Home",
+    href: "/app",
+    Icon: HiOutlineHome,
+  },
+  {
+    text: "Cinemas",
+    href: "/app/cinemas",
+    Icon: HiOutlineRectangleStack,
+  },
+  {
+    text: "Transactions",
+    href: "/app/transactions",
+    Icon: HiOutlineTicket,
+  },
+]
 
 const AppNavigationMobile = () => {
-  const { pathname, query } = useRouter();
-  const currentPath = pathname.split("/")[2];
+  const { pathname } = useRouter()
   return (
-    <div className="sticky bottom-0 z-10 flex h-16 w-full items-center justify-evenly border-t bg-slate-50 shadow-md backdrop-blur-md dark:bg-slate-600 md:hidden">
-      <Link
-        href="/app"
-        className="flex h-full w-3/6 flex-col items-center justify-center gap-1 transition duration-200"
-      >
-        <HiOutlineHome
-          size="18"
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              !currentPath || query.movieId
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
-        />
-        <span
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              !currentPath || query.movieId
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
+    <div className="sticky bottom-0 z-10 flex h-16 w-full items-center justify-evenly border-t shadow-md backdrop-blur-md dark:border-slate-700 dark:bg-slate-900 md:hidden">
+      {appPaths.map((path) => (
+        <Link
+          key={path.text}
+          href={path.href}
+          className="flex h-full w-3/6 flex-col items-center justify-center gap-1 transition duration-200"
         >
-          Home
-        </span>
-      </Link>
-      <Link
-        href="/app/cinemas"
-        className="flex h-full w-3/6 flex-col items-center justify-center gap-1 transition duration-200"
-      >
-        <HiOutlineRectangleStack
-          size="18"
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              currentPath === "cinemas"
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
-        />
-        <span
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              currentPath === "cinemas"
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
-        >
-          Cinemas
-        </span>
-      </Link>
-      <Link
-        href="/app/transactions"
-        className="flex h-full w-3/6 flex-col items-center justify-center gap-1 transition duration-200"
-      >
-        <HiOutlineTicket
-          size="18"
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              currentPath === "transactions"
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
-        />
-        <span
-          className={twMerge(
-            clsx(
-              "font-poppins text-xs tracking-wider",
-              currentPath === "transactions"
-                ? "text-blue-500 dark:text-blue-300"
-                : "text-slate-400 dark:text-slate-50"
-            )
-          )}
-        >
-          Transactions
-        </span>
-      </Link>
+          <path.Icon
+            size="18"
+            className={twMerge(
+              clsx(
+                "font-poppins text-xs tracking-wider text-slate-400 dark:text-slate-50",
+                pathname === path.href && "text-blue-500 dark:text-blue-300"
+              )
+            )}
+          />
+          <span
+            className={twMerge(
+              clsx(
+                "font-poppins text-xs tracking-wider text-slate-400 dark:text-slate-50",
+                pathname === path.href && "text-blue-500 dark:text-blue-300"
+              )
+            )}
+          >
+            {path.text}
+          </span>
+        </Link>
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default AppNavigationMobile;
+export default AppNavigationMobile
