b2c6f92eed700a06ea77a3075ddba131a173dc3e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-08 22:14:23 +0700

    refactor: add movie image & change the font style of the title

--- modules/appPages/cinemaPages/CinemaMoviesPage/CinemaMovieList.jsx
+++ modules/appPages/cinemaPages/CinemaMoviesPage/CinemaMovieList.jsx
@@ -1,44 +1,50 @@
-import Link from "next/link";
-import { HiChevronLeft } from "react-icons/hi2";
+import Image from "next/image"
+import Link from "next/link"
+import { BsArrowLeft, BsArrowLeftShort } from "react-icons/bs"
 
-import AnimatedContainer from "@/components/AnimatedContainer";
+import AnimatedContainer from "@/components/AnimatedContainer"
 
 export const CinemaMovieList = ({ cinemaDetails }) => {
   return (
-    <AnimatedContainer className="flex h-screen justify-center overflow-y-scroll p-4">
-      <div className="w-full lg:w-4/6">
-        <div className="my-2">
-          <div className="grid w-full grid-cols-7 items-center justify-center gap-2">
-            <span className="col-span-3 col-start-2 col-end-7 text-center font-poppins text-sm">
-              {cinemaDetails.name}
-            </span>
-          </div>
-        </div>
-        <div className="mt-4">
-          <div className="flex w-full flex-col items-center justify-start gap-2">
-            {!cinemaDetails.movies.length ? (
-              <div className="flex h-40 items-center justify-center">
-                <p className="font-poppins text-xs lg:text-sm">
-                  No Active Movies
-                </p>
-              </div>
-            ) : (
-              cinemaDetails.movies.map((movie) => (
-                <Link
-                  key={movie.id}
-                  href={"/app/" + cinemaDetails.regionId + "/" + movie.id}
-                  className="flex w-full items-center justify-center gap-2 rounded-md bg-slate-100 p-2"
-                >
-                  <div className="h-40 w-4/12 rounded-lg bg-slate-500" />
-                  <div className="w-8/12">
-                    <p className="font-poppins text-sm">{movie.title}</p>
-                  </div>
-                </Link>
-              ))
-            )}
-          </div>
+    <AnimatedContainer>
+      <div className="sticky top-[59px] flex items-center gap-2 bg-white px-5 py-4 dark:bg-slate-950 lg:static lg:px-10">
+        <button className="lg:hidden">
+          <BsArrowLeftShort size={24} />
+        </button>
+        <h1 className=" font-poppins font-semibold lg:text-3xl">
+          {cinemaDetails.name}
+        </h1>
+      </div>
+      <div className="px-5 py-2 lg:px-10">
+        <div className="flex w-full flex-col gap-2">
+          {!cinemaDetails.movies.length ? (
+            <div className="flex h-40 items-center justify-center">
+              <p className="font-poppins text-xs lg:text-sm">
+                No Active Movies
+              </p>
+            </div>
+          ) : (
+            cinemaDetails.movies.map((movie) => (
+              <Link
+                key={movie.id}
+                href={"/app/" + cinemaDetails.regionId + "/" + movie.id}
+                className="flex w-full items-center gap-4 rounded-md border bg-slate-100 p-3 dark:border-slate-700 dark:bg-slate-900 lg:w-2/6"
+              >
+                <Image
+                  src={movie.image_url}
+                  alt="movie-image"
+                  className="rounded-md"
+                  height={128}
+                  width={120}
+                />
+                <div className="w-8/12">
+                  <p className="font-poppins text-sm">{movie.title}</p>
+                </div>
+              </Link>
+            ))
+          )}
         </div>
       </div>
     </AnimatedContainer>
-  );
-};
+  )
+}
