b452ae3ac35cae4cbc179ab0224ebfb655107d4b
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-09 23:38:53 +0700

    style: move props to end of tag

--- components/AnimatedContainer.jsx
+++ components/AnimatedContainer.jsx
@@ -1,18 +1,17 @@
-import { motion } from "framer-motion";
-import React from "react";
+import { motion } from "framer-motion"
 
 const AnimatedContainer = ({ children, ...props }) => {
   return (
     <motion.div
-      {...props}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.25, ease: "easeInOut" }}
       exit={{ opacity: 0 }}
+      {...props}
     >
       {children}
     </motion.div>
-  );
-};
+  )
+}
 
-export default AnimatedContainer;
+export default AnimatedContainer
