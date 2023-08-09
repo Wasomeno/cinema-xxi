a377db932f1cf33364cecda73ba5dc0670d1e514
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-09 23:41:33 +0700

    refactor: replace AnimatedContainer with custom framer motion div

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -1,46 +1,35 @@
-import { AnimatePresence } from "framer-motion";
-import { HiCheckCircle } from "react-icons/hi";
-import { RiErrorWarningFill } from "react-icons/ri";
-import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion"
+import { useViewport } from "hooks/useViewport"
 
-import { useToastDetails } from "../stores/toastStore";
-import AnimatedContainer from "./AnimatedContainer";
+import { useToastDetails } from "../stores/toastStore"
 
 const Toast = () => {
-  const [show, text, condition] = useToastDetails();
+  const toast = useToastDetails()
+  const viewport = useViewport()
   return (
     <AnimatePresence>
-      {show && (
-        <AnimatedContainer
-          className={twMerge(
-            "fixed bottom-0 left-1/2 z-30 flex h-14 w-5/6 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-lg p-2 shadow-md lg:w-2/6",
-            condition === "success"
-              ? "bg-green-100 dark:bg-green-700"
-              : "bg-red-100 dark:bg-red-700"
-          )}
+      {toast.show && (
+        <motion.div
+          initial={{ opacity: 0, bottom: -5, scale: 0.3 }}
+          animate={{
+            opacity: 1,
+            bottom: viewport.width < 1024 ? 0 : 20,
+            scale: 1,
+          }}
+          exit={{ opacity: 0, bottom: -5, scale: 0.5 }}
+          style={{ translateX: "-50%" }}
+          className={
+            "fixed left-1/2 z-30 flex h-14 w-full items-center gap-4 rounded-t-lg border-t border-slate-300 bg-slate-50 px-4 text-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-slate-700 dark:bg-slate-950 dark:shadow-slate-800 lg:left-1/2 lg:w-80 lg:rounded-lg lg:border"
+          }
         >
-          <div className="w-1/6">
-            {condition === "success" ? (
-              <HiCheckCircle
-                size="18"
-                className="text-green-700 dark:text-green-500"
-              />
-            ) : (
-              <RiErrorWarningFill
-                size="18"
-                className="text-red-700 dark:text-red-500"
-              />
-            )}
-          </div>
-          <div className="w-4/6 text-center">
-            <p className="font-poppins text-xs text-slate-800 dark:font-medium dark:text-slate-100 lg:text-sm">
-              {text}
-            </p>
-          </div>
-        </AnimatedContainer>
+          <span>{toast.icon}</span>
+          <span className="font-poppins text-xs text-slate-800 dark:font-medium dark:text-slate-100 lg:text-sm">
+            {toast.text}
+          </span>
+        </motion.div>
       )}
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default Toast;
+export default Toast
