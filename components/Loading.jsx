954994438758d76a241ac0f763307532e9c5f0de
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-09 23:39:53 +0700

    refactor: replace ModalContainer with CenteredModalContainer

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,26 +1,23 @@
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence } from "framer-motion"
 
-import { useLoadingDetails } from "../stores/loadingStore";
-import AnimatedContainer from "./AnimatedContainer";
-import { ModalContainer } from "./ModalContainer";
-import { Spinner } from "./Spinner";
+import { useLoadingDetails } from "../stores/loadingStore"
+import { CenteredModalContainer } from "./ModalContainer"
+import { Spinner } from "./Spinner"
 
 const Loading = () => {
-  const [loading, loadingText] = useLoadingDetails();
+  const [loading, loadingText] = useLoadingDetails()
   return (
     <AnimatePresence>
       {loading && (
-        <ModalContainer toggleModal={() => null}>
-          <AnimatedContainer className="fixed left-1/2 top-1/2 z-30 flex h-72 w-64 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-5 rounded-lg bg-slate-100 shadow-md dark:bg-slate-800 lg:h-80 lg:w-72">
-            <span className="font-poppins text-sm font-medium tracking-wide lg:text-base">
-              {loadingText}
-            </span>
-            <Spinner />
-          </AnimatedContainer>
-        </ModalContainer>
+        <CenteredModalContainer className="flex h-3/6 flex-col items-center justify-center gap-5 border dark:border-slate-600 lg:h-80 lg:w-72 ">
+          <span className="font-poppins text-sm font-medium lg:text-base">
+            {loadingText}
+          </span>
+          <Spinner />
+        </CenteredModalContainer>
       )}
     </AnimatePresence>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
