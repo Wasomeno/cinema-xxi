1f2e9144f3000205d754d28201867fd3ad83b4e5
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-09 23:44:04 +0700

    refactor: replace toastSuccess & toastError with toast

--- components/reactQuery/mutations/useSideEffects.js
+++ components/reactQuery/mutations/useSideEffects.js
@@ -1,34 +1,34 @@
-import { queryClientApp } from "client/reactQueryClient";
-import { useRouter } from "next/router";
+import { useRouter } from "next/router"
+import { queryClientApp } from "client/reactQueryClient"
 
-import { useLoading } from "../../../stores/loadingStore";
-import { useToast } from "../../../stores/toastStore";
+import { useLoading } from "../../../stores/loadingStore"
+import { useToast } from "../../../stores/toastStore"
 
 export const useSideEffects = ({ text, queryKeys, redirectUrl }) => {
-  const [setLoading, setLoadingText] = useLoading();
-  const [toastSuccess, toastError] = useToast();
-  const router = useRouter();
+  const [setLoading, setLoadingText] = useLoading()
+  const toast = useToast()
+  const router = useRouter()
   return {
     onMutate: () => {
-      setLoadingText(text);
-      setLoading(true);
+      setLoadingText(text)
+      setLoading(true)
     },
     onError: (error) => {
-      let errorMessage;
+      let errorMessage
       if (error.reason) {
-        errorMessage = error.reason;
+        errorMessage = error.reason
       } else if (error.message) {
-        errorMessage = error.message;
+        errorMessage = error.message
       }
-      setLoading(false);
-      toastError(errorMessage);
+      setLoading(false)
+      toast.error(errorMessage)
     },
     onSuccess: async (response) => {
-      const responseJson = await response.json();
-      setLoading(false);
-      toastSuccess(responseJson.message);
-      redirectUrl && router.push(redirectUrl);
-      queryKeys && queryClientApp.invalidateQueries(queryKeys);
+      const responseJson = await response.json()
+      setLoading(false)
+      toast.success(responseJson.message)
+      redirectUrl && router.push(redirectUrl)
+      queryKeys && queryClientApp.invalidateQueries(queryKeys)
     },
-  };
-};
+  }
+}
