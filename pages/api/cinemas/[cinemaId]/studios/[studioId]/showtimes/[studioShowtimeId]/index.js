cfcfb15cbd36afd7c05c18659377a760bf38c6fa
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-10 23:51:10 +0700

    feat: add GET method handler

--- pages/api/cinemas/[cinemaId]/studios/[studioId]/showtimes/[studioShowtimeId]/index.js
+++ pages/api/cinemas/[cinemaId]/studios/[studioId]/showtimes/[studioShowtimeId]/index.js
@@ -1,22 +1,28 @@
-import { prisma } from "lib/prisma";
+import { prisma } from "lib/prisma"
 
 export default async function studioShowtimeHandler(req, res) {
-  const { studioShowtimeId } = req.query;
-  if (req.method === "PATCH") {
-    const { showtimeId, movieId } = req.body;
+  const { studioShowtimeId } = req.query
+  if (req.method === "GET") {
+    const studioShowtime = await prisma.showtimeToMovie.findUnique({
+      where: { id: parseInt(studioShowtimeId) },
+      include: { movie: true, showtime: true },
+    })
+    res.status(200).json(studioShowtime)
+  } else if (req.method === "PATCH") {
+    const { showtimeId, movieId } = req.body
     try {
       await prisma.showtimeToMovie.update({
         where: { id: parseInt(studioShowtimeId) },
         data: {
-          movie: { connect: { id: parseInt(movieId) } },
+          movie: { connect: { id: movieId } },
           showtime: { connect: { id: parseInt(showtimeId) } },
         },
-      });
+      })
       res
         .status(200)
-        .json({ code: 200, message: "Successfully updated studio showtime" });
+        .json({ code: 200, message: "Successfully updated studio showtime" })
     } catch (error) {
-      res.status(500).json({ code: 500, error });
+      res.status(500).json({ code: 500, error })
     }
   }
 }
