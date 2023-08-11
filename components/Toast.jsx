2b9ac92f6a508aa2fcee680a0ed8dd9b13e40cb3
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-11 23:04:51 +0700

    refactor: change border color when in dark mode

--- components/Toast.jsx
+++ components/Toast.jsx
@@ -19,7 +19,7 @@ const Toast = () => {
           exit={{ opacity: 0, bottom: -5, scale: 0.5 }}
           style={{ translateX: "-50%" }}
           className={
-            "fixed left-1/2 z-30 flex h-14 w-full items-center gap-4 rounded-t-lg border-t border-slate-300 bg-slate-50 px-4 text-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-slate-700 dark:bg-slate-950 dark:shadow-slate-800 lg:left-1/2 lg:w-80 lg:rounded-lg lg:border"
+            "fixed left-1/2 z-30 flex h-14 w-full items-center gap-4 rounded-t-lg border border-t bg-slate-50 px-4 text-center shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] dark:border-slate-800 dark:bg-slate-950 dark:shadow-slate-800 lg:left-1/2 lg:w-80 lg:rounded-lg lg:border"
           }
         >
           <span>{toast.icon}</span>
