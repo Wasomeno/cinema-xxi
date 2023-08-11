9844b2982296fc53e233891ba5ff0a2582671c3e
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-11 23:06:35 +0700

    refactor: filter fetched movies to movies that already connected to a cinema

--- pages/api/regions/[regionId]/movies/index.js
+++ pages/api/regions/[regionId]/movies/index.js
@@ -1,19 +1,40 @@
-import { prisma } from "lib/prisma";
+import { prisma } from "lib/prisma"
+import { TbSquareF9 } from "react-icons/tb"
 
 export default async function regionMoviesHandler(req, res) {
-  const { regionId } = req.query;
+  const { regionId } = req.query
   if (req.method === "GET") {
-    const regionCinemas = await prisma.region.findUnique({
+    const region = await prisma.region.findUnique({
       where: { id: parseInt(regionId) },
       select: {
         cinemas: {
-          select: { cinema_movie: { select: { movies: true } } },
+          select: {
+            cinema_movie: {
+              select: {
+                movies: {
+                  include: { _count: { select: { showtime_to_movie: true } } },
+                },
+              },
+            },
+          },
         },
       },
-    });
-    const movies = regionCinemas.cinemas.map((cinema) => cinema.cinema_movie);
+    })
+
+    const cinemaMovies = region.cinemas.filter(
+      (cinema) => cinema.cinema_movie?.movies.length > 0
+    )
+
     res
       .status(200)
-      .json(movies.flatMap((movie) => (movie === null ? [] : movie?.movies)));
+      .json(
+        !cinemaMovies.length
+          ? []
+          : cinemaMovies.flatMap((cinema) =>
+              cinema.cinema_movie.movies.filter(
+                (movie) => movie._count.showtime_to_movie > 0
+              )
+            )
+      )
   }
 }
