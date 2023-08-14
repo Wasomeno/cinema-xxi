c9ba8a88f6e69e6736c1af76d596391d64c56feb
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:24:03 +0700

    refactor: remove border

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -9,7 +9,7 @@ const Loading = () => {
   return (
     <AnimatePresence>
       {loading && (
-        <CenteredModalContainer className="flex h-3/6 flex-col items-center justify-center gap-5 border dark:border-slate-600 lg:h-80 lg:w-72 ">
+        <CenteredModalContainer className="flex h-3/6 flex-col items-center justify-center gap-5 lg:h-80 lg:w-72 ">
           <span className="font-poppins text-sm font-medium lg:text-base">
             {loadingText}
           </span>
