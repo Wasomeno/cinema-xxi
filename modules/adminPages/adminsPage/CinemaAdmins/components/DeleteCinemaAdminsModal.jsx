4d01119dd672f2cf929273ec10daf8598178af3f
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:33:39 +0700

    refactor: remove map on adminIds

--- modules/adminPages/adminsPage/CinemaAdmins/components/DeleteCinemaAdminsModal.jsx
+++ modules/adminPages/adminsPage/CinemaAdmins/components/DeleteCinemaAdminsModal.jsx
@@ -1,24 +1,24 @@
-import { useSession } from "next-auth/react";
+import { useSession } from "next-auth/react"
 
-import { DeleteDataModal } from "@/components/DeleteDataModal";
-import mutation from "@/components/reactQuery/mutations/mutation";
-import { useSideEffects } from "@/components/reactQuery/mutations/useSideEffects";
-import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys";
+import { DeleteDataModal } from "@/components/DeleteDataModal"
+import mutation from "@/components/reactQuery/mutations/mutation"
+import { useSideEffects } from "@/components/reactQuery/mutations/useSideEffects"
+import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys"
 
 export const DeleteCinemaAdminsModal = ({ closeModal, selectedAdmins }) => {
-  const session = useSession();
+  const session = useSession()
   const sideEffects = useSideEffects({
     text: "Deleting cinema admins",
     queryKeys: cinemaQueryKeys.cinemaAdmins(session.data?.user.cinemaId),
-  });
+  })
   const deleteCineaAdmins = mutation({
     url: `/api/cinemas/${session.data.user.cinemaId}/admins/delete`,
     method: "POST",
     body: {
-      adminIds: selectedAdmins.map((admin) => admin.id),
+      adminIds: selectedAdmins,
     },
     sideEffects,
-  });
+  })
   return (
     <DeleteDataModal
       title="Delete Cinema Admins"
@@ -26,5 +26,5 @@ export const DeleteCinemaAdminsModal = ({ closeModal, selectedAdmins }) => {
       closeModal={closeModal}
       deleteFunction={deleteCineaAdmins.mutate}
     />
-  );
-};
+  )
+}
