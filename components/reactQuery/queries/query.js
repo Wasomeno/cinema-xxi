060eaf3039244c471826acfa64b42beffb5502d9
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:53:04 +0700

    style: remove semicolons

--- components/reactQuery/queries/query.js
+++ components/reactQuery/queries/query.js
@@ -1,16 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery } from "@tanstack/react-query"
+import axios from "axios"
 
 export const query = ({ queryKey, url, enabledCondition }) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   return useQuery(
     queryKey,
     async () => {
-      const { data } = await axios.get(url);
-      return data;
+      const { data } = await axios.get(url)
+      return data
     },
     {
       enabled: enabledCondition,
     }
-  );
-};
+  )
+}
