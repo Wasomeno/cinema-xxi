8d678e5180897011c7c86d83d9e23b9095ff3226
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:26:22 +0700

    refactor: replace previous logo with new logo & remove search bar

--- components/Navigations/Admin/AdminToolbar.jsx
+++ components/Navigations/Admin/AdminToolbar.jsx
@@ -1,28 +1,20 @@
 import { useState } from "react"
+import Link from "next/link"
 import { AnimatePresence } from "framer-motion"
 
 import AdminMenuModal from "@/components/AdminMenuModal"
 import { ThemeSwitcher } from "@/components/ThemeSwitcher"
 
 export const AdminToolbar = () => {
-  const [search, setSearch] = useState("")
   const [showMenuModal, setShowMenuModal] = useState(false)
   return (
     <div className="sticky top-0 z-20 flex items-center justify-between border-b bg-white px-4 py-4 dark:border-b-slate-700 dark:bg-slate-900">
       <div className="flex w-5/6 items-center justify-start gap-4">
-        <div className="lg:w-[240px]">
-          <span className="border-2 border-slate-800 px-2 py-1.5 font-poppins text-xs font-semibold tracking-wider dark:border-slate-100 lg:px-4 lg:text-lg">
-            XXI
+        <Link href="/admin" className="text-center lg:w-28">
+          <span className="font-poppins text-base font-semibold tracking-tight lg:text-xl">
+            Cinema
           </span>
-        </div>
-        <div className="w-4/6 lg:w-3/6">
-          <input
-            type="text"
-            value={search}
-            onChange={(event) => setSearch(event.target.value)}
-            className="w-full rounded-lg border border-slate-300 bg-slate-50 p-2 text-xs dark:border-slate-700 dark:bg-slate-900 lg:text-sm"
-          />
-        </div>
+        </Link>
       </div>
 
       <div className="flex w-2/6 items-center justify-end gap-4">
--- components/Navigations/ManagerToolbar.jsx
+++ components/Navigations/ManagerToolbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Link from "next/link"
 import { AnimatePresence } from "framer-motion"
 
 import ManagerMenuModal from "../ManagerMenuModal"
@@ -9,18 +10,11 @@ export const ManagerToolbar = () => {
   return (
     <div className="flex items-center justify-between border-b bg-white px-4 py-4 dark:border-b-slate-700 dark:bg-slate-900">
       <div className="flex w-5/6 items-center justify-start gap-4">
-        <div className="lg:w-[240px]">
-          <span className="border-2 border-slate-800 px-2 py-1.5 font-poppins text-xs font-semibold tracking-wider dark:border-slate-100 lg:px-4 lg:text-lg">
-            XXI
+        <Link href="/manager" className="text-center lg:w-28">
+          <span className="font-poppins text-base font-semibold tracking-tight lg:text-xl">
+            Cinema
           </span>
-        </div>
-        <div className="w-4/6 lg:w-3/6">
-          <input
-            type="text  "
-            className="w-full rounded-lg border bg-slate-100 p-2 text-xs dark:border-slate-600 dark:bg-slate-800 lg:text-sm"
-            placeholder="Search for features..."
-          />
-        </div>
+        </Link>
       </div>
 
       <div className="flex w-2/6 justify-end gap-4">
