5e42c25bd34ee15f8b24565664ff60a203814a31
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:42:17 +0700

    refactor: change the background and border color

--- modules/appPages/moviePage/components/MovieDateSection.jsx
+++ modules/appPages/moviePage/components/MovieDateSection.jsx
@@ -1,14 +1,14 @@
-import clsx from "clsx";
-import { useDates } from "hooks/useDates";
-import { useEffect } from "react";
-import { twMerge } from "tailwind-merge";
+import { useEffect } from "react"
+import clsx from "clsx"
+import { useDates } from "hooks/useDates"
+import { twMerge } from "tailwind-merge"
 
 export default function MovieDateSection({ selectedDate, setSelectedDate }) {
-  const datesDetails = useDates(5);
+  const datesDetails = useDates(5)
 
   useEffect(() => {
-    setSelectedDate(datesDetails[0]);
-  }, []);
+    setSelectedDate(datesDetails[0])
+  }, [])
 
   return (
     <div className="w-11/12 space-y-2 md:w-8/12">
@@ -25,20 +25,27 @@ export default function MovieDateSection({ selectedDate, setSelectedDate }) {
             <button
               className={twMerge(
                 clsx(
-                  "h-10 w-10 cursor-pointer rounded-lg border border-slate-500 transition duration-300 ease-in-out dark:border-slate-500 dark:bg-slate-800 lg:h-14 lg:w-14 lg:rounded-lg",
+                  "h-10 w-10 cursor-pointer rounded-lg border bg-slate-300 opacity-50 transition duration-300 ease-in-out dark:border-slate-700 dark:bg-slate-800 lg:h-14 lg:w-14 lg:rounded-lg",
                   selectedDate?.date === details.date &&
-                    "bg-blue-200 text-slate-800 dark:bg-blue-300 border-slate-400 dark:border-slate-100 dark:text-black"
+                    "border-blue-200 bg-blue-300 text-slate-800 opacity-100 dark:border-blue-700 dark:bg-blue-800 dark:text-black"
                 )
               )}
             >
-              <span className="font-poppins text-xs font-medium">
+              <span className="font-poppins text-xs font-medium dark:text-slate-100">
                 {details.date}
               </span>
             </button>
-            <span className="font-poppins text-xs">{details.day}</span>
+            <span
+              className={twMerge(
+                "font-poppins text-xs opacity-50",
+                selectedDate?.date === details.date && "opacity-100"
+              )}
+            >
+              {details.day}
+            </span>
           </div>
         ))}
       </div>
     </div>
-  );
+  )
 }
