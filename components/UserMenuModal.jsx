3293298b1f9bf44634316b33e299f8936c1f7d7a
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-14 16:25:02 +0700

    refactor: replace theme switching section with ThemeSwitcher component

--- components/UserMenuModal.jsx
+++ components/UserMenuModal.jsx
@@ -1,20 +1,16 @@
-import { useTheme } from "next-themes"
 import { HiOutlineClipboardDocument, HiPower } from "react-icons/hi2"
 import { RiShareBoxFill } from "react-icons/ri"
 import { useToast } from "stores/toastStore"
-import { twMerge } from "tailwind-merge"
 import { useAccount, useBalance, useDisconnect } from "wagmi"
 
 import { ModalContainer } from "./ModalContainer"
+import { ThemeSwitcher } from "./ThemeSwitcher"
 
 const UserMenuModal = ({ toggleShowUserModal }) => {
   const toast = useToast()
   const { address, isDisconnected } = useAccount()
-  const { setTheme, theme, systemTheme } = useTheme()
   const { data: walletBalance } = useBalance({ address: address })
   const { disconnect } = useDisconnect()
-
-  const currentTheme = theme === "system" ? systemTheme : theme
 
   const iconWrapper = (Icon) => (
     <Icon size="16" className="text-slate-700 dark:text-slate-200" />
@@ -41,9 +37,9 @@ const UserMenuModal = ({ toggleShowUserModal }) => {
   return (
     <ModalContainer
       closeModal={toggleShowUserModal}
-      className="h-80 w-full gap-4 p-4 md:right-5 md:top-14 md:h-96 md:w-80"
+      className="h-80 w-full gap-4 border-t p-4 dark:border-slate-800 md:right-5 md:top-14 md:h-96 md:w-80 lg:border"
     >
-      <div className="mx-auto h-1 w-2/6 rounded-full bg-gray-400 bg-opacity-25" />
+      <div className="mx-auto h-1 w-2/6 rounded-full bg-gray-400 bg-opacity-25 lg:hidden" />
       <div className="flex w-full items-center justify-between">
         <div className="flex items-center justify-start gap-2.5 md:w-3/6">
           <div className="h-8 w-8 rounded-full border-2 bg-blue-400" />
@@ -55,26 +51,26 @@ const UserMenuModal = ({ toggleShowUserModal }) => {
         <div className="flex items-center justify-end gap-2 md:w-3/6">
           <button
             onClick={copyAddress}
-            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-700"
+            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-800"
           >
             {iconWrapper(HiOutlineClipboardDocument)}
           </button>
           <button
             onClick={openEtherScan}
-            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-700"
+            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-800"
           >
             {iconWrapper(RiShareBoxFill)}
           </button>
           <button
             onClick={disconnectWallet}
-            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-700"
+            className="rounded-lg bg-slate-200 p-2 dark:bg-slate-800"
           >
             {iconWrapper(HiPower)}
           </button>
         </div>
       </div>
       <div className="flex flex-col items-center gap-2">
-        <span className="font-poppins text-sm text-slate-900 dark:text-slate-50">
+        <span className="font-openSans text-sm text-slate-900 dark:text-slate-50">
           ETH Balance
         </span>
         <span className="font-poppins text-2xl tracking-wider text-slate-900 dark:text-slate-50">
@@ -82,30 +78,9 @@ const UserMenuModal = ({ toggleShowUserModal }) => {
         </span>
       </div>
       <div className="flex flex-col items-start gap-2">
-        <div className="flex w-full items-center justify-between rounded-lg bg-slate-200  p-3 font-poppins text-sm text-slate-900 shadow-sm dark:bg-slate-700 dark:text-slate-50">
+        <div className="flex w-full items-center justify-between rounded-lg bg-slate-200  p-3 font-openSans text-sm text-slate-900 shadow-sm dark:bg-slate-800 dark:text-slate-50">
           <span>Theme</span>
-          <button
-            onClick={() => {
-              if (currentTheme === "dark") {
-                setTheme("light")
-              } else {
-                setTheme("dark")
-              }
-            }}
-            className={twMerge(
-              "relative flex h-5 w-2/12 items-center justify-between rounded-full transition-all duration-300 ease-in-out",
-              currentTheme === "light" && "bg-gray-400",
-              currentTheme === "dark" && "bg-slate-800"
-            )}
-          >
-            <span
-              className={twMerge(
-                "absolute h-4 w-4 rounded-full bg-slate-50 shadow-md backdrop-blur-md transition-all duration-300 ease-in-out",
-                currentTheme === "light" && "left-0",
-                currentTheme === "dark" && "right-0"
-              )}
-            />
-          </button>
+          <ThemeSwitcher />
         </div>
       </div>
     </ModalContainer>
