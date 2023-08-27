a895519398367f21174889f3225b25ff722172ad
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-27 23:04:14 +0700

    refactor: add new image url remote pattern

--- next.config.js
+++ next.config.js
@@ -7,7 +7,6 @@ const env = {
   ROLES_CONTRACT_ADDRESS: "0x6986f8ac26ed37c46dd8c166450d9a0cc464ade7",
   TICKET_CONTRACT_ADDRESS: "0x16cee9a9dd6e65ce9ec600f1b2bd0a7bf99dc6ff",
   NEXTAUTH_SECRET: "5c3d24485bb838879e06f5bebca5652f",
-  IMDB_API_KEY: "k_96rtl2kc",
 }
 
 module.exports = withBundleAnalyzer({
@@ -24,6 +23,12 @@ module.exports = withBundleAnalyzer({
         hostname: "media.21cineplex.com",
         port: "",
         pathname: "/webcontent/**",
+      },
+      {
+        protocol: "https",
+        hostname: "ymudtpspkdopgzrtzevt.supabase.co",
+        port: "",
+        pathname: "/storage/**",
       },
     ],
   },
