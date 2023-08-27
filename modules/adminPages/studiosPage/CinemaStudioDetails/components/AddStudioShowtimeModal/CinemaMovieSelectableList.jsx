9a5afbd571dcada192b3a1862869c4c5fff4d117
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-27 22:36:37 +0700

    refactor: add dark mode background color and border color for showtimes & movies list div container

--- modules/adminPages/studiosPage/CinemaStudioDetails/components/AddStudioShowtimeModal/CinemaMovieSelectableList.jsx
+++ modules/adminPages/studiosPage/CinemaStudioDetails/components/AddStudioShowtimeModal/CinemaMovieSelectableList.jsx
@@ -1,8 +1,8 @@
-import { useSkeleton } from "hooks/useSkeleton";
-import { IoCheckmarkCircle } from "react-icons/io5";
+import { useSkeleton } from "hooks/useSkeleton"
+import { IoCheckmarkCircle } from "react-icons/io5"
 
-import { query } from "@/components/reactQuery/queries/query";
-import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys";
+import { query } from "@/components/reactQuery/queries/query"
+import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys"
 
 export const CinemaMovieSelectableList = ({
   cinemaId,
@@ -13,26 +13,26 @@ export const CinemaMovieSelectableList = ({
     queryKey: cinemaQueryKeys.cinemaMovies(cinemaId),
     url: "/api/cinemas/" + cinemaId + "/movies",
     enabledCondition: cinemaId !== undefined,
-  });
+  })
 
   const skeletons = useSkeleton(
     <div className="flex justify-center border-b p-2">
-      <div className="lg:h-[18px] w-3/6 rounded-lg bg-slate-300 animate-pulse" />
+      <div className="w-3/6 animate-pulse rounded-lg bg-slate-300 lg:h-[18px]" />
     </div>,
     5
-  );
+  )
 
   return (
     <div className="flex flex-1 snap-center flex-col gap-1.5">
       <h5 className="text-sm">Select movie</h5>
-      <div className="flex h-full w-80 flex-col rounded-lg border bg-slate-50 lg:w-full">
+      <div className="flex h-full w-80 flex-col rounded-lg border bg-slate-50 dark:border-slate-600 dark:bg-slate-800 lg:w-full">
         {cinemaMovies.isLoading && skeletons.map((skeleton) => skeleton)}
         {!cinemaMovies.isLoading &&
           cinemaMovies.data?.map((movie) => (
             <button
               onClick={() => selectMovie(movie.id)}
               key={movie.id}
-              className="relative border-b p-2 text-sm"
+              className="relative border-b p-2 text-sm dark:border-slate-700"
             >
               {selectedMovie === movie.id && (
                 <span className="absolute left-5 top-1/2 -translate-y-1/2">
@@ -44,5 +44,5 @@ export const CinemaMovieSelectableList = ({
           ))}
       </div>
     </div>
-  );
-};
+  )
+}
--- modules/adminPages/studiosPage/CinemaStudioDetails/components/AddStudioShowtimeModal/CinemaShowtimeSelectableList.jsx
+++ modules/adminPages/studiosPage/CinemaStudioDetails/components/AddStudioShowtimeModal/CinemaShowtimeSelectableList.jsx
@@ -1,8 +1,8 @@
-import { useSkeleton } from "hooks/useSkeleton";
-import { IoCheckmarkCircle } from "react-icons/io5";
+import { useSkeleton } from "hooks/useSkeleton"
+import { IoCheckmarkCircle } from "react-icons/io5"
 
-import { query } from "@/components/reactQuery/queries/query";
-import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys";
+import { query } from "@/components/reactQuery/queries/query"
+import { cinemaQueryKeys } from "@/components/reactQuery/queries/queryKeys/cinemaQueryKeys"
 
 export const CinemaShowtimeSelectableList = ({
   cinemaId,
@@ -13,26 +13,26 @@ export const CinemaShowtimeSelectableList = ({
     queryKey: cinemaQueryKeys.cinemaShowtimes(cinemaId),
     url: "/api/cinemas/" + cinemaId + "/showtimes",
     enabledCondition: cinemaId !== undefined,
-  });
+  })
 
   const skeletons = useSkeleton(
     <div className="flex justify-center border-b p-2">
-      <div className="lg:h-[18px] w-3/6 rounded-lg bg-slate-300 animate-pulse" />
+      <div className="w-3/6 animate-pulse rounded-lg bg-slate-300 lg:h-[18px]" />
     </div>,
     5
-  );
+  )
 
   return (
     <div className="flex snap-center flex-col gap-1.5 lg:flex-1">
       <h5 className="text-sm">Select Showtime</h5>
-      <div className="flex h-full w-80 flex-col rounded-lg border bg-slate-50 lg:w-full">
+      <div className="flex h-full w-80 flex-col rounded-lg border bg-slate-50 dark:border-slate-600 dark:bg-slate-800 lg:w-full">
         {cinemaShowtimes.isLoading && skeletons.map((skeleton) => skeleton)}
         {!cinemaShowtimes.isLoading &&
           cinemaShowtimes.data?.map((showtime) => (
             <button
               onClick={() => selectShowtime(showtime.id)}
               key={showtime.id}
-              className="relative border-b p-2 text-sm"
+              className="relative border-b p-2 text-sm dark:border-b-slate-700"
             >
               {selectedShowtime === showtime.id && (
                 <span className="absolute left-5 top-1/2 -translate-y-1/2">
@@ -44,5 +44,5 @@ export const CinemaShowtimeSelectableList = ({
           ))}
       </div>
     </div>
-  );
-};
+  )
+}
