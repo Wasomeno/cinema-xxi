9f6b77183bc21a97deee5e79b96704db401454c2
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-27 22:16:39 +0700

    refactor: add window removeListener inside useEffect cleanup function

--- hooks/useViewport.js
+++ hooks/useViewport.js
@@ -1,25 +1,34 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react"
 
 export const useViewport = () => {
   const [viewport, setViewport] = useState({
     width: 0,
     height: 0,
-  });
+  })
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     setViewport((currentViewport) => ({
       ...currentViewport,
       width: window.innerWidth,
       height: window.innerHeight,
-    }));
+    }))
     window.addEventListener("resize", () =>
       setViewport((currentViewport) => ({
         ...currentViewport,
         width: window.innerWidth,
         height: window.innerHeight,
       }))
-    );
-  }, []);
+    )
+    return () => {
+      window.removeEventListener("resize", () =>
+        setViewport((currentViewport) => ({
+          ...currentViewport,
+          width: window.innerWidth,
+          height: window.innerHeight,
+        }))
+      )
+    }
+  }, [])
 
-  return viewport;
-};
+  return viewport
+}
