8f17600ae199f7dcf2cbd523aa9385e0d665ad1b
Wasomeno <kevinanandapradipta@gmail.com>
2023-08-27 22:58:28 +0700

    style: remove semicolons

--- pages/api/users/[userAddress]/transactions/index.js
+++ pages/api/users/[userAddress]/transactions/index.js
@@ -1,19 +1,18 @@
-import { prisma } from "lib/prisma";
+import { prisma } from "lib/prisma"
 
 export default async function userTransactionsHandler(req, res) {
-  const { userAddress } = req.query;
-  console.log(req, res);
+  const { userAddress } = req.query
   if (req.method === "GET") {
     const transactions = await prisma.transaction.findMany({
       where: { user: userAddress },
       include: { movie: true, cinema: true, region: true },
-    });
+    })
 
     res.status(200).json(
       transactions.map((transaction) => ({
         ...transaction,
         showtime: parseInt(transaction.showtime),
       }))
-    );
+    )
   }
 }
