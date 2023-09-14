0ee8c66d4f18603c5fd34f1e196649ee1c90b3bd
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-14 15:36:12 +0700

    refactor(page): move contents inside CinemaAdmins to admin list of admin page

--- pages/admin/admins/index.jsx
+++ pages/admin/admins/index.jsx
@@ -1,11 +1,39 @@
-import { CinemaAdmins } from "modules/adminPages/adminsPage/CinemaAdmins/CinemaAdmins";
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { AnimatePresence } from "framer-motion"
 
-import { AdminLayout } from "@/components/Layouts/AdminLayout";
+import AdminHeader from "@/components/Headers/AdminHeader"
+import { AdminLayout } from "@/components/Layouts/AdminLayout"
+
+import { AddCinemaAdminModal } from "./components/AddCinemaAdminModal"
+import { CinemaAdminsTable } from "./components/CinemaAdminsTable"
+import { DeleteCinemaAdminsModal } from "./components/DeleteCinemaAdminsModal"
+import { EditCinemaAdminModal } from "./components/EditCinemaAdminModal"
 
 export default function CinemaAdminsPage() {
+  const [selectedAdmins, setSelectedAdmins] = useState([])
+  const { query, push } = useRouter()
   return (
     <AdminLayout pageTitle="Admins">
-      <CinemaAdmins />
+      <div className="flex flex-1 flex-col rounded-lg p-4">
+        <AdminHeader>Admins</AdminHeader>
+        <CinemaAdminsTable
+          selectedAdmins={selectedAdmins}
+          setSelectedAdmins={setSelectedAdmins}
+        />
+        <AnimatePresence>
+          {query.add && (
+            <AddCinemaAdminModal closeModal={() => push("/admin/admins")} />
+          )}
+          {query.edit && <EditCinemaAdminModal />}
+          {query.delete && (
+            <DeleteCinemaAdminsModal
+              closeModal={() => push("/admin/admins")}
+              selectedAdmins={selectedAdmins}
+            />
+          )}
+        </AnimatePresence>
+      </div>
     </AdminLayout>
-  );
+  )
 }
