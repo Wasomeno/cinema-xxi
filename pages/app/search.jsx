a58230f2461951ea4df04e52007d38fb4fb9a5db
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-14 15:32:25 +0700

    refactor(page): remove search page

--- pages/app/search.jsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router"
-
-import AppLayout from "@/components/Layouts/AppLayout"
-
-export default function SearchPage() {
-  const router = useRouter()
-  return <AppLayout>{router.query.q}</AppLayout>
-}
