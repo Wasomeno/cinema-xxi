bebe238b126f21e15d158c27f0473935f6bd2370
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-14 16:42:23 +0700

    refactor(hooks): remove useViewport custom hooks

--- components/Layouts/AppLayout.jsx
+++ components/Layouts/AppLayout.jsx
@@ -1,11 +1,9 @@
 import Head from "next/head"
-import { useViewport } from "hooks/useViewport"
 
 import AppNavigation from "../Navigations/App/AppNavigation"
 import AppNavigationMobile from "../Navigations/App/AppNavigationMobile"
 
 const AppLayout = ({ children, pageTitle }) => {
-  const viewport = useViewport()
   return (
     <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-slate-950">
       <Head>
@@ -14,9 +12,7 @@ const AppLayout = ({ children, pageTitle }) => {
       </Head>
       <AppNavigation />
       <div className="relative flex flex-1 flex-col">{children}</div>
-      {viewport.width < 1024 && viewport.height > 500 && (
-        <AppNavigationMobile />
-      )}
+      <AppNavigationMobile />
     </div>
   )
 }
