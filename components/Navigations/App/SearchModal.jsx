4283f451e4fcc35f9aadb920360cadb1b73e085a
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-14 16:38:33 +0700

    refactor(form): remove Form component because there's no need to do a submit action

--- components/Navigations/App/SearchModal.jsx
+++ components/Navigations/App/SearchModal.jsx
@@ -3,13 +3,13 @@ import { useRouter } from "next/router"
 import { BiMovie } from "react-icons/bi"
 import { HiOutlineBuildingOffice2 } from "react-icons/hi2"
 
-import { Form } from "@/components/Forms"
 import { CenteredModal, ModalHeader } from "@/components/Modal"
 import { query } from "@/components/reactQuery/queries/query"
 import { Spinner } from "@/components/Spinner"
 
 export const SearchModal = ({ toggleModal }) => {
   const [searchQuery, setSearchQuery] = useState("")
+
   const router = useRouter()
   const searchResults = query({
     queryKey: ["search"],
@@ -33,13 +33,10 @@ export const SearchModal = ({ toggleModal }) => {
   return (
     <CenteredModal
       closeModal={toggleModal}
-      className="flex h-3/6 flex-1 flex-col bg-slate-50 px-6 py-4 dark:bg-slate-800 lg:h-72 lg:w-5/12"
+      className="flex h-5/6 flex-1 flex-col bg-slate-50 px-6 py-4 dark:bg-slate-800 lg:h-72 lg:w-5/12"
     >
       <ModalHeader title="Search" className="mb-3" closeModal={toggleModal} />
-      <Form
-        onSubmit={() => router.push(`/app/search?q=${searchQuery}`)}
-        className=" mb-2 w-full lg:block"
-      >
+      <div className=" mb-2 w-full lg:block">
         <input
           type="text"
           onChange={(event) => {
@@ -48,7 +45,7 @@ export const SearchModal = ({ toggleModal }) => {
           className="w-full rounded-lg border bg-slate-100 px-3 py-1.5 text-sm dark:border-slate-700 dark:bg-slate-800"
           placeholder="Search for cinemas or movies...."
         />
-      </Form>
+      </div>
       <div className="flex flex-1 flex-col gap-2 overflow-y-scroll">
         {searchResults.isFetching ? (
           <div className="flex flex-1 items-center justify-center">
@@ -85,7 +82,6 @@ export const SearchModal = ({ toggleModal }) => {
                 key={result.index}
                 onClick={() => {
                   toggleModal()
-                  // router.push(`/app/cinemas/${result.id}`)
                 }}
                 className="flex items-center gap-4 rounded-md p-2 text-left font-openSans text-sm transition hover:bg-slate-100 dark:hover:bg-slate-700"
               >
