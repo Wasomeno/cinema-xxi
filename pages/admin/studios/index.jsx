ecb9e7ba134664521eb403a5a52c4de469665d0e
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-14 15:43:44 +0700

    refactor(page): move contents inside CinemaStudios to admin studios page

--- pages/admin/studios/index.jsx
+++ pages/admin/studios/index.jsx
@@ -1,11 +1,38 @@
-import { CinemaStudios } from "modules/adminPages/studiosPage/CinemaStudios";
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { AnimatePresence } from "framer-motion"
 
-import { AdminLayout } from "@/components/Layouts/AdminLayout";
+import { AddStudioModal } from "@/components/Admin/Studios/AddStudioModal"
+import { DeleteStudioModal } from "@/components/Admin/Studios/DeleteStudioModal"
+import { EditStudioModal } from "@/components/Admin/Studios/EditStudioModal"
+import { StudioTable } from "@/components/Admin/Studios/StudioTable"
+import AdminHeader from "@/components/Headers/AdminHeader"
+import { AdminLayout } from "@/components/Layouts/AdminLayout"
 
 export default function CinemaStudiosPage() {
+  const [selectedStudios, setSelectedStudios] = useState([])
+  const { query, push } = useRouter()
   return (
     <AdminLayout pageTitle="Studios">
-      <CinemaStudios />
+      <div className="flex w-full flex-1 flex-col rounded-lg p-4">
+        <AdminHeader>Studios</AdminHeader>
+        <StudioTable
+          selectedStudios={selectedStudios}
+          setSelectedStudios={setSelectedStudios}
+        />
+        <AnimatePresence>
+          {query.add && (
+            <AddStudioModal closeModal={() => push("/admin/studios")} />
+          )}
+          {query.edit && <EditStudioModal />}
+          {query.delete && (
+            <DeleteStudioModal
+              selectedStudio={selectedStudios}
+              closeModal={() => push("/admin/studios")}
+            />
+          )}
+        </AnimatePresence>
+      </div>
     </AdminLayout>
-  );
+  )
 }
