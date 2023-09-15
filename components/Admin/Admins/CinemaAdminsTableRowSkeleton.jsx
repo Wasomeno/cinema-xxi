14bf0882b33b4277946b411c33a2af924c261e61
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-15 17:30:09 +0700

    feat(skeleton): add skeleton for cinema admin table

---
+++ components/Admin/Admins/CinemaAdminsTableRowSkeleton.jsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { useSkeleton } from "hooks/useSkeleton"
+
+const CinemaAdminsTableRowSkeleton = () => {
+  return (
+    <tr>
+      <td className="px-6 py-4">
+        <div className="flex justify-center">
+          <div className="h-6 w-6 animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-8" />
+        </div>
+      </td>
+      <td className="px-6 py-4">
+        <div className="flex justify-center">
+          <div className="h-6 w-6 animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-8" />
+        </div>
+      </td>
+      <td className="px-6 py-4">
+        <div className="flex justify-center">
+          <div className="h-6 w-24 animate-pulse rounded-lg  bg-slate-300 lg:h-8 lg:w-32" />
+        </div>
+      </td>
+      <td className="px-6 py-4">
+        <div className="flex justify-center">
+          <div className="h-6 w-24  animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-32" />
+        </div>
+      </td>
+      <td className="px-6 py-4">
+        <div className="flex justify-center">
+          <div className="h-6 w-6 animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-8" />
+        </div>
+      </td>
+    </tr>
+  )
+}
+
+export const CinemaAdminsTableRowSkeletons = ({ table }) => {
+  const skeletons = useSkeleton(
+    <CinemaAdminsTableRowSkeleton table={table} />,
+    3
+  )
+  return skeletons
+}
