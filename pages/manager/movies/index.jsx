5bf6f24fffd77a19bf62c60d8f5050230f85c417
Wasomeno <kevinanandapradipta@gmail.com>
2023-09-15 17:44:09 +0700

    refactor: change to named function

--- pages/manager/movies/index.jsx
+++ pages/manager/movies/index.jsx
@@ -9,40 +9,34 @@ import { AllMoviesTable } from "@/components/Manager/Movies/AllMoviesTable"
 import { DeleteMoviesModal } from "@/components/Manager/Movies/DeleteMoviesModal"
 import { MovieDetailsModal } from "@/components/Manager/Movies/MovieDetailsModal"
 
-const Movies = () => {
+export default function MoviesPage() {
   const [selectedMovies, setSelectedMovies] = useState([])
   const router = useRouter()
   return (
-    <div className="flex flex-1 flex-col gap-2 overflow-y-scroll p-4">
-      <ManagerHeader>Manage Movies</ManagerHeader>
-      <AllMoviesTable
-        selectedMovies={selectedMovies}
-        setSelectedMovies={setSelectedMovies}
-      />
-      <AnimatePresence>
-        {router.query.view && (
-          <MovieDetailsModal
-            closeModal={() => router.push("/manager/movies")}
-          />
-        )}
-        {router.query.add && (
-          <AddMoviesModal closeModal={() => router.push("/manager/movies")} />
-        )}
-        {router.query.delete && (
-          <DeleteMoviesModal
-            closeModal={() => router.push("/manager/movies")}
-            selectedMovies={selectedMovies}
-          />
-        )}
-      </AnimatePresence>
-    </div>
-  )
-}
-
-export default function MoviesPage() {
-  return (
     <ManagerLayout pageTitle="Movies">
-      <Movies />
+      <div className="flex flex-1 flex-col gap-2 overflow-y-scroll p-4">
+        <ManagerHeader>Manage Movies</ManagerHeader>
+        <AllMoviesTable
+          selectedMovies={selectedMovies}
+          setSelectedMovies={setSelectedMovies}
+        />
+        <AnimatePresence>
+          {router.query.view && (
+            <MovieDetailsModal
+              closeModal={() => router.push("/manager/movies")}
+            />
+          )}
+          {router.query.add && (
+            <AddMoviesModal closeModal={() => router.push("/manager/movies")} />
+          )}
+          {router.query.delete && (
+            <DeleteMoviesModal
+              closeModal={() => router.push("/manager/movies")}
+              selectedMovies={selectedMovies}
+            />
+          )}
+        </AnimatePresence>
+      </div>
     </ManagerLayout>
   )
 }
--- pages/manager/regions/index.jsx
+++ pages/manager/regions/index.jsx
@@ -9,38 +9,36 @@ import { AllRegionsTable } from "@/components/Manager/Regions/AllRegionsTable"
 import { DeleteRegionsModal } from "@/components/Manager/Regions/DeleteRegionsModal"
 import { EditRegionModal } from "@/components/Manager/Regions/EditRegionModal"
 
-const Regions = () => {
+export default function RegionsPage() {
   const [selectedRegions, setSelectedRegions] = useState([])
   const router = useRouter()
   return (
-    <div className="flex flex-1 flex-col gap-2 overflow-y-scroll p-4">
-      <ManagerHeader>Manage Regions</ManagerHeader>
-      <AllRegionsTable
-        selectedRegions={selectedRegions}
-        setSelectedRegions={setSelectedRegions}
-      />
-      <AnimatePresence>
-        {router.query.add && (
-          <AddRegionModal closeModal={() => router.push("/manager/regions")} />
-        )}
-        {router.query.edit && (
-          <EditRegionModal closeModal={() => router.push("/manager/regions")} />
-        )}
-        {router.query.delete && (
-          <DeleteRegionsModal
-            closeModal={() => router.push("/manager/regions")}
-            selectedRegions={selectedRegions}
-          />
-        )}
-      </AnimatePresence>
-    </div>
-  )
-}
-
-export default function RegionsPage() {
-  return (
     <ManagerLayout pageTitle="Regions">
-      <Regions />
+      <div className="flex flex-1 flex-col gap-2 overflow-y-scroll p-4">
+        <ManagerHeader>Manage Regions</ManagerHeader>
+        <AllRegionsTable
+          selectedRegions={selectedRegions}
+          setSelectedRegions={setSelectedRegions}
+        />
+        <AnimatePresence>
+          {router.query.add && (
+            <AddRegionModal
+              closeModal={() => router.push("/manager/regions")}
+            />
+          )}
+          {router.query.edit && (
+            <EditRegionModal
+              closeModal={() => router.push("/manager/regions")}
+            />
+          )}
+          {router.query.delete && (
+            <DeleteRegionsModal
+              closeModal={() => router.push("/manager/regions")}
+              selectedRegions={selectedRegions}
+            />
+          )}
+        </AnimatePresence>
+      </div>
     </ManagerLayout>
   )
 }
