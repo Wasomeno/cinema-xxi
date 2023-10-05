ae7935f45477e979ef5aafed1155ce23a258e6f2
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-05 18:12:55 +0700

    refactor: remove selectedRegion props ,add useRouter ,add router.replace inside region button onClick

--- components/App/Home/RegionListModal.jsx
+++ components/App/Home/RegionListModal.jsx
@@ -1,19 +1,20 @@
+import { useRouter } from "next/router"
 import { useSkeleton } from "hooks/useSkeleton"
 import { HiCheck } from "react-icons/hi2"
 
 import { CenteredModal, ModalHeader } from "@/components/Modal"
 import { useAllRegions } from "@/components/reactQuery/queries/Region/useAllRegions"
 
-const RegionListModal = ({
-  selectedRegion,
-  setSelectedRegion,
-  toggleShowRegionList,
-}) => {
+const RegionListModal = ({ setSelectedRegion, toggleShowRegionList }) => {
   const allRegion = useAllRegions()
   const skeletons = useSkeleton(
     <div className="h-10 w-full animate-pulse rounded-lg bg-slate-300 dark:bg-slate-600" />,
     5
   )
+
+  const router = useRouter()
+
+  const regionId = parseInt(router.query.region ?? "1")
 
   return (
     <CenteredModal
@@ -27,24 +28,26 @@ const RegionListModal = ({
       />
       <div className="flex flex-col items-center gap-2 overflow-y-scroll">
         {allRegion.isLoading && skeletons.map((skeleton) => skeleton)}
+
         {!allRegion.isLoading && allRegion.status !== "error"
           ? allRegion.data?.map((region) => (
-              <div
+              <button
                 key={region.id}
                 onClick={() => {
                   setSelectedRegion(region)
                   toggleShowRegionList()
+                  router.replace(`${router.pathname}?region=${region.id}`)
                 }}
                 className="relative flex w-full cursor-pointer justify-center rounded-lg bg-slate-200 p-2 text-sm tracking-wide dark:bg-slate-700"
               >
-                {selectedRegion.id === region.id && (
+                {regionId === region.id && (
                   <span className="absolute left-4 top-1/2 -translate-y-1/2">
                     <HiCheck />
                   </span>
                 )}
 
                 <span className="text-xs lg:text-sm">{region.name}</span>
-              </div>
+              </button>
             ))
           : null}
       </div>
