ca05635b919386c2af80c10d4e170317ca9ffde9
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-08 15:25:58 +0700

    refactor: remove 'XXI Cinema App' in title and add useEffect to contain function that automatically connect user wallet

--- components/Layouts/AppLayout.jsx
+++ components/Layouts/AppLayout.jsx
@@ -1,13 +1,30 @@
+import { useEffect } from "react"
 import Head from "next/head"
+import { useAccount, useConnect } from "wagmi"
+import { MetaMaskConnector } from "wagmi/connectors/metaMask"
 
 import AppNavigation from "../Navigations/App/AppNavigation"
 import AppNavigationMobile from "../Navigations/App/AppNavigationMobile"
 
-const AppLayout = ({ children, pageTitle }) => {
+const AppLayout = ({ children, title }) => {
+  const { connect } = useConnect()
+  const { isConnected } = useAccount()
+
+  useEffect(() => {
+    const isMetamaskConnected =
+      localStorage.getItem("isMetamaskConnected") !== null
+    if (isMetamaskConnected && !isConnected) {
+      connect({
+        chainId: 11155111,
+        connector: new MetaMaskConnector(),
+      })
+    }
+  }, [])
+
   return (
     <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-slate-950">
       <Head>
-        <title>{`XXI Cinema App  ${pageTitle ? `| ${pageTitle}` : ""}`}</title>
+        <title>{`${title ?? ""}`}</title>
         <meta property="description" content="App page" />
       </Head>
       <AppNavigation />
