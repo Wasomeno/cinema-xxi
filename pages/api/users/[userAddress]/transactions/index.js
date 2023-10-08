c7985eb8b847c8a5729ccd8f309bf2ef4f002943
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-08 15:40:54 +0700

    feat: add condition for the time of transaction creation needed to be fetched

--- pages/api/users/[userAddress]/transactions/index.js
+++ pages/api/users/[userAddress]/transactions/index.js
@@ -1,10 +1,17 @@
 import { prisma } from "lib/prisma"
 
 export default async function userTransactionsHandler(req, res) {
-  const { userAddress } = req.query
+  const { userAddress, history } = req.query
+
+  const dateISOString = new Date().toISOString()
+  const isHistory = history !== undefined
+
   if (req.method === "GET") {
     const transactions = await prisma.transaction.findMany({
-      where: { user: userAddress },
+      where: {
+        user: userAddress,
+        createdAt: isHistory ? { lt: dateISOString } : { gt: dateISOString },
+      },
       include: { movie: true, cinema: true, region: true },
     })
 
