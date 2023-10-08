f8bf630bf6a8cc2d683781662520df78c547a74c
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-08 15:19:03 +0700

    refactor: add custom animation for modal container based on the screen size

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion"
+import { useViewport } from "hooks/useViewport"
 import { cn } from "lib/utils"
 import { createPortal } from "react-dom"
 import { HiXMark } from "react-icons/hi2"
@@ -6,40 +8,58 @@ import { twMerge } from "tailwind-merge"
 import AnimatedContainer from "./AnimatedContainer"
 
 export const Modal = ({ children, className, closeModal }) => {
+  const { width } = useViewport()
   return createPortal(
     <>
       <AnimatedContainer
         onClick={closeModal}
         className="fixed bottom-0 left-0 z-20 h-screen w-full bg-slate-800 bg-opacity-50 backdrop-blur-sm"
       />
-      <AnimatedContainer
+      <motion.div
+        initial={{ opacity: 0, bottom: width <= 500 ? "-50px" : 0 }}
+        animate={{ opacity: 1, bottom: 0 }}
+        transition={{
+          duration: 0.25,
+          ease: "easeInOut",
+          delay: width <= 500 ? 0.1 : 0,
+        }}
+        exit={{ opacity: 0, bottom: width <= 500 ? "-50px" : 0 }}
         className={twMerge(
-          "fixed bottom-0 z-40 w-full rounded-none rounded-t-lg px-4 py-2 shadow-md lg:rounded-lg",
+          "fixed  z-40 w-full rounded-none rounded-t-lg px-4 py-2 shadow-md lg:rounded-lg",
           className
         )}
       >
         {children}
-      </AnimatedContainer>
+      </motion.div>
     </>,
     document.getElementById("modal-portal-container")
   )
 }
 
 export const CenteredModal = ({ children, className, closeModal }) => {
+  const { width } = useViewport()
   return createPortal(
     <>
       <AnimatedContainer
         onClick={closeModal}
         className="fixed bottom-0 left-0 z-20 h-screen w-full bg-slate-800 bg-opacity-50 backdrop-blur-sm"
       />
-      <AnimatedContainer
+      <motion.div
+        initial={{ opacity: 0, bottom: width <= 500 ? "-50px" : 0 }}
+        animate={{ opacity: 1, bottom: 0 }}
+        transition={{
+          duration: 0.25,
+          ease: "easeInOut",
+          delay: width <= 500 ? 0.1 : 0,
+        }}
+        exit={{ opacity: 0, bottom: width <= 500 ? "-50px" : 0 }}
         className={twMerge(
-          "fixed inset-x-1/2 bottom-0 z-40 h-[90%] w-full -translate-x-1/2 flex-col rounded-none rounded-t-2xl px-4 py-2 shadow-md sm:h-3/6 sm:w-4/6 lg:inset-y-1/2 lg:h-5/6 lg:w-4/6 lg:-translate-y-1/2 lg:rounded-xl",
+          "fixed inset-x-1/2 z-40 h-[90%] w-full -translate-x-1/2 flex-col rounded-none rounded-t-2xl p-4 shadow-md sm:h-3/6 sm:w-4/6 lg:inset-y-1/2 lg:h-5/6 lg:w-4/6 lg:-translate-y-1/2 lg:rounded-xl",
           className
         )}
       >
         {children}
-      </AnimatedContainer>
+      </motion.div>
     </>,
     document.getElementById("modal-portal-container")
   )
