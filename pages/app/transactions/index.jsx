94c93d081f0d13994a6447358dfbfac3345af010
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-08 15:41:27 +0700

    refactor: add function to get the transaction details url

--- pages/app/transactions/index.jsx
+++ pages/app/transactions/index.jsx
@@ -34,13 +34,19 @@ export default function AppTransactionsPage() {
 
   const skeletons = useSkeleton(<TransactionCardSkeleton />, 5)
 
+  function getTransactionURL(transactionId) {
+    const newSearchParams = new URLSearchParams(searchParams.toString())
+    newSearchParams.set("id", transactionId)
+    return `${router.pathname}?${newSearchParams.toString()}`
+  }
+
   return (
-    <AppLayout pageTitle="Transactions">
+    <AppLayout title="Transactions | Cinema App">
       {!isConnected ? (
         <WalletNotConnected />
       ) : (
-        <AnimatedContainer className="relative flex min-h-screen flex-1 flex-col bg-opacity-95 p-5 lg:p-10">
-          <div className="mb-4 space-y-1.5 text-start">
+        <AnimatedContainer className="relative flex min-h-screen flex-1 flex-col bg-opacity-95 p-4 lg:p-10">
+          <div className="mb-2 space-y-1.5 text-start lg:mb-4">
             <h1 className="font-poppins text-base font-semibold lg:text-3xl">
               Transactions
             </h1>
@@ -49,7 +55,7 @@ export default function AppTransactionsPage() {
             <button
               onClick={() => router.replace(`${router.pathname}`)}
               className={twMerge(
-                "rounded-lg bg-slate-100  px-3 py-1.5 text-xs text-slate-500 transition duration-200 lg:text-sm",
+                "rounded-lg bg-slate-100 px-3  py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
                 !isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
               )}
             >
@@ -58,7 +64,7 @@ export default function AppTransactionsPage() {
             <button
               onClick={() => router.replace(`${router.pathname}?history=true`)}
               className={twMerge(
-                "rounded-lg bg-slate-100 px-3 py-1.5 text-xs text-slate-500 transition duration-200 lg:text-sm",
+                "rounded-lg bg-slate-100 px-3 py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
                 isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
               )}
             >
@@ -70,8 +76,8 @@ export default function AppTransactionsPage() {
             {!transactions.isLoading && transactions.data?.length
               ? transactions.data.map((transaction) => (
                   <TransactionCard
-                    href={`/app/transactions?id=${transaction.id}`}
                     key={transaction.id}
+                    href={getTransactionURL(transaction.id)}
                     cinema={transaction.cinema}
                     movie={transaction.movie}
                     showtime={transaction.showtime}
@@ -83,8 +89,10 @@ export default function AppTransactionsPage() {
 
             {!transactions.isLoading && !transactions.data?.length ? (
               <div className="flex h-80 w-full  flex-col items-center justify-center gap-1.5">
-                <span className="text-slate-400">No Transactions</span>
-                <RxCrossCircled size="32" className="text-slate-400" />
+                <span className="text-sm text-slate-400 lg:text-base">
+                  No Transactions
+                </span>
+                <RxCrossCircled className="h-6 w-6 text-slate-400 lg:h-7 lg:w-7" />
               </div>
             ) : null}
           </div>
