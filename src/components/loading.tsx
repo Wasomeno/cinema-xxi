86a878d3ec5e7675cf1c460a4241844a5b5df0cf
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-16 16:50:00 +0700

    refactor: change useLoading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -7,13 +7,13 @@ import { CenteredModal } from "./modal"
 import { Spinner } from "./spinner"
 
 export function Loading() {
-  const [loading, loadingText] = useLoadingDetails()
+  const { loading, text } = useLoadingDetails()
   return (
     <AnimatePresence>
       {loading && (
         <CenteredModal className="flex h-3/6 flex-col items-center justify-center gap-5 bg-slate-50 dark:bg-slate-800 lg:h-80 lg:w-72 ">
           <span className="font-poppins text-sm font-medium lg:text-base">
-            {loadingText}
+            {text}
           </span>
           <Spinner />
         </CenteredModal>
