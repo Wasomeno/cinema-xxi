cc26d3480b6adb265e69f35c20c12426efdc153d
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-16 16:49:01 +0700

    refactor: change function name to GET

--- src/app/api/users/[userAddress]/route.ts
+++ src/app/api/users/[userAddress]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 import { prisma } from "@/lib/prisma"
 
-export async function userDetailsHandler(
+export async function GET(
   request: NextRequest,
   context: { params: { userAddress: string } }
 ) {
--- src/app/api/users/[userAddress]/transactions/[transactionId]/route.ts
+++ src/app/api/users/[userAddress]/transactions/[transactionId]/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server"
 
 import { prisma } from "@/lib/prisma"
 
-export default async function transactionDetails(
+export async function GET(
   request: NextRequest,
   context: { params: { transactionId: string } }
 ) {
