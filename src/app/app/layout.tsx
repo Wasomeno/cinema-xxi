50b5124b6cca996da5120d979a474810232eaa24
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-18 20:35:01 +0700

    refactor: remove WalletAutoReconnect

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react"
 
-import { WalletAutoReconnect } from "@/components/App/wallet-auto-reconnect"
 import { AppNavigation } from "@/components/Navigations/App/app-navigation"
 import { AppNavigationMobile } from "@/components/Navigations/App/app-navigation-mobile"
 
@@ -10,12 +9,10 @@ type AppLayoutProps = {
 
 export default function AppLayout({ children }: AppLayoutProps) {
   return (
-    <WalletAutoReconnect>
-      <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-slate-950">
-        <AppNavigation />
-        <div className="relative flex flex-1 flex-col">{children}</div>
-        <AppNavigationMobile />
-      </div>
-    </WalletAutoReconnect>
+    <div className="flex min-h-screen flex-1 flex-col bg-white dark:bg-slate-950">
+      <AppNavigation />
+      <div className="relative flex flex-1 flex-col">{children}</div>
+      <AppNavigationMobile />
+    </div>
   )
 }
