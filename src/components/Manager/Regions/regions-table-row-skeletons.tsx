7254153eb32b9c2450933a953f28f73cbc977e1d
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-18 21:30:06 +0700

    refactor(name): rename to regions-table-row-skeleton

--- src/components/Manager/Regions/regions-table-row-skeletons.tsx
+++ src/components/Manager/Regions/regions-table-row-skeletons.tsx
@@ -25,16 +25,11 @@ const RegionTableSkeleton = () => {
           <div className="h-6 w-6  animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-8" />
         </div>
       </td>
-      <td className="px-6 py-4">
-        <div className="flex justify-center">
-          <div className="h-6 w-6 animate-pulse rounded-lg bg-slate-300  lg:h-8 lg:w-8" />
-        </div>
-      </td>
     </tr>
   )
 }
 
-export const RegionTableSkeletons = () => {
+export const RegionsTableRowSkeletons = () => {
   const skeletons = useSkeleton(<RegionTableSkeleton />, 3)
   return skeletons
 }
