acea32be8b7522987d25e68d7ff795e787125b07
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-18 21:33:17 +0700

    refactor(import): remove unused imports

--- src/components/App/ConnectWalletModal/connect-wallet-modal.tsx
+++ src/components/App/ConnectWalletModal/connect-wallet-modal.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { Dispatch, SetStateAction } from "react"
+import React from "react"
 import { useConnect } from "wagmi"
 
 import { CenteredModal } from "@/components/modal"
