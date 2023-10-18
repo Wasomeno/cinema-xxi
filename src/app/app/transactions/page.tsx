9ec767e086aa2a81e7fdb1f08d5e3f403a207a5e
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-18 21:01:34 +0700

    refactor: remove isConnected, remove enabled transactions query

--- src/app/app/transactions/page.tsx
+++ src/app/app/transactions/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
-import { Prisma, Transaction } from "@prisma/client"
+import { Prisma } from "@prisma/client"
 import { useQuery } from "@tanstack/react-query"
 import { RxCrossCircled } from "react-icons/rx"
 import { twMerge } from "tailwind-merge"
@@ -14,10 +14,10 @@ import {
   TransactionCardSkeleton,
 } from "@/components/App/Transaction/transaction-card"
 import { TransactionDetailsModal } from "@/components/App/Transaction/TransactionDetailsModal"
-import { WalletNotConnected } from "@/components/WalletNotConnected"
+import { WalletConnection } from "@/components/App/wallet-connection"
 
-export default function AppTransactionsPage() {
-  const { address, isConnected } = useAccount()
+export default function TransactionsPage() {
+  const { address } = useAccount()
 
   const searchParams = useSearchParams()
 
@@ -29,13 +29,10 @@ export default function AppTransactionsPage() {
     Prisma.TransactionGetPayload<{
       include: { movie: true; cinema: true; region: true }
     }>[]
-  >(
-    ["transactions", isHistory],
-    () =>
-      fetch(
-        `/api/users/${address}/transactions?${searchParams.toString()}`
-      ).then((response) => response.json()),
-    { enabled: isConnected }
+  >(["transactions", isHistory], () =>
+    fetch(`/api/users/${address}/transactions?${searchParams.toString()}`).then(
+      (response) => response.json()
+    )
   )
 
   const skeletons = useSkeleton(<TransactionCardSkeleton />, 5)
@@ -46,60 +43,61 @@ export default function AppTransactionsPage() {
     return `${pathname}?${newSearchParams.toString()}`
   }
 
-  if (!isConnected) return <WalletNotConnected />
   return (
-    <AnimatedContainer className="relative flex min-h-screen flex-1 flex-col bg-opacity-95 p-4 lg:p-10">
-      <div className="mb-2 space-y-1.5 text-start lg:mb-4">
-        <h1 className="font-poppins text-base font-semibold lg:text-3xl">
-          Transactions
-        </h1>
-      </div>
-      <div className="flex gap-2">
-        <button
-          onClick={() => router.replace(`${pathname}`)}
-          className={twMerge(
-            "rounded-lg bg-slate-100 px-3  py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
-            !isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
-          )}
-        >
-          Active
-        </button>
-        <button
-          onClick={() => router.replace(`${pathname}?history=true`)}
-          className={twMerge(
-            "rounded-lg bg-slate-100 px-3 py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
-            isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
-          )}
-        >
-          History
-        </button>
-      </div>
-      <div className="mt-4 flex flex-col gap-2">
-        {transactions.isLoading && skeletons.map((skeleton) => skeleton)}
-        {!transactions.isLoading && transactions.data?.length
-          ? transactions.data.map((transaction) => (
-              <TransactionCard
-                key={transaction.id}
-                href={getTransactionURL(transaction.id)}
-                cinema={transaction.cinema}
-                movie={transaction.movie}
-                showtime={parseInt(transaction.showtime.toString())}
-                studio={transaction.studio}
-                ticketAmount={transaction.ticketIds.length}
-              />
-            ))
-          : null}
+    <WalletConnection>
+      <AnimatedContainer className="relative flex min-h-screen flex-1 flex-col bg-opacity-95 p-4 lg:p-10">
+        <div className="mb-2 space-y-1.5 text-start lg:mb-4">
+          <h1 className="font-poppins text-base font-semibold lg:text-3xl">
+            Transactions
+          </h1>
+        </div>
+        <div className="flex gap-2">
+          <button
+            onClick={() => router.replace(`${pathname}`)}
+            className={twMerge(
+              "rounded-lg bg-slate-100 px-3  py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
+              !isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
+            )}
+          >
+            Active
+          </button>
+          <button
+            onClick={() => router.replace(`${pathname}?history=true`)}
+            className={twMerge(
+              "rounded-lg bg-slate-100 px-3 py-1.5 text-xs font-medium text-slate-500 transition duration-200 lg:text-sm",
+              isHistory && "bg-blue-100 text-slate-800 dark:bg-blue-300"
+            )}
+          >
+            History
+          </button>
+        </div>
+        <div className="mt-4 flex flex-col gap-2">
+          {transactions.isLoading && skeletons.map((skeleton) => skeleton)}
+          {!transactions.isLoading && transactions.data?.length
+            ? transactions.data.map((transaction) => (
+                <TransactionCard
+                  key={transaction.id}
+                  href={getTransactionURL(transaction.id)}
+                  cinema={transaction.cinema}
+                  movie={transaction.movie}
+                  showtime={parseInt(transaction.showtime.toString())}
+                  studio={transaction.studio}
+                  ticketAmount={transaction.ticketIds.length}
+                />
+              ))
+            : null}
 
-        {!transactions.isLoading && !transactions.data?.length ? (
-          <div className="flex h-80 w-full  flex-col items-center justify-center gap-1.5">
-            <span className="text-sm text-slate-400 lg:text-base">
-              No Transactions
-            </span>
-            <RxCrossCircled className="h-6 w-6 text-slate-400 lg:h-7 lg:w-7" />
-          </div>
-        ) : null}
-      </div>
-      <TransactionDetailsModal />
-    </AnimatedContainer>
+          {!transactions.isLoading && !transactions.data?.length ? (
+            <div className="flex h-80 w-full  flex-col items-center justify-center gap-1.5">
+              <span className="text-sm text-slate-400 lg:text-base">
+                No Transactions
+              </span>
+              <RxCrossCircled className="h-6 w-6 text-slate-400 lg:h-7 lg:w-7" />
+            </div>
+          ) : null}
+        </div>
+        <TransactionDetailsModal />
+      </AnimatedContainer>
+    </WalletConnection>
   )
 }
