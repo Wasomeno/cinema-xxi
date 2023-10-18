55fb082a9a1b1f8f8e24096a255c6381a9c8a9cb
Wasomeno <kevinanandapradipta@gmail.com>
2023-10-18 21:33:36 +0700

    refactor: remove unused import

--- src/components/App/Movie/available-cinema.tsx
+++ src/components/App/Movie/available-cinema.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { Dispatch, ReactNode, SetStateAction, useState } from "react"
+import {  ReactNode, useState } from "react"
 import clsx from "clsx"
 import { motion } from "framer-motion"
 import moment from "moment"
 import { HiChevronRight } from "react-icons/hi2"
 
-import { StudioShowtime, useSelectedDate, useSelectedShowtime } from "./ticket-context-provider"
+import { StudioShowtime, useSelectedDate, useSelectedShowtime } from "@/stores/ticketStore"
 import { Cinema } from "@prisma/client"
 import { useDateTime } from "@/hooks/useDateTime"
 import { usePathname,useRouter } from "next/navigation"
@@ -59,7 +59,7 @@ export function Cinema({ cinema, children }:{cinema:Cinema, children:ReactNode})
 }
 
 export function Showtime({ showtime }:{showtime:StudioShowtime}) {
-  const { setSelectedShowtime } = useSelectedShowtime()
+  const { selectShowtime } = useSelectedShowtime()
   const { selectedDate } = useSelectedDate()
   const dateTime = useDateTime()
 
@@ -83,7 +83,7 @@ export function Showtime({ showtime }:{showtime:StudioShowtime}) {
       animate={{ display: "inline-block" }}
       transition={{ bounce: false, delay: 0.05 }}
       exit={{ display: "hidden" }}
-      onClick={() => {(setSelectedShowtime as Dispatch<SetStateAction<StudioShowtime>>)(showtime); router.replace(`${pathname}?seats=true`)}}
+      onClick={() => {selectShowtime(showtime); router.replace(`${pathname}?seats=true`)}}
       className="rounded-md bg-slate-200 p-2 px-3 text-center font-poppins text-xs shadow-sm transition duration-200 ease-in-out hover:bg-slate-300 disabled:opacity-50 dark:bg-slate-800 dark:hover:bg-slate-700 md:text-sm"
     >
       {showtime.showtime.hour} :{showtime.showtime.minutes}
