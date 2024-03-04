1249c789038b7e89126fb13e1248a7321bb81de3
Wasomeno <kevinanandapradipta@gmail.com>
2024-03-04 18:24:29 +0700

    refactor(cinemas-page): remove wallet connection wrapper

--- src/app/app/cinemas/page.tsx
+++ src/app/app/cinemas/page.tsx
@@ -1,25 +1,22 @@
 import { AnimatedContainer } from "@/components/animated-container"
 import { CinemaList } from "@/components/App/Cinemas/cinema-list"
 import { CinemaSearchInput } from "@/components/App/Cinemas/cinema-search-input"
-import { WalletConnection } from "@/components/App/wallet-connection"
 
 export default function AppCinemaSearchPage() {
   return (
-    <WalletConnection>
-      <AnimatedContainer className="flex-1 p-4 lg:p-10">
-        <div className="space-y-1.5text-start mb-2.5">
-          <h1 className="font-poppins text-base font-semibold lg:text-3xl">
-            Search Cinemas
-          </h1>
-          <p className="text-xs lg:text-base">
-            Search for cinema of your choosing
-          </p>
-        </div>
-        <div className="sticky top-[59px] bg-white dark:bg-slate-950 lg:static lg:w-3/6">
-          <CinemaSearchInput />
-        </div>
-        <CinemaList />
-      </AnimatedContainer>
-    </WalletConnection>
+    <AnimatedContainer className="flex-1 p-4 lg:p-10">
+      <div className="space-y-1.5text-start mb-2.5">
+        <h1 className="font-poppins text-base font-semibold lg:text-3xl">
+          Search Cinemas
+        </h1>
+        <p className="text-xs lg:text-base">
+          Search for cinema of your choosing
+        </p>
+      </div>
+      <div className="sticky top-[59px] bg-white dark:bg-slate-950 lg:static lg:w-3/6">
+        <CinemaSearchInput />
+      </div>
+      <CinemaList />
+    </AnimatedContainer>
   )
 }
